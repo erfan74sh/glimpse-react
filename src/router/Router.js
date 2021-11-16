ad659a6ded6044a8ec640d6229b8231589d40e29
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-16 18:03:30 +0330

    create router.js

---
+++ src/router/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+// pages
+import App from "../App";
+
+const Router = () => {
+	return (
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	);
+};
+
+export default Router;
