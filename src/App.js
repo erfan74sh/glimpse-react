7df7bf4a2185537b8aa8ccda231d595b64d89407
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-16 18:05:07 +0330

    add routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+// pages
+import Home from "./pages/Home";
+import Estimation from "./pages/Estimation";
+import Result from "./pages/Estimation/Result";
+import Estimate from "./pages/Estimation/Estimate";
+// style
 import "./App.scss";
 
 function App() {
-	return <div className="App">this is the project</div>;
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/estimation" element={<Estimation />}>
+				<Route index element={<Estimate />} />
+				<Route path="estimate" element={<Estimate />} />
+				<Route path="result" element={<Result />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
