03cfb88a3d9a03c6b19bac670e422ff648123f0b
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-17 19:32:46 +0330

    add color/spacing custom properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				blue: {
+					550: "#147CDD",
+				},
+				gray: {
+					950: "#030303",
+				},
+			},
+			spacing: {
+				152: "38rem",
+				160: "40rem",
+			},
+		},
 	},
 	variants: {
 		extend: {},
