d056614602f5f7d0a39c7ded48754075df6f81b5
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-17 19:33:42 +0330

    create Nav.jsx comp

---
+++ src/pages/Estimation/Nav/Nav.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// assets
+import Logo from "../../../assets/images/logo-02.png";
+
+const Header = () => {
+	return (
+		<nav className="bg-gray-950 text-white">
+			<ul className="flex justify-between items-center px-9 py-5">
+				<li>
+					<Link to="/">
+						<img src={Logo} alt="glimpse logo" />
+					</Link>
+				</li>
+				<li>
+					<ul className="flex justify-items-center items-center gap-x-40">
+						<li>
+							<Link to="/">home</Link>
+						</li>
+						<li>
+							<Link to="/">home</Link>
+						</li>
+						<li>
+							<Link to="/">home</Link>
+						</li>
+						<li>
+							<Link to="/">home</Link>
+						</li>
+					</ul>
+				</li>
+				<li>user name</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Header;
