432d915dbd2d1f5a4d923df6481ff8204a1e81a0
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-18 21:54:20 +0330

    fix margin bot in header

--- src/pages/Estimation/Result/Result.js
+++ src/pages/Estimation/Result/Result.js
@@ -7,7 +7,7 @@ import "./Result.scss";
 const Result = () => {
 	return (
 		<main className=" px-24 py-16" id="result__main">
-			<header className="mb-14">
+			<header className="mb-10">
 				<h1 className="pl-3 leading-8 border-l-8 border-blue-550 text-blue-550 text-3xl font-bold uppercase">
 					result
 				</h1>
