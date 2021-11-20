9d11e717d69e1a9f637228f647790ef35cf2bb36
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-20 17:18:59 +0330

    handle data in Result.js

--- src/pages/Estimation/Result/Result.js
+++ src/pages/Estimation/Result/Result.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // compoents
 import Chart from "./Chart";
 // style
 import "./Result.scss";
 
 const Result = () => {
+	const [series, setSeries] = useState([]);
+	useEffect(() => {
+		const data = [
+			{
+				name: "output_1",
+				stroke: "#926ECB",
+				opacity: 1,
+				data: [
+					{ name: "X- Dimention", uv: 400, amt: 8 },
+					{ name: "Y- Dimention", uv: 450, amt: 20 },
+					{ name: "Material", uv: 380, amt: 30 },
+					{ name: "Shading", uv: 410, amt: 20 },
+					{ name: "UDI", uv: 410, amt: 65 },
+					{ name: "SDA", uv: 410, amt: 80 },
+					{ name: "ASE", uv: 410, amt: 35 },
+				],
+			},
+			{
+				name: "output_2",
+				stroke: "#00C48C",
+				opacity: 1,
+				data: [
+					{ name: "X- Dimention", uv: 400, amt: 10 },
+					{ name: "Y- Dimention", uv: 450, amt: 30 },
+					{ name: "Material", uv: 380, amt: 70 },
+					{ name: "Shading", uv: 410, amt: 40 },
+					{ name: "UDI", uv: 410, amt: 50 },
+					{ name: "SDA", uv: 410, amt: 55 },
+					{ name: "ASE", uv: 410, amt: 10 },
+				],
+			},
+		];
+		setSeries(data);
+	}, []);
+
 	return (
 		<main className=" px-24 py-16" id="result__main">
 			<header className="mb-10">
@@ -14,7 +49,7 @@ const Result = () => {
 			</header>
 			<main className="">
 				<section className="flex justify-center items-center max-h-152 overflow-hidden">
-					<Chart />
+					<Chart series={series} />
 				</section>
 				<section className="flex flex-col px-52 mt-20">
 					<h2 className=" mb-9 mx-8 pb-4 border-b text-xl font-normal uppercase border-gray-500">
