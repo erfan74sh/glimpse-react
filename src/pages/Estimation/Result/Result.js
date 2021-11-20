0e33e57e3173ffced611f589b3dd0f517c47031e
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-20 18:53:43 +0330

    add visibility handler to manage outputs in chart

--- src/pages/Estimation/Result/Result.js
+++ src/pages/Estimation/Result/Result.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // compoents
 import Chart from "./Chart";
 // style
 import "./Result.scss";
+// icons
+import {
+	faEye,
+	faEyeSlash,
+	faSortDown,
+	faSortUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Result = () => {
 	const [series, setSeries] = useState([]);
@@ -12,6 +20,7 @@ const Result = () => {
 				name: "output_1",
 				stroke: "#926ECB",
 				opacity: 1,
+				visible: true,
 				data: [
 					{ name: "X- Dimention", uv: 400, amt: 8 },
 					{ name: "Y- Dimention", uv: 450, amt: 20 },
@@ -26,6 +35,7 @@ const Result = () => {
 				name: "output_2",
 				stroke: "#00C48C",
 				opacity: 1,
+				visible: true,
 				data: [
 					{ name: "X- Dimention", uv: 400, amt: 10 },
 					{ name: "Y- Dimention", uv: 450, amt: 30 },
@@ -39,6 +49,14 @@ const Result = () => {
 		];
 		setSeries(data);
 	}, []);
+
+	const handleVisibility = (e) => {
+		const name = e.currentTarget.parentNode.dataset.name;
+		let temState = [...series];
+		let item = temState.filter((entry) => entry.name === name)[0];
+		item.visible = !item.visible;
+		setSeries(temState);
+	};
 
 	return (
 		<main className=" px-24 py-16" id="result__main">
@@ -55,6 +73,28 @@ const Result = () => {
 					<h2 className=" mb-9 mx-8 pb-4 border-b text-xl font-normal uppercase border-gray-500">
 						geometry
 					</h2>
+					<ul className="flex flex-col gap-y-9">
+						{series.map((entry, i) => (
+							<li
+								key={i}
+								className="flex content-between items-center mx-16 px-4 py-3.5 text-sm rounded-md shadow-xl bg-white"
+								data-name={entry.name}
+							>
+								<span className="font-bold">{i + 1} -&nbsp; </span>{" "}
+								<span>{` ${entry.name}`}</span>
+								<button className="ml-auto mr-4" onClick={handleVisibility}>
+									{entry.visible ? (
+										<FontAwesomeIcon icon={faEye} />
+									) : (
+										<FontAwesomeIcon icon={faEyeSlash} />
+									)}
+								</button>
+								<button className="relative bottom-1 text-base leading-5 text-gray-500">
+									<FontAwesomeIcon icon={faSortDown} />
+								</button>
+							</li>
+						))}
+					</ul>
 				</section>
 			</main>
 		</main>
