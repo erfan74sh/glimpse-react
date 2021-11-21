9707b4a8237e923863efdb2eed5b5f1f46e8f2a0
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-21 21:53:14 +0330

    create progress bar

--- src/components/progress-bar/ProgressBar.jsx
+++ src/components/progress-bar/ProgressBar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+// asset
+// import TickIcon from "../../assets/icons/tick.png";
 
 const ProgressBar = () => {
-	return <div>progress bar</div>;
+	return (
+		<div className="flex h-full flex-col items-center pt-9">
+			<div className="w-6 h-6 flex items-center content-center bg-blue-550 rounded-full text-center border-2 border-white">
+				<span className="mx-auto p-1 text-xs font-light text-white">1</span>
+			</div>
+			<span className="h-1/2 w-1 bg-gray-400"></span>
+			<div className="w-6 h-6 flex items-center content-center bg-blue-550 rounded-full text-center border-2 border-white">
+				<span className="mx-auto p-1 text-xs font-light text-white">2</span>
+			</div>
+			<span className="h-1/2 w-1 bg-gray-200"></span>
+			<div className="w-6 h-6 flex items-center content-center bg-blue-550 rounded-full text-center border-2 border-white">
+				<span className="mx-auto p-1 text-xs font-light text-white">3</span>
+			</div>
+		</div>
+	);
 };
 
 export default ProgressBar;
