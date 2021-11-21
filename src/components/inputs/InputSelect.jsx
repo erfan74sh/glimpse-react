88458ddcb4ef9ca5537c8f3bb8c2e5764964af62
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-21 16:50:09 +0330

    add selectedvalue state and handler/ get dropdown items from props

--- src/components/inputs/InputSelect.jsx
+++ src/components/inputs/InputSelect.jsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import CarretDown from "../../assets/icons/caret-down.svg";
 
 const InputSelect = ({ placeHolder, items }) => {
+	const [selectedValue, setSelectedValue] = useState("");
+	const handleSelectedValue = (e) => {
+		setSelectedValue(e.target.dataset.value);
+		setShowDropdown(false);
+	};
+
 	const [showDropdown, setShowDropdown] = useState(false);
 	const handleShowDropdown = () => {
 		setShowDropdown(!showDropdown);
@@ -11,7 +17,12 @@ const InputSelect = ({ placeHolder, items }) => {
 	return (
 		<div className="relative mr-8 px-4 py-3.5 rounded-md shadow-xl bg-white">
 			<div className="flex items-center justify-between">
-				<span>X- Dimention</span>
+				{selectedValue.length !== 0 ? (
+					<span>{selectedValue}</span>
+				) : (
+					<span className="text-gray-500">{placeHolder}</span>
+				)}
+
 				<button
 					type="button"
 					className="flex justify-center w-10 self-stretch items-center"
@@ -22,10 +33,17 @@ const InputSelect = ({ placeHolder, items }) => {
 			</div>
 			{showDropdown && (
 				<ul className="w-full absolute flex flex-col gap-y-3 top-0 left-0 px-4 py-3.5 transform translate-y-16 rounded-md shadow-md bg-white text-gray-400 z-10">
-					<li className="hover:text-black">item 1</li>
-					<li className="hover:text-black">item 2</li>
-					<li className="hover:text-black">item 3</li>
-					<li className="hover:text-black">item 4</li>
+					{items.map((item) => (
+						<li
+							className={`hover:text-black cursor-default ${
+								selectedValue === item ? "text-black" : ""
+							}`}
+							data-value={item}
+							onClick={handleSelectedValue}
+						>
+							{item}
+						</li>
+					))}
 				</ul>
 			)}
 		</div>
