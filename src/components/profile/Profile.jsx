9d3aab185940b73e44e37d1d3f840e49d3b205d6
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-21 19:31:52 +0330

    create profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+// asset
+import AvatarImg from "../../assets/avatars/avatar-01.png";
 
 const Profile = () => {
-	return <div>profile</div>;
+	return (
+		<button type="button" className="flex gap-x-6 items-center">
+			<span className="text-xl">User name</span>
+			<img
+				src={AvatarImg}
+				alt="avatr image"
+				className="w-10 h-10 object-cover"
+			/>
+		</button>
+	);
 };
 
 export default Profile;
