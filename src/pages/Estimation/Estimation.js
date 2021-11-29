bfdf2e7d97f6253fe7d2b6fe3e33b661c0482f5b
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 00:20:09 +0330

    import Nav comp from components folder

--- src/pages/Estimation/Estimation.js
+++ src/pages/Estimation/Estimation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
 // components
-import Nav from "./Nav";
+import Nav from "../../components/Nav";
 // style
 import "./Estimation.scss";
 
