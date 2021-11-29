9d58fd05a29fb493dcbec910ce1508af3b65a598
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-29 23:25:04 +0330

    create initial inputData state with fake values

---
+++ src/features/data/inputDataSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	data: {
+		xDim: 0,
+		yDim: 0,
+		rotation: 0,
+		wwrNorth: 0,
+		wwrSouth: 0,
+		shadingType: 1,
+		reflectanWall: 0,
+		reflectanCeiling: 0,
+		reflectanFloor: 0,
+		vtGlass: 0.58,
+		southNeighborDist: 3,
+		southNeighborHeight: 4,
+		northNeighborDist: 3,
+		northNeighborHeight: 4,
+	},
+};
+
+export const inputDataSlice = createSlice({
+	name: "input",
+	initialState,
+	reducers: {},
+});
+
+export const selectInput = (state) => state.input.data;
+export default inputDataSlice.reducer;
