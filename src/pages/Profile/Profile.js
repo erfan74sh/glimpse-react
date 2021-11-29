05a9820654d906b415d21fc60d9893aae572a01a
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 00:41:11 +0330

    create initial profile page

---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../../components/Nav";
+
+const Profile = () => {
+	return (
+		<div className="h-screen bg-gray-50">
+			<header className="">
+				<Nav />
+			</header>
+			<main>main</main>
+		</div>
+	);
+};
+
+export default Profile;
