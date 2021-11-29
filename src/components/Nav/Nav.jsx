25cd72346d7412c1a3e6f95b02482678d24d1e4b
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 00:19:49 +0330

    put Nav comp inside components folder

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// component
+import Profile from "../profile/Profile";
+// assets
+import Logo from "../../assets/images/logo-02.png";
+
+const Header = () => {
+	return (
+		<nav className="bg-gray-950 text-white">
+			<ul className="flex justify-between items-center px-9 py-5">
+				<li>
+					<Link to="/">
+						<img src={Logo} alt="glimpse logo" />
+					</Link>
+				</li>
+				<li>
+					<ul className="flex justify-items-center items-center gap-x-40">
+						<li>
+							<Link to="/">home</Link>
+						</li>
+						<li>
+							<Link to="/">simulations</Link>
+						</li>
+						<li>
+							<Link to="/">about us</Link>
+						</li>
+						<li>
+							<Link to="/">contact us</Link>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<Profile />
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Header;
