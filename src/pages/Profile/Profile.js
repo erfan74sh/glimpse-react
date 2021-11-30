9e92e6636e3d8c3bde9474ce533f52066bef8be9
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 16:27:09 +0330

    edit main section in frofile page/import child components

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Nav from "../../components/Nav";
+import ProfileCard from "./profileCard/ProfileCard";
+import ProfileInfo from "./profileInfo/ProfileInfo";
 
 const Profile = () => {
 	return (
@@ -7,7 +9,17 @@ const Profile = () => {
 			<header className="">
 				<Nav />
 			</header>
-			<main>main</main>
+			<main className="px-24 py-16">
+				<header className="mb-14">
+					<h1 className="pl-3 leading-8 border-l-8 border-blue-550 text-blue-550 text-3xl font-bold uppercase">
+						profile
+					</h1>
+				</header>
+				<main>
+					<ProfileCard />
+					<ProfileInfo />
+				</main>
+			</main>
 		</div>
 	);
 };
