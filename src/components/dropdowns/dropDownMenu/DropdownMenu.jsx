346598d3c62f5d3977fcd887cb29ceddb3e7a0a4
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 22:12:06 +0330

    initial creation

---
+++ src/components/dropdowns/dropDownMenu/DropdownMenu.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DropdownMenu = () => {
+	return <div>dropdown menu</div>;
+};
+
+export default DropdownMenu;
---
+++ src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DropdownProfile = () => {
+	return <div>dropdown</div>;
+};
+
+export default DropdownProfile;
