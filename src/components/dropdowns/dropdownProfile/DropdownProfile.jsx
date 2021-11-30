9c49aeef85afc047ae045e47c0a8597799387c63
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 22:59:45 +0330

    create profile dropdown

--- src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
+++ src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+//
+// assets
+import ProfileIcon from "../../../assets/icons/Profile.svg";
+import ChargeIcon from "../../../assets/icons/charge.svg";
+import SignOutIcon from "../../../assets/icons/sign-out.svg";
 
 const DropdownProfile = () => {
-	return <div>dropdown</div>;
+	return (
+		<ul className=" absolute flex flex-col px-3 text-xl text-gray-500 font-normal bg-white shadow-lg top-full right-0 w-max capitalize rounded-md transform translate-y-9">
+			<li className="flex gap-x-4 px-2 py-4">
+				<img src={ProfileIcon} alt="profile icon" />
+				<span>profile</span>
+			</li>
+			<li className="flex gap-x-4 px-2 py-4 border-b-2 border-t-2 border-gray-300">
+				<img src={ChargeIcon} alt="charge icon" />
+				<span>charge account</span>
+			</li>
+			<li className="flex gap-x-4 px-2 py-4">
+				<img src={SignOutIcon} alt="sign out icon" />
+				<span className="text-red-350">sign out</span>
+			</li>
+		</ul>
+	);
 };
 
 export default DropdownProfile;
