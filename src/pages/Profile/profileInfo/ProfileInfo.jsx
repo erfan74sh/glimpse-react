e95df1bb18ca74bf8c312ce3f99b4835ab19ce06
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 18:04:58 +0330

    complete information section

--- src/pages/Profile/profileInfo/ProfileInfo.jsx
+++ src/pages/Profile/profileInfo/ProfileInfo.jsx
@@ -15,7 +15,48 @@ const ProfileInfo = () => {
 						edit
 					</button>
 				</header>
-				<main></main>
+				<main className="flex flex-col gap-y-6 py-8 px-4">
+					<div className="flex gap-x-6 justify-between items-center">
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">first name</span>
+							<span>mohammad</span>
+						</div>
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">address</span>
+							<span>No 64. Farhangian st , Esfahan</span>
+						</div>
+					</div>
+					<div className="flex gap-x-6 justify-between items-center">
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">last name</span>
+							<span>rajaian</span>
+						</div>
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">last name</span>
+							<span>rajaian</span>
+						</div>
+					</div>
+					<div className="flex gap-x-6 justify-between items-center">
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">education</span>
+							<span>Master of Energy and architecture</span>
+						</div>
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">email</span>
+							<span>mrajaian@gmail.com</span>
+						</div>
+					</div>
+					<div className="flex gap-x-6 justify-between items-center">
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">position</span>
+							<span>Co_founder of more Office</span>
+						</div>
+						<div className="flex flex-col w-1/2 text-lg capitalize">
+							<span className="font-bold">phone</span>
+							<span>09361759625</span>
+						</div>
+					</div>
+				</main>
 			</section>
 			<section>
 				<header className="flex gap-x-4 pb-4 border-b-2 border-gray-400">
@@ -29,7 +70,7 @@ const ProfileInfo = () => {
 						edit
 					</button>
 				</header>
-				<main></main>
+				<main className="py-8 px-4"></main>
 			</section>
 		</section>
 	);
