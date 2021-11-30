bc29d18577a9fe14122faf362c8b63412c69fa41
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 20:26:16 +0330

    edit layout

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -7,7 +7,14 @@ const AboutUs = () => {
 			<header className="">
 				<Nav />
 			</header>
-			<main>main</main>
+			<main className="px-24 py-16">
+				<header className="mb-14">
+					<h1 className="pl-3 leading-8 border-l-8 border-blue-550 text-blue-550 text-3xl font-bold uppercase">
+						our team
+					</h1>
+				</header>
+				<main className="flex gap-x-12"></main>
+			</main>
 		</div>
 	);
 };
