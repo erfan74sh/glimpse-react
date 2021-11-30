2e69703531fabf22455cd5b5de2cf1423a789309
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 17:45:33 +0330

    create two main sections and layout

--- src/pages/Profile/profileInfo/ProfileInfo.jsx
+++ src/pages/Profile/profileInfo/ProfileInfo.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const ProfileInfo = () => {
-	return <div>profile info</div>;
+	return (
+		<section className="flex flex-col flex-grow gap-y-20">
+			<section>
+				<header className="flex gap-x-4 pb-4 border-b-2 border-gray-400">
+					<h3 className="text-gray-650 text-2xl font-bold capitalize">
+						information
+					</h3>
+					<button
+						type="button"
+						className="text-xl font-normal text-blue-550 border-b-2 border-blue-550 capitalize"
+					>
+						edit
+					</button>
+				</header>
+				<main></main>
+			</section>
+			<section>
+				<header className="flex gap-x-4 pb-4 border-b-2 border-gray-400">
+					<h3 className="text-gray-650 text-2xl font-bold capitalize">
+						information
+					</h3>
+					<button
+						type="button"
+						className="text-xl font-normal text-blue-550 border-b-2 border-blue-550 capitalize"
+					>
+						edit
+					</button>
+				</header>
+				<main></main>
+			</section>
+		</section>
+	);
 };
 
 export default ProfileInfo;
