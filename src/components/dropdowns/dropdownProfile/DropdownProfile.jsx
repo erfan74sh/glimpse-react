eea6a5cbfae6eee3b419000d3adf1f412a124d78
Erfan Shafiee <erfan74sh@gmail.com>
2021-11-30 23:26:07 +0330

    add route link to profileDropdown items

--- src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
+++ src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import ProfileIcon from "../../../assets/icons/Profile.svg";
 import ChargeIcon from "../../../assets/icons/charge.svg";
 import SignOutIcon from "../../../assets/icons/sign-out.svg";
+import { Link } from "react-router-dom";
 
 const DropdownProfile = ({ showDropdown }) => {
 	return (
@@ -12,17 +13,27 @@ const DropdownProfile = ({ showDropdown }) => {
 				showDropdown ? "max-h-screen" : "max-h-0"
 			} absolute flex flex-col px-3 text-xl text-gray-500 font-normal bg-white shadow-lg top-full right-0 w-max capitalize rounded-md transform translate-y-9 overflow-hidden transition-all`}
 		>
-			<li className="flex gap-x-4 px-2 py-4">
-				<img src={ProfileIcon} alt="profile icon" />
-				<span>profile</span>
+			<li>
+				<Link to="/profile" className="flex gap-x-4 px-2 py-4">
+					<img src={ProfileIcon} alt="profile icon" />
+					<span>profile</span>
+				</Link>
 			</li>
-			<li className="flex gap-x-4 px-2 py-4 border-b-2 border-t-2 border-gray-300">
-				<img src={ChargeIcon} alt="charge icon" />
-				<span>charge account</span>
+			<li>
+				<Link
+					to="#"
+					className="flex gap-x-4 px-2 py-4 border-b-2 border-t-2 border-gray-300"
+				>
+					<img src={ChargeIcon} alt="charge icon" />
+					<span>charge account</span>
+				</Link>
 			</li>
-			<li className="flex gap-x-4 px-2 py-4">
-				<img src={SignOutIcon} alt="sign out icon" />
-				<span className="text-red-350">sign out</span>
+
+			<li>
+				<Link to="#" className="flex gap-x-4 px-2 py-4">
+					<img src={SignOutIcon} alt="sign out icon" />
+					<span className="text-red-350">sign out</span>
+				</Link>
 			</li>
 		</ul>
 	);
