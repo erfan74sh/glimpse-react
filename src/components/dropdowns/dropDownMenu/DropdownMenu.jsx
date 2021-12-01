1eb8951fbf36bf80d4e98219aa780d9d475170f3
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-01 20:22:42 +0330

    map throgh dropdown items/edit style of dropdown

--- src/components/dropdowns/dropDownMenu/DropdownMenu.jsx
+++ src/components/dropdowns/dropDownMenu/DropdownMenu.jsx
@@ -2,20 +2,23 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const DropdownMenu = () => {
+	const items = [
+		{ title: "Energy and Thermal Comfort", path: "/" },
+		{ title: "Daylight", path: "/" },
+		{ title: "Structure", path: "/" },
+		{ title: "Fire in building", path: "/" },
+		{ title: "Acoustic", path: "/" },
+	];
+
 	return (
-		<ul className="absolute flex flex-col px-3 text-xl text-gray-500 font-normal bg-white shadow-lg top-full right-0 w-max capitalize rounded-md transform translate-y-9 overflow-hidden transition-all">
-			<li className="px-2 py-4">
-				<Link to="/">first item</Link>
-			</li>
-			<li className="px-2 py-4">
-				<Link to="/">first item</Link>
-			</li>
-			<li className="px-2 py-4">
-				<Link to="/">first item</Link>
-			</li>
-			<li className="px-2 py-4">
-				<Link to="/">first item</Link>
-			</li>
+		<ul className="absolute flex flex-col px-3 text-xl text-gray-500 font-normal bg-white shadow-lg top-full right-0 w-max capitalize rounded-md transform translate-y-9 overflow-hidden transition-all z-10 divide-y-2 divide-gray-300">
+			{items.map((item, idx) => {
+				return (
+					<li className="px-2 py-4 " key={idx}>
+						<Link to={item.path}>{item.title}</Link>
+					</li>
+				);
+			})}
 		</ul>
 	);
 };
