44f2e339c278f5d9f71bd2170b859c52cbdc7b13
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-01 20:33:00 +0330

    change visibility of dropdown on hover

--- src/components/dropdowns/dropDownMenu/DropdownMenu.jsx
+++ src/components/dropdowns/dropDownMenu/DropdownMenu.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const DropdownMenu = () => {
+const DropdownMenu = ({ showDropdown }) => {
 	const items = [
 		{ title: "Energy and Thermal Comfort", path: "/" },
 		{ title: "Daylight", path: "/" },
@@ -11,7 +11,11 @@ const DropdownMenu = () => {
 	];
 
 	return (
-		<ul className="absolute flex flex-col px-3 text-xl text-gray-500 font-normal bg-white shadow-lg top-full right-0 w-max capitalize rounded-md transform translate-y-9 overflow-hidden transition-all z-10 divide-y-2 divide-gray-300">
+		<ul
+			className={`${
+				showDropdown ? "max-h-screen" : "max-h-0"
+			} absolute flex flex-col px-3 text-xl text-gray-500 font-normal bg-white shadow-lg top-full right-0 w-max capitalize rounded-md transform translate-y-9 overflow-hidden transition-all z-10 divide-y-2 divide-gray-300`}
+		>
 			{items.map((item, idx) => {
 				return (
 					<li className="px-2 py-4 " key={idx}>
