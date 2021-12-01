55aecedba804989c28416a3d38cc7d4d7ebd80b0
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-01 20:00:10 +0330

    add dropdown items and styling

--- src/components/dropdowns/dropDownMenu/DropdownMenu.jsx
+++ src/components/dropdowns/dropDownMenu/DropdownMenu.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const DropdownMenu = () => {
-	return <div>dropdown menu</div>;
+	return (
+		<ul className="absolute flex flex-col px-3 text-xl text-gray-500 font-normal bg-white shadow-lg top-full right-0 w-max capitalize rounded-md transform translate-y-9 overflow-hidden transition-all">
+			<li className="px-2 py-4">
+				<Link to="/">first item</Link>
+			</li>
+			<li className="px-2 py-4">
+				<Link to="/">first item</Link>
+			</li>
+			<li className="px-2 py-4">
+				<Link to="/">first item</Link>
+			</li>
+			<li className="px-2 py-4">
+				<Link to="/">first item</Link>
+			</li>
+		</ul>
+	);
 };
 
 export default DropdownMenu;
