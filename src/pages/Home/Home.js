c0096603970f857eafb32e089768f627501a599a
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-02 12:30:57 +0330

    add navbar to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+// components
+import Nav from "../../components/Nav";
 // style
 import "./Home.scss";
 
 const Home = () => {
-	return <div>home page</div>;
+	return (
+		<>
+			<header className="">
+				<Nav />
+			</header>
+		</>
+	);
 };
 
 export default Home;
