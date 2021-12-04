d91d6576c5bb236df613a74898a44e405f28fc57
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-04 21:27:40 +0330

    control tracker height based on step prop

--- src/components/progress-bar/ProgressBar.jsx
+++ src/components/progress-bar/ProgressBar.jsx
@@ -2,21 +2,23 @@ import React from "react";
 // asset
 import TickIcon from "../../assets/icons/tick.png";
 
-const ProgressBar = () => {
+const ProgressBar = ({ step }) => {
 	return (
 		<div className="flex h-full flex-col items-center justify-between relative">
 			<div className="absolute top-0 left-1/2 w-1 h-full bg-gray-200 transform -translate-x-1/2"></div>
-			<div className="absolute top-0 left-1/2 w-1 h-1/2 bg-gray-400 transform -translate-x-1/2"></div>
+			<div
+				className={`absolute top-0 left-1/2 w-1 ${
+					step === "geometry" ? "h-0" : step === "material" ? "h-1/2" : "h-full"
+				} bg-gray-400 transform -translate-x-1/2 transition-all`}
+			></div>
 			<div className="w-6 h-6 flex items-center justify-center bg-blue-550 rounded-full text-center border-2 border-white z-10">
 				<span className="text-xs font-light text-white">1</span>
 			</div>
-			{/* <span className="h-1/2 w-1 flex-shrink bg-gray-400"></span> */}
 			<div className="w-6 h-6 flex items-center justify-center bg-blue-550 rounded-full text-center border-2 border-white z-10">
 				<span className="text-xs font-light text-white">
 					<img src={TickIcon} alt="tick icon" />
 				</span>
 			</div>
-			{/* <span className="h-1/2 w-1 flex-shrink bg-gray-200"></span> */}
 			<div className="w-6 h-6 flex items-center justify-center bg-blue-550 rounded-full text-center border-2 border-white z-10">
 				<span className="text-xs font-light text-white">3</span>
 			</div>
