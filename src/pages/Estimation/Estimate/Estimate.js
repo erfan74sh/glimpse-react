5b827de2ace3526a29df13e2b4d649712c4882e3
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-04 19:19:04 +0330

    create step handler

--- src/pages/Estimation/Estimate/Estimate.js
+++ src/pages/Estimation/Estimate/Estimate.js
@@ -18,6 +18,30 @@ import ArrowLeft from "../../../assets/images/arrow-left.svg";
 const Estimate = () => {
 	const inputData = useSelector(selectInput);
 	const [step, setStep] = useState("geometry");
+	const handleStep = (currntStep, direction = "prev") => {
+		if (direction === "next") {
+			switch (currntStep) {
+				case "geometry":
+					setStep("material");
+					break;
+				case "material":
+					setStep("site plan");
+					break;
+				default:
+					return;
+			}
+		} else {
+			switch (currntStep) {
+				case "material":
+					setStep("geometry");
+					break;
+				case "site plan":
+					setStep("material");
+				default:
+					return;
+			}
+		}
+	};
 	return (
 		<main className=" px-24 py-16" id="estimate__main">
 			<header className="mb-14">
