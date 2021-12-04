c92400cc0ec90900b3de88d1f61b2b8178c21f8f
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-04 21:48:48 +0330

    edit placeholders

--- src/pages/Estimation/Estimate/FormSteps/SitePlan/SitePlan.jsx
+++ src/pages/Estimation/Estimate/FormSteps/SitePlan/SitePlan.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+// components
+import InputSelect from "../../../../../components/inputs/InputSelect";
 
 const SitePlan = () => {
-	return <div>site plan</div>;
+	return (
+		<fieldset className="flex flex-col gap-y-9 pr-10">
+			<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+				site plan
+			</legend>
+			<InputSelect
+				placeHolder="South neighbor distance"
+				items={["item1", "item2", "item3"]}
+			/>
+			<InputSelect
+				placeHolder="South neighbor height"
+				items={["item1", "item2", "item3"]}
+			/>
+			<InputSelect
+				placeHolder="North neighbor distance"
+				items={["item1", "item2", "item3"]}
+			/>
+			<InputSelect
+				placeHolder="North neighbor height"
+				items={["item1", "item2", "item3"]}
+			/>
+		</fieldset>
+	);
 };
 
 export default SitePlan;
