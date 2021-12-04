f1c629892d965997c441765c98dcfbee9a14a118
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-04 20:06:23 +0330

    create material component

---
+++ src/pages/Estimation/Estimate/FormSteps/Material/Material.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+// components
+import InputSelect from "../../../../../components/inputs/InputSelect";
+
+const Material = () => {
+	return (
+		<fieldset className="flex flex-col gap-y-9 pr-10">
+			<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+				material
+			</legend>
+			<InputSelect
+				placeHolder="Wall-material"
+				items={["item1", "item2", "item3"]}
+			/>
+			<InputSelect
+				placeHolder="placeholder"
+				items={["item1", "item2", "item3"]}
+			/>
+			<InputSelect
+				placeHolder="placeholder"
+				items={["item1", "item2", "item3"]}
+			/>
+			<InputSelect
+				placeHolder="placeholder"
+				items={["item1", "item2", "item3"]}
+			/>
+			<InputSelect
+				placeHolder="placeholder"
+				items={["item1", "item2", "item3"]}
+			/>
+		</fieldset>
+	);
+};
+
+export default Material;
