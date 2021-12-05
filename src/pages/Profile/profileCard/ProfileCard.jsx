f16cf5c622147bffdeeaaedab00b34efb60df0cf
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-06 00:04:25 +0330

    use userInfo state for content

--- src/pages/Profile/profileCard/ProfileCard.jsx
+++ src/pages/Profile/profileCard/ProfileCard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // assets
 import ProfilePic from "../../../assets/profile/01.png";
 import Sucsess from "../../../assets/icons/sucsess.png";
+// states
+import { selectUserInfo } from "../../../features/userInfo/UserInfoSlice";
 
 const ProfileCard = () => {
+	const userInfo = useSelector(selectUserInfo);
+
 	return (
 		<div className="flex flex-col justify-between items-center w-113 p-14 pb-6 rounded-md bg-white shadow-full">
 			<img
@@ -12,12 +17,12 @@ const ProfileCard = () => {
 				className="w-48 h-48 object-cover overflow-hidden rounded-full mb-16"
 			/>
 			<div className="flex flex-col items-center gap-y-5 mb-12 text-xl text-gray-650 font-normal">
-				<h3 className="mb-5 text-3xl font-bold capitalize">mohammad rajaian</h3>
+				<h3 className="mb-5 text-3xl font-bold capitalize">{`${userInfo.firstName} ${userInfo.lastName}`}</h3>
 				<span className="px-10 text-center capitalize">
-					master of energy and architecture
+					{userInfo.education}
 				</span>
-				<span>mrajaian@gmail.com</span>
-				<span>09361759625</span>
+				<span>{userInfo.email}</span>
+				<span>{userInfo.phone}</span>
 			</div>
 			<img src={Sucsess} alt="sucsess icon" />
 		</div>
