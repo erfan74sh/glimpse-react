e770cf84f4ce7f5bcfcee6a3cbfcb9395a4f2e8f
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-05 19:51:47 +0330

    add userInfoSlice to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import inputReducer from "../features/data/inputDataSlice";
+import userInfoSlice from "../features/userInfo/UserInfoSlice";
 
 export const store = configureStore({
 	reducer: {
 		input: inputReducer,
+		userInfo: userInfoSlice,
 	},
 });
