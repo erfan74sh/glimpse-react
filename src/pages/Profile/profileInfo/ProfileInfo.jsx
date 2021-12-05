fe470c8cb5721bda0fb471d26dfd332f1b9f6966
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-05 09:55:38 +0330

    change layout of profile information to flex-row

--- src/pages/Profile/profileInfo/ProfileInfo.jsx
+++ src/pages/Profile/profileInfo/ProfileInfo.jsx
@@ -15,43 +15,35 @@ const ProfileInfo = () => {
 						edit
 					</button>
 				</header>
-				<main className="flex flex-col gap-y-6 py-8 px-4">
-					<div className="flex gap-x-6 justify-between items-center">
-						<div className="flex flex-col w-1/2 text-lg capitalize">
+				<main className="flex gap-x-6 py-8 px-4">
+					<div className="w-1/2 flex flex-col gap-y-6">
+						<div className="flex flex-col text-lg capitalize">
 							<span className="font-bold">first name</span>
 							<span>mohammad</span>
 						</div>
-						<div className="flex flex-col w-1/2 text-lg capitalize">
-							<span className="font-bold">address</span>
-							<span>No 64. Farhangian st , Esfahan</span>
-						</div>
-					</div>
-					<div className="flex gap-x-6 justify-between items-center">
-						<div className="flex flex-col w-1/2 text-lg capitalize">
+						<div className="flex flex-col text-lg capitalize">
 							<span className="font-bold">last name</span>
 							<span>rajaian</span>
 						</div>
-						<div className="flex flex-col w-1/2 text-lg capitalize">
-							<span className="font-bold">last name</span>
-							<span>rajaian</span>
-						</div>
-					</div>
-					<div className="flex gap-x-6 justify-between items-center">
-						<div className="flex flex-col w-1/2 text-lg capitalize">
+						<div className="flex flex-col text-lg capitalize">
 							<span className="font-bold">education</span>
 							<span>Master of Energy and architecture</span>
 						</div>
-						<div className="flex flex-col w-1/2 text-lg capitalize">
-							<span className="font-bold">email</span>
-							<span>mrajaian@gmail.com</span>
-						</div>
-					</div>
-					<div className="flex gap-x-6 justify-between items-center">
-						<div className="flex flex-col w-1/2 text-lg capitalize">
+						<div className="flex flex-col text-lg capitalize">
 							<span className="font-bold">position</span>
 							<span>Co_founder of more Office</span>
 						</div>
-						<div className="flex flex-col w-1/2 text-lg capitalize">
+					</div>
+					<div className="w-1/2 flex flex-col gap-y-6">
+						<div className="flex flex-col text-lg capitalize">
+							<span className="font-bold">address</span>
+							<span>No 64. Farhangian st , Esfahan</span>
+						</div>
+						<div className="flex flex-col text-lg capitalize">
+							<span className="font-bold">email</span>
+							<span>mrajaian@gmail.com</span>
+						</div>
+						<div className="flex flex-col text-lg capitalize">
 							<span className="font-bold">phone</span>
 							<span>09361759625</span>
 						</div>
