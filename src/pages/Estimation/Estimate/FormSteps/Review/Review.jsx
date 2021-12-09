135ed77a83e843adf311a303d517bfb6a0432bb8
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-09 13:51:21 +0330

    add buttons and style layout

--- src/pages/Estimation/Estimate/FormSteps/Review/Review.jsx
+++ src/pages/Estimation/Estimate/FormSteps/Review/Review.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const Review = () => {
-	return <div>review</div>;
+const Review = ({ handleStep }) => {
+	const handlePrevStep = (e) => {
+		handleStep(e);
+	};
+
+	return (
+		<>
+			<fieldset className="flex flex-col gap-y-9 pr-10">
+				<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+					review
+				</legend>
+			</fieldset>
+			<section className="flex justify-center gap-x-4 mt-auto mr-8 pr-10">
+				<button
+					type="button"
+					className="flex items-center gap-x-1 px-6 py-2 text-white font-medium uppercase rounded-md bg-blue-550"
+					onClick={handlePrevStep}
+					value="site plan"
+				>
+					prev <span className="text-xs lowercase">(SitePlan)</span>
+				</button>
+			</section>
+		</>
+	);
 };
 
 export default Review;
