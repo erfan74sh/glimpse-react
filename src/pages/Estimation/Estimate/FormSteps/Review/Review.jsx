f77bdb01953dc57cb99e0ec2e64c342a023c5b1b
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-09 14:58:26 +0330

    create list of input values and style review component

--- src/pages/Estimation/Estimate/FormSteps/Review/Review.jsx
+++ src/pages/Estimation/Estimate/FormSteps/Review/Review.jsx
@@ -7,15 +7,95 @@ const Review = ({ handleStep }) => {
 
 	return (
 		<>
-			<fieldset className="flex flex-col gap-y-9 pr-10">
-				<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+			<section className="flex flex-col pr-10">
+				<h2 className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
 					review
-				</legend>
-			</fieldset>
+				</h2>
+				<ul className="flex flex-col gap-y-2">
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							X-dimention:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							Y-dimention:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							WWR-north:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							WWR-south:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							shading type:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							wall material:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							ceiling material:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							floor material:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							glass material:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							south neighbor distance:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							south neighbor height:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							north neighbor distance:
+						</span>
+						<span>calue</span>
+					</li>
+					<li className="flex justify-between border-b border-gray-400 text-gray-650">
+						<span className="text-gray-900 font-semibold capitalize">
+							north neighbor height:
+						</span>
+						<span>calue</span>
+					</li>
+				</ul>
+			</section>
 			<section className="flex justify-center gap-x-4 mt-auto mr-8 pr-10">
 				<button
 					type="button"
-					className="flex items-center gap-x-1 px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
+					className="flex items-center justify-between px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
 					onClick={handlePrevStep}
 					value="site plan"
 				>
