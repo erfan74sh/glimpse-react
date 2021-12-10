c26d113d0ec6d08b7e622b1df79b5e1f8d6c2324
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-10 13:48:45 +0330

    create value state and handleValue fn

--- src/components/inputs/InputRange/InputRange.jsx
+++ src/components/inputs/InputRange/InputRange.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputRange = ({ min, max, step }) => {
+	const initialValue = (Number(max) - Number(min)) / 2 + Number(min);
+	const [value, setValue] = useState(initialValue);
+
+	const handleValue = (e) => {
+		setValue(e.target.value);
+	};
+
 	return (
 		<div className="mr-8">
 			<input
@@ -9,7 +16,7 @@ const InputRange = ({ min, max, step }) => {
 				max={max}
 				step={step}
 				// value="16"
-				defaultValue={(Number(max) - Number(min)) / 2 + Number(min)}
+				defaultValue={value}
 				className="w-full"
 			/>
 			<span>value</span>
