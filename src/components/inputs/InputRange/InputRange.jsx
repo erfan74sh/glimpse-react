83c2564f39a1774af5b96656d18db0c8fce9b3c4
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-10 13:46:13 +0330

    create input range and span for display value

--- src/components/inputs/InputRange/InputRange.jsx
+++ src/components/inputs/InputRange/InputRange.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const InputRange = () => {
-	return <div>input range</div>;
+const InputRange = ({ min, max, step }) => {
+	return (
+		<div className="mr-8">
+			<input
+				type="range"
+				min={min}
+				max={max}
+				step={step}
+				// value="16"
+				defaultValue={(Number(max) - Number(min)) / 2 + Number(min)}
+				className="w-full"
+			/>
+			<span>value</span>
+		</div>
+	);
 };
 
 export default InputRange;
