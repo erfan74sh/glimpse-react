cc7e9f190d33407d15e21c75b3506b4ebb82b11c
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-12 23:12:19 +0330

    create 2 side of 3d model

--- src/pages/Estimation/Estimate/3D/_3D.jsx
+++ src/pages/Estimation/Estimate/3D/_3D.jsx
@@ -1,11 +1,16 @@
 import React, { useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, TransformControls } from "@react-three/drei";
 import { useSpring, animated } from "@react-spring/three";
+// state
+import { selectInput } from "../../../../features/data/inputDataSlice";
 
-const Box = () => {
+const Box = ({ xDim, yDim, position, rotation }) => {
+	// const data = useSelector(selectInput);
 	const mesh = useRef(null);
-
+	let str = Math.PI * 2;
+	console.log(str);
 	// const [expand, setExpand] = useState(false);
 
 	// const { scale } = useSpring({ scale: expand ? [1.5, 1.5, 1.5] : [1, 1, 1] });
@@ -15,36 +20,46 @@ const Box = () => {
 		// 	<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
 		// 	<meshStandardMaterial attach="material" color="blue" />
 		// </animated.mesh>
-		<mesh ref={mesh}>
-			<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-			<meshStandardMaterial attach="material" color="blue" />
+		<mesh ref={mesh} rotation={rotation} position={position} scale={[1, 1, 1]}>
+			<ringBufferGeometry attach="geometry" args={[2 / 5, 3 / 5, 4]} />
+			<meshStandardMaterial attach="material" color="#4d6ab9" />
 		</mesh>
 	);
 };
 
-const _3D = ({ dimentions }) => {
-	console.log(dimentions);
+const _3D = ({ xDim, yDim }) => {
 	return (
 		<Canvas
 			colorManagement
 			camera={{
 				position: [2, 2, 2],
-				zoom: 100,
-				near: 1,
-				far: 1000,
-				top: 100,
-				bottom: -100,
-				right: 100,
-				left: -100,
+				zoom: 220,
+				near: 0.01,
+				far: 10000,
+				top: 500,
+				bottom: -500,
+				right: 500,
+				left: -500,
 			}}
 			orthographic
 		>
 			<ambientLight intensity={0.3} />
 			<directionalLight position={[0, 10, 5]} intensity={1.5} />
-			<TransformControls mode="scale">
-				<Box dimentions={dimentions} />
-			</TransformControls>
-			<OrbitControls />
+			<Box
+				xDim={xDim}
+				yDim={yDim}
+				position={[0, 0, 0]}
+				rotation={[0, 0, Math.PI / 4]}
+			/>
+			<Box
+				xDim={xDim}
+				yDim={yDim}
+				position={[0.5, 0, -0.5]}
+				rotation={[0, Math.PI / 2, Math.PI / 4]}
+			/>
+			{/* <TransformControls mode="scale">
+			</TransformControls> */}
+			{/* <OrbitControls /> */}
 		</Canvas>
 	);
 };
