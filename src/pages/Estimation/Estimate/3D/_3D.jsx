e87d69d115a9cc56d32dfd8299183fdf38e91c86
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-13 19:13:07 +0330

    create a box

--- src/pages/Estimation/Estimate/3D/_3D.jsx
+++ src/pages/Estimation/Estimate/3D/_3D.jsx
@@ -9,8 +9,7 @@ import { selectInput } from "../../../../features/data/inputDataSlice";
 const Box = ({ xDim, yDim, position, rotation }) => {
 	// const data = useSelector(selectInput);
 	const mesh = useRef(null);
-	let str = Math.PI * 2;
-	console.log(str);
+	console.log(xDim, yDim);
 	// const [expand, setExpand] = useState(false);
 
 	// const { scale } = useSpring({ scale: expand ? [1.5, 1.5, 1.5] : [1, 1, 1] });
@@ -20,8 +19,11 @@ const Box = ({ xDim, yDim, position, rotation }) => {
 		// 	<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
 		// 	<meshStandardMaterial attach="material" color="blue" />
 		// </animated.mesh>
-		<mesh ref={mesh} rotation={rotation} position={position} scale={[1, 1, 1]}>
-			<ringBufferGeometry attach="geometry" args={[2 / 5, 3 / 5, 4]} />
+		<mesh ref={mesh}>
+			<boxBufferGeometry
+				attach="geometry"
+				args={[xDim / 10, 3.5 / 10, yDim / 10]}
+			/>
 			<meshStandardMaterial attach="material" color="#4d6ab9" />
 		</mesh>
 	);
@@ -33,7 +35,7 @@ const _3D = ({ xDim, yDim }) => {
 			colorManagement
 			camera={{
 				position: [2, 2, 2],
-				zoom: 220,
+				zoom: 210,
 				near: 0.01,
 				far: 10000,
 				top: 500,
@@ -45,18 +47,8 @@ const _3D = ({ xDim, yDim }) => {
 		>
 			<ambientLight intensity={0.3} />
 			<directionalLight position={[0, 10, 5]} intensity={1.5} />
-			<Box
-				xDim={xDim}
-				yDim={yDim}
-				position={[0, 0, 0]}
-				rotation={[0, 0, Math.PI / 4]}
-			/>
-			<Box
-				xDim={xDim}
-				yDim={yDim}
-				position={[0.5, 0, -0.5]}
-				rotation={[0, Math.PI / 2, Math.PI / 4]}
-			/>
+			<Box xDim={xDim} yDim={yDim} />
+
 			{/* <TransformControls mode="scale">
 			</TransformControls> */}
 			{/* <OrbitControls /> */}
