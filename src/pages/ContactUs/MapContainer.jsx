6666b742a4fca714cc323bb073e6a1efe4c09770
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-14 13:47:04 +0330

    add shahid beheshti location to map

--- src/pages/ContactUs/MapContainer.jsx
+++ src/pages/ContactUs/MapContainer.jsx
@@ -4,9 +4,15 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 export class MapContainer extends Component {
 	render() {
 		return (
-			<Map google={this.props.google} zoom={14}>
+			<Map
+				google={this.props.google}
+				zoom={15}
+				initialCenter={{
+					lat: 35.79911916713814,
+					lng: 51.394691793335404,
+				}}
+			>
 				<Marker onClick={this.onMarkerClick} name={"Current location"} />
-
 				<InfoWindow onClose={this.onInfoWindowClose}>
 					<div>{/* <h1>{this.state.selectedPlace.name}</h1> */}</div>
 				</InfoWindow>
