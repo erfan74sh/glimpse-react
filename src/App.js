bd621ee920b9362850f9ede3162ad0da0fe74802
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-14 21:01:54 +0330

    add SignIn and SignUp components as child in "auth" route

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 // pages
 import Home from "./pages/Home";
+// path: estimation
 import Estimation from "./pages/Estimation";
 import Result from "./pages/Estimation/Result";
 import Estimate from "./pages/Estimation/Estimate";
+//
 import Profile from "./pages/Profile";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import PricingPlans from "./pages/PricingPlans";
+// path: auth
 import SignInSignUp from "./pages/signIn-signUp";
+import SignIn from "./pages/signIn-signUp/SignIn";
+import SignUp from "./pages/signIn-signUp/SignUp";
 // style
 import "./App.scss";
 
@@ -26,7 +31,11 @@ function App() {
 			<Route path="about-us" element={<AboutUs />} />
 			<Route path="contact-us" element={<ContactUs />} />
 			<Route path="pricing" element={<PricingPlans />} />
-			<Route path="auth" element={<SignInSignUp />}></Route>
+			<Route path="auth" element={<SignInSignUp />}>
+				<Route index element={<SignIn />} />
+				<Route path="sign-in" element={<SignIn />} />
+				<Route path="sign-up" element={<SignUp />} />
+			</Route>
 		</Routes>
 	);
 }
