98481fbf5f28585b74eb512acbe4a085436ce5d4
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-14 21:32:41 +0330

    style form input and container

--- src/pages/signIn-signUp/SignUp/SignUp.jsx
+++ src/pages/signIn-signUp/SignUp/SignUp.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+// assets
+import Logo from "../../../assets/images/logo-02.png";
 
 const SignUp = () => {
 	return (
-		<form>
-			<input type="text" name="first-name" placeholder="firstname" />
-			<input type="text" name="last-name" placeholder="lastname" />
-			<input type="email" name="email" placeholder="email" />
-			<input type="password" name="password" placeholder="password" />
-			<button type="submit">submit</button>
-		</form>
+		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
+			<img src={Logo} alt="logo image" className="w-60 h-auto" />
+			<form className="flex flex-col items-center gap-y-4 ">
+				<input
+					type="text"
+					name="first-name"
+					placeholder="firstname"
+					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
+				/>
+				<input
+					type="text"
+					name="last-name"
+					placeholder="lastname"
+					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
+				/>
+				<input
+					type="email"
+					name="email"
+					placeholder="email"
+					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
+				/>
+				<input
+					type="password"
+					name="password"
+					placeholder="password"
+					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
+				/>
+				<button type="submit">submit</button>
+			</form>
+		</div>
 	);
 };
 
