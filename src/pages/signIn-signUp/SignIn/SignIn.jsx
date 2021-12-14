f1d17b3ab474f46361c0aac3673a4d272e38fe9d
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-14 21:39:16 +0330

    create form and link to sign-up

--- src/pages/signIn-signUp/SignIn/SignIn.jsx
+++ src/pages/signIn-signUp/SignIn/SignIn.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// assets
+import Logo from "../../../assets/images/logo-02.png";
 
 const SignIn = () => {
-	return <div>sign in</div>;
+	return (
+		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
+			<img src={Logo} alt="logo image" className="w-60 h-auto" />
+			<form className="flex flex-col items-center gap-y-4 ">
+				<input
+					type="text"
+					name="username"
+					placeholder="username"
+					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
+				/>
+				<input
+					type="password"
+					name="password"
+					placeholder="password"
+					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
+				/>
+				<button type="submit">submit</button>
+			</form>
+			<span className="text-lg font-normal text-blue-550">
+				or{" "}
+				<Link to="sign-up" className="underline">
+					sign up
+				</Link>{" "}
+			</span>
+		</div>
+	);
 };
 
 export default SignIn;
