59ef0e6113c083bdc6214419f09a6441627dcdab
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-14 14:52:21 +0330

    add all pricing plans

--- src/pages/PricingPlans/PricingPlans.js
+++ src/pages/PricingPlans/PricingPlans.js
@@ -5,6 +5,7 @@ import Nav from "../../components/Nav";
 import GoldBadge from "../../assets/images/badge-gold.png";
 import SilverBadge from "../../assets/images/badge-silver.png";
 import BronzeBadge from "../../assets/images/badge-bronze.png";
+import { Link } from "react-router-dom";
 
 const PricingPlans = () => {
 	return (
@@ -21,7 +22,49 @@ const PricingPlans = () => {
 					</h1>
 				</header>
 				<section>
-					<article></article>
+					<article>
+						<img src={SilverBadge} alt="gold badge" />
+						<h3>silver</h3>
+						<ul>
+							<li>15 Calculation </li>
+							<li>20 Revision</li>
+							<li>Numerical Results</li>
+							<li>Simulation</li>
+							<li>Rhino And Revit</li>
+							<li>Exports</li>
+						</ul>
+						<button type="button">
+							<Link to="#">buy now</Link>
+						</button>
+					</article>
+					<article>
+						<img src={GoldBadge} alt="gold badge" />
+						<h3>gold</h3>
+						<ul>
+							<li>30 Calculation </li>
+							<li>45 Revision</li>
+							<li>Numerical Results</li>
+							<li>Simulation</li>
+							<li>Rhino And Revit</li>
+							<li> Grasshopper Exports</li>
+							<li>Design Suggestions</li>
+						</ul>
+						<button type="button">
+							<Link to="#">buy now</Link>
+						</button>
+					</article>
+					<article>
+						<img src={BronzeBadge} alt="gold badge" />
+						<h3>bronze</h3>
+						<ul>
+							<li>2 Calculation </li>
+							<li>4 Revision</li>
+							<li>Numerical Results</li>
+						</ul>
+						<button type="button">
+							<Link to="#">buy now</Link>
+						</button>
+					</article>
 				</section>
 			</main>
 		</>
