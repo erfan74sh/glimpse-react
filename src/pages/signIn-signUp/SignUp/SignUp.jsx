6df62a81d6044bd167c506abe807d39dbe72fc85
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-14 21:06:12 +0330

    create sign-up form

--- src/pages/signIn-signUp/SignUp/SignUp.jsx
+++ src/pages/signIn-signUp/SignUp/SignUp.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SignUp = () => {
-	return <div>sign up</div>;
+	return (
+		<form>
+			<input type="text" name="first-name" placeholder="firstname" />
+			<input type="text" name="last-name" placeholder="lastname" />
+			<input type="email" name="email" placeholder="email" />
+			<input type="password" name="password" placeholder="password" />
+			<button type="submit">submit</button>
+		</form>
+	);
 };
 
 export default SignUp;
