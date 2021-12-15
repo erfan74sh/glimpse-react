d60a4f11ebcecc27566419c6fe9c331533bc3d9f
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-15 22:09:08 +0330

    replace SignInForm.jsx to hardcoded form

--- src/pages/signIn-signUp/SignIn/SignIn.jsx
+++ src/pages/signIn-signUp/SignIn/SignIn.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// components
+import SignInForm from "./form";
 // assets
 import Logo from "../../../assets/images/logo-02.png";
 
@@ -7,21 +9,7 @@ const SignIn = () => {
 	return (
 		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
 			<img src={Logo} alt="logo image" className="w-60 h-auto" />
-			<form className="flex flex-col items-center gap-y-4 ">
-				<input
-					type="text"
-					name="username"
-					placeholder="username"
-					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
-				/>
-				<input
-					type="password"
-					name="password"
-					placeholder="password"
-					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
-				/>
-				<button type="submit">Log in</button>
-			</form>
+			<SignInForm />
 			<Link to="#" className="text-sm text-gray-650 font-normal capitalize">
 				forget password?
 			</Link>
