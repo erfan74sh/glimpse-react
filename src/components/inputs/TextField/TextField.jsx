c21aa2084dc6ff9962f13e98dc31d13460fe0e35
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-15 20:41:16 +0330

    edit position of error message

--- src/components/inputs/TextField/TextField.jsx
+++ src/components/inputs/TextField/TextField.jsx
@@ -11,7 +11,7 @@ const TextField = ({ ...props }) => {
 				className="px-4 py-3 border border-gray-300 rounded-md outline-none"
 			/>
 			{meta.touched && meta.error ? (
-				<span className="absolute bottom-0 left-0 transform translate-y-full text-xs text-red-600">
+				<span className="absolute bottom-0 left-1 transform translate-y-full text-xs text-red-600">
 					{meta.error}
 				</span>
 			) : null}
