d31b552bb4e54568396a9a8f8c783499a3328ec5
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-15 20:37:52 +0330

    create error message and style it

--- src/components/inputs/TextField/TextField.jsx
+++ src/components/inputs/TextField/TextField.jsx
@@ -4,13 +4,18 @@ import { useField } from "formik";
 const TextField = ({ ...props }) => {
 	const [field, meta] = useField(props);
 	return (
-		<>
+		<div className="relative">
 			<input
 				{...field}
 				{...props}
 				className="px-4 py-3 border border-gray-300 rounded-md outline-none"
 			/>
-		</>
+			{meta.touched && meta.error ? (
+				<span className="absolute bottom-0 left-0 transform translate-y-full text-xs text-red-600">
+					{meta.error}
+				</span>
+			) : null}
+		</div>
 	);
 };
 
