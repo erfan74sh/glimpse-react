b1de0250474f88371eebe42cd73d8c7e6b55482b
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-15 20:41:55 +0330

    remove prior form and replace with form component

--- src/pages/signIn-signUp/SignUp/SignUp.jsx
+++ src/pages/signIn-signUp/SignUp/SignUp.jsx
@@ -9,33 +9,6 @@ const SignUp = () => {
 		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
 			<img src={Logo} alt="logo image" className="w-60 h-auto" />
 			<SignUpForm />
-			<form className="flex flex-col items-center gap-y-4 ">
-				<input
-					type="text"
-					name="first-name"
-					placeholder="firstname"
-					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
-				/>
-				<input
-					type="text"
-					name="last-name"
-					placeholder="lastname"
-					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
-				/>
-				<input
-					type="email"
-					name="email"
-					placeholder="email"
-					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
-				/>
-				<input
-					type="password"
-					name="password"
-					placeholder="password"
-					className="px-4 py-3 border border-gray-300 rounded-md outline-none"
-				/>
-				<button type="submit">submit</button>
-			</form>
 			<span className="text-lg font-normal text-blue-550 capitalize">
 				or{" "}
 				<Link to="/auth/sign-in" className="underline">
