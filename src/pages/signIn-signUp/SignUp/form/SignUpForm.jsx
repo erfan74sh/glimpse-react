6c5c2bf706e294e3e46ba6cd49ad961ad878dbc3
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-15 20:25:58 +0330

    define a validation schema

--- src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
+++ src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import * as yup from "yup";
 // components
 import TextField from "../../../../components/inputs/TextField/TextField";
 
 const SignUpForm = () => {
+	const validate = yup.object({
+		firstName: yup
+			.string()
+			.max(15, "must be 15 characters or less")
+			.required("required"),
+		lastName: yup
+			.string()
+			.max(15, "must be 15 characters or less")
+			.required("required"),
+		email: yup.string().email("email is invalid").required("required"),
+		password: yup
+			.string()
+			.min(6, "password must be at least 6 characters")
+			.max(20, "password must be 20 characters or less")
+			.required("required"),
+	});
 	return (
 		<Formik
 			initialValues={{
@@ -12,6 +29,7 @@ const SignUpForm = () => {
 				email: "",
 				password: "",
 			}}
+			validationSchema={validate}
 		>
 			<Form className="flex flex-col items-center gap-y-4">
 				<TextField type="text" name="firstName" placeholder="first name" />
