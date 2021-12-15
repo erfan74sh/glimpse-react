6e83fcac260f2554ce1ba3987eee20e262cae34c
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-15 19:27:33 +0330

    import form in sign up page

--- src/pages/signIn-signUp/SignUp/SignUp.jsx
+++ src/pages/signIn-signUp/SignUp/SignUp.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 // assets
 import Logo from "../../../assets/images/logo-02.png";
+import SignUpForm from "./form/SignUpForm";
 
 const SignUp = () => {
 	return (
 		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
 			<img src={Logo} alt="logo image" className="w-60 h-auto" />
+			<SignUpForm />
 			<form className="flex flex-col items-center gap-y-4 ">
 				<input
 					type="text"
