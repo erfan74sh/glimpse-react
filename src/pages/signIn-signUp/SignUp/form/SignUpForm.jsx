2e732b0909754c108dd0b24726b0722afcca85cb
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-15 20:04:29 +0330

    define initial values for formik

--- src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
+++ src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
@@ -5,9 +5,19 @@ import TextField from "../../../../components/inputs/TextField/TextField";
 
 const SignUpForm = () => {
 	return (
-		<Formik>
-			<Form>
-				<TextField type="text" name="firstName" placeHolder="first name" />
+		<Formik
+			initialValues={{
+				firstName: "",
+				lastName: "",
+				email: "",
+				password: "",
+			}}
+		>
+			<Form className="flex flex-col items-center gap-y-4">
+				<TextField type="text" name="firstName" placeholder="first name" />
+				<TextField type="text" name="lastName" placeholder="last name" />
+				<TextField type="email" name="email" placeholder="email" />
+				<TextField type="password" name="password" placeholder="password" />
 			</Form>
 		</Formik>
 	);
