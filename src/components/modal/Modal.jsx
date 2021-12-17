178ce2f4d979c719de72caf9a976c7902892653b
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-17 22:02:58 +0330

    add width and height to modal container

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = () => {
 	return (
 		<div className="h-screen w-screen fixed flex items-center justify-center bg-white bg-opacity-80 z-30">
-			<section className="p-5 bg-white rounded-lg">
+			<section className="p-5 bg-white rounded-lg w-1/3 h-1/2">
 				<header className="flex justify-between items-center">
 					<h2>modal title here</h2>
 					<button>x</button>
