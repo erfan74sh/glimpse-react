24a965cfb977c06ce78da1c21cb7ab11b947643a
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-17 21:58:53 +0330

    style modal header container

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = () => {
 	return (
 		<div className="h-screen w-screen fixed flex items-center justify-center bg-white bg-opacity-80 z-30">
 			<section className="p-5 bg-white rounded-lg">
-				<header>
+				<header className="flex justify-between items-center">
 					<h2>modal title here</h2>
 					<button>x</button>
 				</header>
