aa99b3822b4966757343175d96191b0172919aa7
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-17 23:01:46 +0330

    set onclick handler to close btn

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Modal = () => {
+const Modal = ({ closeModal }) => {
 	return (
 		<div className="h-screen w-screen fixed flex items-center justify-center bg-white bg-opacity-80 z-30">
 			<section className="p-5 bg-white rounded-lg w-1/3 h-1/2">
 				<header className="pb-1 px-1 flex justify-between items-center border-b-2 border-gray-300">
 					<h2>modal title here</h2>
-					<button className="w-8">x</button>
+					<button className="w-8" onClick={closeModal}>
+						x
+					</button>
 				</header>
 			</section>
 		</div>
