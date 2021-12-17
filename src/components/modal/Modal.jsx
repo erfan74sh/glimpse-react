2bc9f8b508fd9777b38f4b8107f4be18af5d63f0
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-17 23:09:41 +0330

    close modal when click outside of modal container

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Modal = ({ closeModal }) => {
 	return (
-		<div className="h-screen w-screen fixed flex items-center justify-center bg-white bg-opacity-80 z-30">
-			<section className="p-5 bg-white rounded-lg w-1/3 h-1/2">
+		<div
+			className="h-screen w-screen fixed flex items-center justify-center bg-white bg-opacity-80 z-30"
+			onClick={closeModal}
+		>
+			<section
+				className="p-5 bg-white rounded-lg w-1/3 h-1/2"
+				onClick={(e) => {
+					e.stopPropagation();
+				}}
+			>
 				<header className="pb-1 px-1 flex justify-between items-center border-b-2 border-gray-300">
 					<h2>modal title here</h2>
 					<button className="w-8" onClick={closeModal}>
