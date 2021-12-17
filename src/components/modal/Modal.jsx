11d67b0d6aba3d87d95a47a168ce97383b5f69ca
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-17 22:07:17 +0330

    add padding to title

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = () => {
 	return (
 		<div className="h-screen w-screen fixed flex items-center justify-center bg-white bg-opacity-80 z-30">
 			<section className="p-5 bg-white rounded-lg w-1/3 h-1/2">
-				<header className="flex justify-between items-center">
+				<header className="pb-1 px-1 flex justify-between items-center border-b-2 border-gray-300">
 					<h2>modal title here</h2>
 					<button>x</button>
 				</header>
