d1ea32bbdb0fb165d73058ea8f026e649474470e
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 13:17:03 +0330

    use Formik in modal form

--- src/pages/Home/EstimationModal/EstimationModal.jsx
+++ src/pages/Home/EstimationModal/EstimationModal.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { Formik, Form } from "formik";
 // components
 import RadioBtn from "../../../components/inputs/RadioBtn";
 
 const EstimationModal = () => {
 	return (
-		<form className="flex flex-col gap-y-2">
-			<label>category</label>
-			<input type="text" />
-			<label>type</label>
-			<input type="text" />
-			<RadioBtn name="newOrPrevProject">new project</RadioBtn>
-			<RadioBtn name="newOrPrevProject">prev project</RadioBtn>
-			<button type="submit">next</button>
-		</form>
+		<Formik>
+			<Form className="flex flex-col gap-y-2">
+				<label>category</label>
+				<input type="text" />
+				<label>type</label>
+				<input type="text" />
+				<RadioBtn name="newOrPrevProject">new project</RadioBtn>
+				<RadioBtn name="newOrPrevProject">prev project</RadioBtn>
+				<button type="submit">next</button>
+			</Form>
+		</Formik>
 	);
 };
 
