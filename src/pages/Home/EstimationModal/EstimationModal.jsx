f0da46b9fd0174007f75fcacd8f4bd36c9187ba4
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 15:47:12 +0330

    show apropriate text field based on project

--- src/pages/Home/EstimationModal/EstimationModal.jsx
+++ src/pages/Home/EstimationModal/EstimationModal.jsx
@@ -2,21 +2,38 @@ import React from "react";
 import { Formik, Form, useFormikContext } from "formik";
 // components
 import RadioBtn from "../../../components/inputs/RadioBtn";
+import TextField from "../../../components/inputs/TextField/TextField";
 
 const ProjectName = () => {
 	const { values } = useFormikContext();
 	return (
 		<>
-			{values.newOrPrevProject === "newProject"
-				? "new project"
-				: "prev project"}
+			{values.newOrPrevProject === "newProject" ? (
+				<TextField
+					type="text"
+					name="newProjectName"
+					placeholder="enter a name for your project"
+				/>
+			) : (
+				<TextField
+					type="text"
+					name="prevProjectName"
+					placeholder="choose from prev projects"
+				/>
+			)}
 		</>
 	);
 };
 
 const EstimationModal = () => {
 	return (
-		<Formik initialValues={{ newOrPrevProject: "newProject" }}>
+		<Formik
+			initialValues={{
+				newOrPrevProject: "newProject",
+				newProjectName: "",
+				prevProjectName: "",
+			}}
+		>
 			<Form className="flex flex-col gap-y-2">
 				<RadioBtn name="newOrPrevProject" value="newProject">
 					new project
