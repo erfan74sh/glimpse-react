de3202d66d0830356345cc3881f9bd0197ffaca8
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 13:17:26 +0330

    get props and pass it to input

--- src/components/inputs/RadioBtn/RadioBtn.jsx
+++ src/components/inputs/RadioBtn/RadioBtn.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useField } from "formik";
 
-const RadioBtn = () => {
-	return <div>radio</div>;
+const RadioBtn = ({ children, ...props }) => {
+	const [field, meta] = useField({ ...props, type: "radio" });
+	return (
+		<label>
+			<input type="radio" {...props} />
+			{children}
+		</label>
+	);
 };
 
 export default RadioBtn;
