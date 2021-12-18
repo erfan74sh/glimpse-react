c9e4e21c8c31d844db1ccaedd76d16b115e18c38
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 17:08:18 +0330

    create radio btn options based on props

--- src/components/inputs/SelectField/SelectField.jsx
+++ src/components/inputs/SelectField/SelectField.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useField } from "formik";
+// components
+import RadioBtn from "../RadioBtn";
 
-const SelectField = () => {
-	return <div>select field</div>;
+const SelectField = ({ selectOptions }) => {
+	return (
+		<div>
+			{selectOptions.map((option, idx) => {
+				return (
+					<RadioBtn name="category" value={option.label} key={idx}>
+						{option.label}
+					</RadioBtn>
+				);
+			})}
+		</div>
+	);
 };
 
 export default SelectField;
