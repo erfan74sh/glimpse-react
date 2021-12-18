539e893aa47c0878e3313716e8b371c224c826f7
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 18:27:05 +0330

    handle showing drop down on click

--- src/components/inputs/SelectField/SelectField.jsx
+++ src/components/inputs/SelectField/SelectField.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useField, useFormikContext } from "formik";
 // components
 
@@ -13,14 +13,24 @@ const DropdownRadio = ({ children, ...props }) => {
 };
 
 const SelectField = ({ selectOptions, name }) => {
+	const [showDropdown, setShowDropdown] = useState(false);
 	const { values } = useFormikContext();
+
 	return (
 		<div className="relative">
-			<div className="flex justify-between items-center p-2 border border-gray-300 rounded-md outline-none">
+			<div
+				onClick={() => setShowDropdown(!showDropdown)}
+				className="flex justify-between items-center p-2 border border-gray-300 rounded-md outline-none"
+			>
 				<span>{values[name]}</span>
 				<span> {">"} </span>
 			</div>
-			<ul className="flex flex-col absolute -bottom-1 transform translate-y-full w-full p-2 z-10 rounded-md bg-red-350">
+			<ul
+				onChange={() => setShowDropdown(false)}
+				className={`${
+					showDropdown ? "flex" : "hidden"
+				} flex-col absolute -bottom-1 transform translate-y-full w-full p-2 z-10 rounded-md bg-red-350`}
+			>
 				{selectOptions.map((option, idx) => {
 					return (
 						<li key={idx}>
