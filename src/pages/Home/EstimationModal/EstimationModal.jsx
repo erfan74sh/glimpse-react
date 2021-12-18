02abb2bac036f6c37299b01eb4609cff765f8353
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 23:09:41 +0330

    create validation for estimationCategory and scale

--- src/pages/Home/EstimationModal/EstimationModal.jsx
+++ src/pages/Home/EstimationModal/EstimationModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik, Form, useFormikContext } from "formik";
+import * as yup from "yup";
 // components
 import RadioBtn from "../../../components/inputs/RadioBtn";
 import TextField from "../../../components/inputs/TextField";
@@ -67,12 +68,30 @@ const EstimationModal = () => {
 	return (
 		<Formik
 			initialValues={{
+				estimationCategory: "choose one",
+				estimationScale: "choose one",
 				newOrPrevProject: "newProject",
 				newProjectName: "",
 				prevProjectName: "",
-				estimationCategory: "choose one",
-				estimationScale: "choose one",
 			}}
+			validationSchema={yup.object({
+				estimationCategory: yup
+					.string()
+					.required("please select one")
+					.oneOf(
+						[
+							"energy, daylight and thermal comfort",
+							"structure",
+							"fire in building",
+							"acoustic",
+						],
+						"pick from provided list"
+					),
+				estimationScale: yup
+					.string()
+					.required("choose scale")
+					.oneOf(["zone", "building", "city"], "pick from provided list"),
+			})}
 		>
 			<Form className="flex flex-col gap-y-4">
 				<SelectField
