fe3e303ea681396d4fa086b48858638131a2f649
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 15:46:18 +0330

    remove error message

--- src/components/inputs/RadioBtn/RadioBtn.jsx
+++ src/components/inputs/RadioBtn/RadioBtn.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { useField } from "formik";
 
 const RadioBtn = ({ children, ...props }) => {
-	const [field, meta] = useField({ ...props, type: "radio" });
+	const [field] = useField({ ...props, type: "radio" });
 	return (
 		<div>
 			<label>
 				<input type="radio" {...field} {...props} />
 				{children}
 			</label>
-			{/* {meta.touched && meta.error ? <span>{meta.error}</span> : null} */}
 		</div>
 	);
 };
