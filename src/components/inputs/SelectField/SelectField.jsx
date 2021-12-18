167dbc485386c19ca5bdd86499f77df0888c6c15
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-18 20:53:23 +0330

    style selectField component

--- src/components/inputs/SelectField/SelectField.jsx
+++ src/components/inputs/SelectField/SelectField.jsx
@@ -14,12 +14,13 @@ const DropdownRadio = ({ children, ...props }) => {
 	);
 };
 
-const SelectField = ({ selectOptions, name }) => {
+const SelectField = ({ selectOptions, name, label }) => {
 	const [showDropdown, setShowDropdown] = useState(false);
 	const { values } = useFormikContext();
 
 	return (
-		<div className="relative">
+		<div className="relative flex flex-col gap-y-1 capitalize">
+			<label>{label}</label>
 			<div
 				onClick={() => setShowDropdown(!showDropdown)}
 				className="flex justify-between items-center p-2 border border-gray-300 rounded-md outline-none"
@@ -38,11 +39,14 @@ const SelectField = ({ selectOptions, name }) => {
 				onChange={() => setShowDropdown(false)}
 				className={`${
 					showDropdown ? "flex" : "hidden"
-				} flex-col absolute -bottom-1 transform translate-y-full w-full p-2 z-10 rounded-md bg-red-350`}
+				} flex-col absolute -bottom-1 transform translate-y-full w-full p-2 px-3 z-10 rounded-md bg-white shadow-full-sm`}
 			>
 				{selectOptions.map((option, idx) => {
 					return (
-						<li key={idx}>
+						<li
+							key={idx}
+							className={`py-1 transition text-gray-500 hover:text-gray-900`}
+						>
 							<DropdownRadio name={name} value={option.label}>
 								{option.label}
 							</DropdownRadio>
