1275fc5a6b5ffdb1779cf4f7fb82cfbd3d0d797b
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-20 10:43:43 +0330

    define steps state

--- src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
+++ src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const BreadCrumps = () => {
+	const [steps, setSteps] = useState({
+		category: "",
+		scale: "",
+		project: "",
+		zone: "",
+	});
+	useEffect(() => {
+		// todo: fetch data and update state here if user has filled some inputs before
+	}, []);
+
 	return <div>breadcrumps</div>;
 };
 
