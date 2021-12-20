8b4c4978450115874f9693566220b8f78ebf8c8f
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-20 11:08:06 +0330

    change category with handleCategory fn

--- src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
+++ src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from "react";
 
-const Category = ({ steps }) => {
+const Category = ({ steps, handleCategory }) => {
 	return (
 		<div>
 			{steps.category !== "" ? (
 				<span>{`${steps.category} > `}</span>
 			) : (
-				<form>
+				<form onChange={handleCategory}>
 					<fieldset>category:</fieldset>
 					<label>
 						energy
-						<input type="radio" name="category" />
+						<input type="radio" name="category" value="energy" />
 					</label>
 					<label>
 						structure
-						<input type="radio" name="category" />
+						<input type="radio" name="category" value="structure" />
 					</label>
 					<label>
 						fire
-						<input type="radio" name="category" />
+						<input type="radio" name="category" value="fire" />
 					</label>
 					<label>
 						acoustic
-						<input type="radio" name="category" />
+						<input type="radio" name="category" value="acoustic" />
 					</label>
 				</form>
 			)}
@@ -46,13 +46,23 @@ const BreadCrumps = () => {
 		project: "",
 		zone: "",
 	});
+
+	const handleStep = (e) => {
+		setSteps({ ...steps, [e.target.name]: e.target.value });
+	};
+
 	useEffect(() => {
 		// todo: fetch data and update state here if user has filled some inputs before
 	}, []);
 
 	return (
 		<div>
-			<Category steps={steps} />
+			<Category
+				steps={steps}
+				handleCategory={(e) => {
+					handleStep(e);
+				}}
+			/>
 			<Scale steps={steps} />
 			<Project steps={steps} />
 			<Zone steps={steps} />
