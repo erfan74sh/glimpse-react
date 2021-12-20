554dbfc2e1373b3f2ef253e06f8330cee46c4118
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-20 21:18:45 +0330

    reading zone param from url

--- src/pages/Estimation/Result/Zone/Zone.jsx
+++ src/pages/Estimation/Result/Zone/Zone.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Zone = () => {
-	return <div>zone</div>;
+	const params = useParams();
+	return <div>zone: {params.zoneId}</div>;
 };
 
 export default Zone;
