bd3e5e88a09736fc722307702869a142b6e29b4a
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-22 18:38:29 +0330

    create some markers latlng and add them to map

--- src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
+++ src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 const mapContainerStyle = {
 	width: "70vw",
@@ -10,6 +10,12 @@ const center = {
 	lng: 53.688046,
 };
 
+const markers = [
+	{ lat: 32.654629, lng: 51.667984 },
+	{ lat: 38.096237, lng: 46.2738 },
+	{ lat: 29.591768, lng: 52.583698 },
+];
+
 const ModalMap = () => {
 	const { isLoaded, loadError } = useLoadScript({
 		googleMapsApiKey: "AIzaSyAJ141ObenmuFdj9mIj7YogQBoS7vstB0U",
@@ -19,11 +25,11 @@ const ModalMap = () => {
 
 	return (
 		<div>
-			<GoogleMap
-				mapContainerStyle={mapContainerStyle}
-				zoom={5}
-				center={center}
-			></GoogleMap>
+			<GoogleMap mapContainerStyle={mapContainerStyle} zoom={5} center={center}>
+				{markers.map((marker, idx) => (
+					<Marker key={idx} position={{ lat: marker.lat, lng: marker.lng }} />
+				))}
+			</GoogleMap>
 		</div>
 	);
 };
