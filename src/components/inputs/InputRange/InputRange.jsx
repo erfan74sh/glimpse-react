42a53a9d5ef133a78a89c8d5209ed44cf2697036
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-23 20:38:28 +0330

    create inputRange as a formik custom field

--- src/components/inputs/InputRange/InputRange.jsx
+++ src/components/inputs/InputRange/InputRange.jsx
@@ -1,29 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { useField } from "formik";
 
-const InputRange = ({ label, min, max, step, handleValue }) => {
-	// const initialValue = Number(min);
-	const [value, setValue] = useState(min);
-
-	const handleChangeValue = (e) => {
-		setValue(e.target.value);
-		handleValue(e);
-	};
+const InputRange = ({ label, min, max, step, ...props }) => {
+	const [field] = useField({ ...props, type: "range" });
 
 	return (
 		<div className="mr-8">
 			<div className="flex justify-between">
 				<label>{label}</label>
-				<span>{value}</span>
+				<span>{field.value}</span>
 			</div>
 			<input
 				type="range"
 				min={min}
 				max={max}
 				step={step}
-				// value="16"
-				defaultValue={min}
+				{...field}
+				{...props}
 				className="w-full"
-				onInput={handleChangeValue}
 			/>
 		</div>
 	);
