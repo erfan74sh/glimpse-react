f1ade697a85aa0c0ffd19457809d2334f472365e
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-25 15:25:30 +0330

    edit Material component style of container

--- src/pages/Estimation/Estimate/FormSteps/Material/Material.jsx
+++ src/pages/Estimation/Estimate/FormSteps/Material/Material.jsx
@@ -70,7 +70,7 @@ const Material = ({ nextStep, prevStep }) => {
 	return (
 		<>
 			<Formik initialValues={data}>
-				<Form>
+				<Form className="h-full flex flex-col">
 					<fieldset className="flex flex-col gap-y-9 pr-10">
 						<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
 							material
@@ -83,7 +83,6 @@ const Material = ({ nextStep, prevStep }) => {
 							step=".05"
 							handleValue={handleWallMaterialValue}
 						/>
-
 						<InputRange
 							name="ceilingMaterial"
 							label="Ceiling-material"
@@ -108,26 +107,26 @@ const Material = ({ nextStep, prevStep }) => {
 							handleValue={handleGlassMaterialValue}
 						/>
 					</fieldset>
+					<section className="flex justify-center gap-x-4 mt-auto pr-10">
+						<button
+							type="button"
+							className="flex items-center gap-x-1 px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
+							value="geometry"
+							onClick={prevStep}
+						>
+							prev <span className="text-xs lowercase">(geometry)</span>
+						</button>
+						<button
+							type="button"
+							className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
+							value="site plan"
+							onClick={nextStep}
+						>
+							next <span className="text-xs lowercase">(site plan)</span>
+						</button>
+					</section>
 				</Form>
 			</Formik>
-			<section className="flex justify-center gap-x-4 mt-auto mr-8 pr-10">
-				<button
-					type="button"
-					className="flex items-center gap-x-1 px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
-					value="geometry"
-					onClick={prevStep}
-				>
-					prev <span className="text-xs lowercase">(geometry)</span>
-				</button>
-				<button
-					type="button"
-					className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
-					value="site plan"
-					onClick={nextStep}
-				>
-					next <span className="text-xs lowercase">(site plan)</span>
-				</button>
-			</section>
 		</>
 	);
 };
