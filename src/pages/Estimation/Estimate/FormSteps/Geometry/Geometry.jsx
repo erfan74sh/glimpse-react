0c8b9667d1b29c56965d478edc1ecb6aa8304579
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-25 14:42:53 +0330

    add validation for xDim/ change layoute and add handleSubmit

--- src/pages/Estimation/Estimate/FormSteps/Geometry/Geometry.jsx
+++ src/pages/Estimation/Estimate/FormSteps/Geometry/Geometry.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Form, Formik } from "formik";
+import * as yup from "yup";
 // state
 import {
 	selectInput,
@@ -74,105 +75,76 @@ const Geometry = ({ nextStep }) => {
 	];
 
 	return (
-		<>
-			<Formik initialValues={data}>
-				<Form>
-					<fieldset className="flex flex-col gap-y-9 pr-10">
-						<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
-							geometry
-						</legend>
-						<InputRange
-							name="xDim"
-							label="X-Dimention"
-							min="8"
-							max="24"
-							step="0.5"
-							handleValue={handleXDimValue}
-						/>
-						<InputRange
-							name="yDim"
-							label="Y-Dimention"
-							min="3"
-							max="10"
-							step="0.5"
-							handleValue={handleYDimValue}
-						/>
-						<InputRange
-							name="wwrNorth"
-							label="WWR-North"
-							min="10"
-							max="80"
-							step="10"
-							handleValue={handleWwrNorthValue}
-						/>
-						<InputRange
-							name="wwrSouth"
-							label="WWR-South"
-							min="10"
-							max="80"
-							step="10"
-							handleValue={handleWwrSouthValue}
-						/>
-						<SelectField
-							name="shadingType"
-							selectOptions={shadingOptions}
-							label="shading type"
-							placeholder="choose one type"
-							handleValue={handleShadingTypeValue}
-						/>
-					</fieldset>
-				</Form>
-			</Formik>
-			{/* <fieldset className="flex flex-col gap-y-9 pr-10">
-				<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
-					geometry
-				</legend>
-				<InputRange
-					label="X-Dimention"
-					min="8"
-					max="24"
-					step="0.5"
-					handleValue={(e) => handleXDimValue(e)}
-				/>
-				<InputRange
-					label="Y-Dimention"
-					min="3"
-					max="10"
-					step="0.5"
-					handleValue={(e) => handleYDimValue(e)}
-				/>
-				<InputRange
-					label="WWR-North"
-					min="10"
-					max="80"
-					step="10"
-					handleValue={(e) => handleWwrNorthValue(e)}
-				/>
-				<InputRange
-					label="WWR-South"
-					min="10"
-					max="80"
-					step="10"
-					handleValue={(e) => handleWwrSouthValue(e)}
-				/>
-
-				<InputSelect
-					placeHolder="Shading Type"
-					items={["Horizontal", "Horizontal Louvre", "Vertical", "All modes"]}
-					handleValue={(e) => handleShadingTypeValue(e)}
-				/>
-			</fieldset> */}
-			<section className="flex justify-center gap-x-4 mt-auto mr-8 pr-10">
-				<button
-					type="button"
-					className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
-					onClick={nextStep}
-					value="material"
-				>
-					next <span className="text-xs lowercase">(material)</span>
-				</button>
-			</section>
-		</>
+		<Formik
+			initialValues={data}
+			onSubmit={(values) => {
+				// todo: edit form submit
+				console.log(values);
+				nextStep();
+			}}
+			validationSchema={yup.object({
+				xDim: yup
+					.number()
+					.min(8, "x-dimention must be equal or greater than 8")
+					.max(24, "x-dimention must be equal or less than 24")
+					.required("required"),
+			})}
+		>
+			<Form className="h-full flex flex-col">
+				<fieldset className="flex flex-col gap-y-9 pr-10">
+					<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+						geometry
+					</legend>
+					<InputRange
+						name="xDim"
+						label="X-Dimention"
+						min="8"
+						max="24"
+						step="0.5"
+						handleValue={handleXDimValue}
+					/>
+					<InputRange
+						name="yDim"
+						label="Y-Dimention"
+						min="3"
+						max="10"
+						step="0.5"
+						handleValue={handleYDimValue}
+					/>
+					<InputRange
+						name="wwrNorth"
+						label="WWR-North"
+						min="10"
+						max="80"
+						step="10"
+						handleValue={handleWwrNorthValue}
+					/>
+					<InputRange
+						name="wwrSouth"
+						label="WWR-South"
+						min="10"
+						max="80"
+						step="10"
+						handleValue={handleWwrSouthValue}
+					/>
+					<SelectField
+						name="shadingType"
+						selectOptions={shadingOptions}
+						label="shading type"
+						placeholder="choose one type"
+						handleValue={handleShadingTypeValue}
+					/>
+				</fieldset>
+				<section className="flex justify-center gap-x-4 mt-auto pr-10">
+					<button
+						type="submit"
+						className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
+					>
+						next <span className="text-xs lowercase">(material)</span>
+					</button>
+				</section>
+			</Form>
+		</Formik>
 	);
 };
 
