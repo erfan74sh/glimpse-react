f6fa7811f9455d4b4b4fe79cc769d111cd8dc80c
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-25 15:34:23 +0330

    remove reactFragment element

--- src/pages/Estimation/Estimate/FormSteps/Material/Material.jsx
+++ src/pages/Estimation/Estimate/FormSteps/Material/Material.jsx
@@ -69,92 +69,90 @@ const Material = ({ nextStep, prevStep }) => {
 	];
 
 	return (
-		<>
-			<Formik
-				initialValues={data}
-				onSubmit={(values) => {
-					// todo: edit submit function
-					console.log(values);
-					nextStep();
-				}}
-				validationSchema={yup.object({
-					wallMaterial: yup
-						.number()
-						.min(0.2, "must be between 0.2 and 0.7")
-						.max(0.7, "")
-						.required("required"),
-					ceilingMaterial: yup
-						.number()
-						.min(0.2, "must be between 0.2 and 0.7")
-						.max(0.7, "")
-						.required("required"),
-					floorMaterial: yup
-						.number()
-						.min(0.2, "must be between 0.2 and 0.7")
-						.max(0.7, "")
-						.required("required"),
-					glassMaterial: yup
-						.number()
-						.oneOf([0.58, 0.72, 0.8, 0.9], "choose one")
-						.required("required"),
-				})}
-			>
-				<Form className="h-full flex flex-col">
-					<fieldset className="flex flex-col gap-y-9 pr-10">
-						<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
-							material
-						</legend>
-						<InputRange
-							name="wallMaterial"
-							label="Wall-material"
-							min=".2"
-							max=".7"
-							step=".05"
-							handleValue={handleWallMaterialValue}
-						/>
-						<InputRange
-							name="ceilingMaterial"
-							label="Ceiling-material"
-							min=".2"
-							max=".7"
-							step=".05"
-							handleValue={handleCeilingMaterialValue}
-						/>
-						<InputRange
-							name="floorMaterial"
-							label="Floor-material"
-							min=".2"
-							max=".7"
-							step=".05"
-							handleValue={handleFloorMaterialValue}
-						/>
-						<SelectField
-							name="glassMaterial"
-							selectOptions={glassMaterialOptions}
-							label="Glass-material"
-							placeholder="choose one type"
-							handleValue={handleGlassMaterialValue}
-						/>
-					</fieldset>
-					<section className="flex justify-center gap-x-4 mt-auto pr-10">
-						<button
-							type="button"
-							className="flex items-center gap-x-1 px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
-							value="geometry"
-							onClick={prevStep}
-						>
-							prev <span className="text-xs lowercase">(geometry)</span>
-						</button>
-						<button
-							type="submit"
-							className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
-						>
-							next <span className="text-xs lowercase">(site plan)</span>
-						</button>
-					</section>
-				</Form>
-			</Formik>
-		</>
+		<Formik
+			initialValues={data}
+			onSubmit={(values) => {
+				// todo: edit submit function
+				console.log(values);
+				nextStep();
+			}}
+			validationSchema={yup.object({
+				wallMaterial: yup
+					.number()
+					.min(0.2, "must be between 0.2 and 0.7")
+					.max(0.7, "")
+					.required("required"),
+				ceilingMaterial: yup
+					.number()
+					.min(0.2, "must be between 0.2 and 0.7")
+					.max(0.7, "")
+					.required("required"),
+				floorMaterial: yup
+					.number()
+					.min(0.2, "must be between 0.2 and 0.7")
+					.max(0.7, "")
+					.required("required"),
+				glassMaterial: yup
+					.number()
+					.oneOf([0.58, 0.72, 0.8, 0.9], "choose one")
+					.required("required"),
+			})}
+		>
+			<Form className="h-full flex flex-col">
+				<fieldset className="flex flex-col gap-y-9 pr-10">
+					<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+						material
+					</legend>
+					<InputRange
+						name="wallMaterial"
+						label="Wall-material"
+						min=".2"
+						max=".7"
+						step=".05"
+						handleValue={handleWallMaterialValue}
+					/>
+					<InputRange
+						name="ceilingMaterial"
+						label="Ceiling-material"
+						min=".2"
+						max=".7"
+						step=".05"
+						handleValue={handleCeilingMaterialValue}
+					/>
+					<InputRange
+						name="floorMaterial"
+						label="Floor-material"
+						min=".2"
+						max=".7"
+						step=".05"
+						handleValue={handleFloorMaterialValue}
+					/>
+					<SelectField
+						name="glassMaterial"
+						selectOptions={glassMaterialOptions}
+						label="Glass-material"
+						placeholder="choose one type"
+						handleValue={handleGlassMaterialValue}
+					/>
+				</fieldset>
+				<section className="flex justify-center gap-x-4 mt-auto pr-10">
+					<button
+						type="button"
+						className="flex items-center gap-x-1 px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
+						value="geometry"
+						onClick={prevStep}
+					>
+						prev <span className="text-xs lowercase">(geometry)</span>
+					</button>
+					<button
+						type="submit"
+						className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
+					>
+						next <span className="text-xs lowercase">(site plan)</span>
+					</button>
+				</section>
+			</Form>
+		</Formik>
 	);
 };
 
