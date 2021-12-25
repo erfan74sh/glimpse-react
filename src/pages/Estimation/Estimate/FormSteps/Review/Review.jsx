45a7d3fd045524c7eb4acc4e091da4e2b3bae44c
Erfan Shafiee <erfan74sh@gmail.com>
2021-12-25 15:51:19 +0330

    edit container layout

--- src/pages/Estimation/Estimate/FormSteps/Review/Review.jsx
+++ src/pages/Estimation/Estimate/FormSteps/Review/Review.jsx
@@ -7,7 +7,7 @@ const Review = ({ prevStep }) => {
 	const data = useSelector(selectInput);
 
 	return (
-		<>
+		<div className="h-full flex flex-col">
 			<section className="flex flex-col pr-10">
 				<h2 className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
 					review
@@ -93,7 +93,7 @@ const Review = ({ prevStep }) => {
 					</li>
 				</ul>
 			</section>
-			<section className="flex justify-center gap-x-4 mt-auto mr-8 pr-10">
+			<section className="flex justify-center gap-x-4 mt-auto pr-10">
 				<button
 					type="button"
 					className="flex items-center justify-between px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
@@ -103,7 +103,7 @@ const Review = ({ prevStep }) => {
 					prev <span className="text-xs lowercase">(SitePlan)</span>
 				</button>
 			</section>
-		</>
+		</div>
 	);
 };
 
