f0eff9e7d63c1fc3b1dac0deb158b89c5f1c6a79
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-03 00:39:48 +0330

    add info section and ! mark

--- src/components/moreInfo/MoreInfo.jsx
+++ src/components/moreInfo/MoreInfo.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const MoreInfo = () => {
-	return <div>more info</div>;
+const MoreInfo = ({ moreInfo }) => {
+	return (
+		<div>
+			<span>!</span>
+			<span>{moreInfo}</span>
+		</div>
+	);
 };
 
 export default MoreInfo;
