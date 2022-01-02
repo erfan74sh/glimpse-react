10717b30bfa938597c5e317ff347141629112c74
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-03 01:17:17 +0330

    change cursor to help

--- src/components/moreInfo/MoreInfo.jsx
+++ src/components/moreInfo/MoreInfo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MoreInfo = ({ moreInfo }) => {
 	return (
 		<div className="relative flex items-center group">
-			<span className="flex items-center justify-center w-4 h-4 text-xs border border-blue-550 rounded-full text-blue-550">
+			<span className="flex items-center justify-center w-4 h-4 text-xs font-bold border border-blue-550 rounded-full text-blue-550 cursor-help">
 				!
 			</span>
 			<span className="hidden w-max px-1 absolute left-full transform translate-x-1 rounded-md text-white text-sm bg-gray-650 bg-opacity-75 group-hover:block">
