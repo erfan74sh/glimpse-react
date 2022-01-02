c4d8ba6b55180d749638709b1e1729dd464afdae
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-03 01:02:08 +0330

    style info span

--- src/components/moreInfo/MoreInfo.jsx
+++ src/components/moreInfo/MoreInfo.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const MoreInfo = ({ moreInfo }) => {
 	return (
-		<div>
+		<div className="relative flex items-center">
 			<span className="flex items-center justify-center w-4 h-4 text-xs border border-blue-550 rounded-full text-blue-550">
 				!
 			</span>
-			<span>{moreInfo}</span>
+			<span className="w-max px-1 absolute left-full transform translate-x-1 rounded-md text-white text-sm bg-gray-650 bg-opacity-75">
+				{moreInfo}
+			</span>
 		</div>
 	);
 };
