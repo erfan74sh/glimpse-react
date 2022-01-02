b2d19ba959194ae68598e867527fd4b5b994f457
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-03 01:20:52 +0330

    change padding and font-size for more info section

--- src/components/moreInfo/MoreInfo.jsx
+++ src/components/moreInfo/MoreInfo.jsx
@@ -6,7 +6,7 @@ const MoreInfo = ({ moreInfo }) => {
 			<span className="flex items-center justify-center w-4 h-4 text-xs font-bold border border-blue-550 rounded-full text-blue-550 cursor-help">
 				!
 			</span>
-			<span className="hidden w-max px-1 absolute left-full transform translate-x-1 rounded-md text-white text-sm bg-gray-650 bg-opacity-75 group-hover:block">
+			<span className="hidden w-max px-2 py-1 absolute left-full transform translate-x-1 rounded-md text-white text-xs bg-gray-650 bg-opacity-75 group-hover:block">
 				{moreInfo}
 			</span>
 		</div>
