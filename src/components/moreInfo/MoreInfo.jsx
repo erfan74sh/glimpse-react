a38d31f59aae7e40ecead84ecb5958f407925224
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-03 01:14:21 +0330

    add hover for showing description

--- src/components/moreInfo/MoreInfo.jsx
+++ src/components/moreInfo/MoreInfo.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const MoreInfo = ({ moreInfo }) => {
 	return (
-		<div className="relative flex items-center">
+		<div className="relative flex items-center group">
 			<span className="flex items-center justify-center w-4 h-4 text-xs border border-blue-550 rounded-full text-blue-550">
 				!
 			</span>
-			<span className="w-max px-1 absolute left-full transform translate-x-1 rounded-md text-white text-sm bg-gray-650 bg-opacity-75">
+			<span className="hidden w-max px-1 absolute left-full transform translate-x-1 rounded-md text-white text-sm bg-gray-650 bg-opacity-75 group-hover:block">
 				{moreInfo}
 			</span>
 		</div>
