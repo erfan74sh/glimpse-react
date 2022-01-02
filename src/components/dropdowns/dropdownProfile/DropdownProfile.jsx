caf28e9587e2587ef1574c8810b32ec693f28191
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-02 23:41:49 +0330

    change size of icon

--- src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
+++ src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
@@ -15,25 +15,25 @@ const DropdownProfile = ({ showDropdown }) => {
 		>
 			<li>
 				<Link to="/profile" className="flex gap-x-4 px-2 py-4">
-					<img src={ProfileIcon} alt="profile icon" />
+					<img src={ProfileIcon} alt="profile icon" className="w-6 h-auto" />
 					<span>profile</span>
 				</Link>
 			</li>
 			<li>
 				<Link to="#" className="flex gap-x-4 px-2 py-4">
-					<img src={ProfileIcon} alt="profile icon" />
+					<img src={ProfileIcon} alt="profile icon" className="w-6 h-auto" />
 					<span>projects history</span>
 				</Link>
 			</li>
 			<li>
 				<Link to="#" className="flex gap-x-4 px-2 py-4">
-					<img src={ChargeIcon} alt="charge icon" />
+					<img src={ChargeIcon} alt="charge icon" className="w-6 h-auto" />
 					<span>charge account</span>
 				</Link>
 			</li>
 			<li>
 				<Link to="#" className="flex gap-x-4 px-2 py-4">
-					<img src={SignOutIcon} alt="sign out icon" />
+					<img src={SignOutIcon} alt="sign out icon" className="w-6 h-auto" />
 					<span className="text-red-350">sign out</span>
 				</Link>
 			</li>
