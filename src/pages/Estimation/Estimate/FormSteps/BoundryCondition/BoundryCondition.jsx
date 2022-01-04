4b6947e0320f204411ec19f1be1aee012392fe2c
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-04 12:34:16 +0330

    create formik for boundry condition comp

--- src/pages/Estimation/Estimate/FormSteps/BoundryCondition/BoundryCondition.jsx
+++ src/pages/Estimation/Estimate/FormSteps/BoundryCondition/BoundryCondition.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Form, Formik } from "formik";
+// import * as yup from "yup";
+// // state
+import {
+	selectInput,
+	updateData,
+} from "../../../../../features/data/inputDataSlice";
+// // components
+// import SelectField from "../../../../../components/inputs/SelectField/SelectField";
 
-const BoundryCondition = () => {
-	return <div>boundry</div>;
+const BoundryCondition = ({ nextStep, prevStep }) => {
+	const data = useSelector(selectInput);
+
+	// const dispatch = useDispatch();
+
+	return (
+		<Formik
+			initialValues={data}
+			onSubmit={(values) => {
+				// todo: edit form submit
+				console.log(values);
+				nextStep();
+			}}
+			// validationSchema={yup.object({
+
+			// })}
+		>
+			<Form className="h-full flex flex-col">
+				<fieldset className="flex flex-col gap-y-9 pr-10">
+					<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+						Boundry conditions
+					</legend>
+					{/* <SelectField
+						name="floorLevel"
+						selectOptions={floorLevelOptions}
+						label="floor level"
+						placeholder="choose level"
+						handleValue={() => null}
+					/> */}
+				</fieldset>
+			</Form>
+		</Formik>
+	);
 };
 
 export default BoundryCondition;
