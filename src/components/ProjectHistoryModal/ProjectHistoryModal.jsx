80098016b90ad7e0652addd3cb32cf1467015e6a
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-04 19:45:00 +0330

    style ul tag

--- src/components/ProjectHistoryModal/ProjectHistoryModal.jsx
+++ src/components/ProjectHistoryModal/ProjectHistoryModal.jsx
@@ -3,7 +3,43 @@ import { Link } from "react-router-dom";
 
 const ProjectHistoryModal = () => {
 	return (
-		<ul>
+		<ul className="flex flex-col gap-y-5 py-5">
+			<li className="w-133 bg-white shadow-full-sm rounded-md">
+				<Link
+					to="#"
+					className="flex items-center justify-between p-3 text-gray-600"
+				>
+					<span className="font-semibold ">1 - project name</span>
+					<span className="text-xs">Last edit :1/2/2022</span>
+				</Link>
+			</li>
+			<li className="w-133 bg-white shadow-full-sm rounded-md">
+				<Link
+					to="#"
+					className="flex items-center justify-between p-3 text-gray-600"
+				>
+					<span className="font-semibold ">1 - project name</span>
+					<span className="text-xs">Last edit :1/2/2022</span>
+				</Link>
+			</li>
+			<li className="w-133 bg-white shadow-full-sm rounded-md">
+				<Link
+					to="#"
+					className="flex items-center justify-between p-3 text-gray-600"
+				>
+					<span className="font-semibold ">1 - project name</span>
+					<span className="text-xs">Last edit :1/2/2022</span>
+				</Link>
+			</li>
+			<li className="w-133 bg-white shadow-full-sm rounded-md">
+				<Link
+					to="#"
+					className="flex items-center justify-between p-3 text-gray-600"
+				>
+					<span className="font-semibold ">1 - project name</span>
+					<span className="text-xs">Last edit :1/2/2022</span>
+				</Link>
+			</li>
 			<li className="w-133 bg-white shadow-full-sm rounded-md">
 				<Link
 					to="#"
