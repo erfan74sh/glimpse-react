a241d0149b4aa6a7ad3829a71e43691338ea0d2d
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-04 19:39:51 +0330

    style li tag

--- src/components/ProjectHistoryModal/ProjectHistoryModal.jsx
+++ src/components/ProjectHistoryModal/ProjectHistoryModal.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const ProjectHistoryModal = () => {
 	return (
 		<ul>
-			<li>
+			<li className="px-2 py-3 w-133 bg-white shadow-full-sm rounded-md">
 				<Link to="#">
 					<span>1 - project name</span>
 					<span>Last edit :1/2/2022</span>
