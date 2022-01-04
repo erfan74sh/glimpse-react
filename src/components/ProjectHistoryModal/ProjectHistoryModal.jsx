152a9d6f470835566e2bd02d2c46315572f0b886
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-04 19:43:05 +0330

    style li content

--- src/components/ProjectHistoryModal/ProjectHistoryModal.jsx
+++ src/components/ProjectHistoryModal/ProjectHistoryModal.jsx
@@ -4,10 +4,13 @@ import { Link } from "react-router-dom";
 const ProjectHistoryModal = () => {
 	return (
 		<ul>
-			<li className="px-2 py-3 w-133 bg-white shadow-full-sm rounded-md">
-				<Link to="#" className="flex items-center justify-between">
-					<span>1 - project name</span>
-					<span>Last edit :1/2/2022</span>
+			<li className="w-133 bg-white shadow-full-sm rounded-md">
+				<Link
+					to="#"
+					className="flex items-center justify-between p-3 text-gray-600"
+				>
+					<span className="font-semibold ">1 - project name</span>
+					<span className="text-xs">Last edit :1/2/2022</span>
 				</Link>
 			</li>
 		</ul>
