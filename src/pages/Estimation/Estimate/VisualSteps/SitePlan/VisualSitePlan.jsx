fd14e4f2647890ae3aa10b1a61f624253df2d54e
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-05 13:21:35 +0330

    add dynamic data and title

--- src/pages/Estimation/Estimate/VisualSteps/SitePlan/VisualSitePlan.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/SitePlan/VisualSitePlan.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const VisualSitePlan = () => {
-	return <div>site plan</div>;
+	return (
+		<>
+			<h2 className=" mb-9 mx-8 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+				details
+			</h2>
+			<div className="">
+				<div className="w-full px-10 h-96">content site plan</div>
+			</div>
+		</>
+	);
 };
 
 export default VisualSitePlan;
