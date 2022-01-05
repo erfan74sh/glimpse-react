6cc349404fd4c9bcd21d8a99c4c26cdfb1643009
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-05 13:21:23 +0330

    add dynamic data and title

--- src/pages/Estimation/Estimate/VisualSteps/Hvac/VisualHvac.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/Hvac/VisualHvac.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const VisualHvac = () => {
-	return <div>hvac</div>;
+	return (
+		<>
+			<h2 className=" mb-9 mx-8 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+				details
+			</h2>
+			<div className="">
+				<div className="w-full px-10 h-96">content hvac</div>
+			</div>
+		</>
+	);
 };
 
 export default VisualHvac;
