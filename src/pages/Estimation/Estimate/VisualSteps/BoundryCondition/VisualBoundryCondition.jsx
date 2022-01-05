2cbf9f9f377e5b72fed43f8e981a6e90dc16e090
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-05 13:13:01 +0330

    add 3d model to ViusalBoundryCondition component

--- src/pages/Estimation/Estimate/VisualSteps/BoundryCondition/VisualBoundryCondition.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/BoundryCondition/VisualBoundryCondition.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+// component
+import Model3D from "../../3D";
+// state
+import { selectInput } from "../../../../../features/data/inputDataSlice";
 
 const VisualBoundryCondition = () => {
-	return <div>boundry</div>;
+	const inputData = useSelector(selectInput);
+	return (
+		<Model3D
+			xDim={inputData.xDim}
+			yDim={inputData.yDim}
+			wwrNorth={inputData.wwrNorth}
+			wwrSouth={inputData.wwrSouth}
+			shadingType={inputData.shadingType}
+		/>
+	);
 };
 
 export default VisualBoundryCondition;
