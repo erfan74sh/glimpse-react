01b258ed8c82b095df018c1af7999ed8436b2b69
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-05 13:11:35 +0330

    add 3d model to VisualGeometry component

--- src/pages/Estimation/Estimate/VisualSteps/Geometry/VisualGeometry.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/Geometry/VisualGeometry.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+// component
+import Model3D from "../../3D";
+// state
+import { selectInput } from "../../../../../features/data/inputDataSlice";
 
 const VisualGeometry = () => {
-	return <div>geometry</div>;
+	const inputData = useSelector(selectInput);
+	return (
+		<Model3D
+			xDim={inputData.xDim}
+			yDim={inputData.yDim}
+			wwrNorth={inputData.wwrNorth}
+			wwrSouth={inputData.wwrSouth}
+			shadingType={inputData.shadingType}
+		/>
+	);
 };
 
 export default VisualGeometry;
