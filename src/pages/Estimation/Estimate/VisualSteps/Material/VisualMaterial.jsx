a0336ac2135fa179ceee5b202f436853161063ca
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-06 21:50:00 +0330

    add text to material container

--- src/pages/Estimation/Estimate/VisualSteps/Material/VisualMaterial.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/Material/VisualMaterial.jsx
@@ -9,12 +9,20 @@ const VisualMaterial = () => {
 			<div className="">
 				<div className="w-full flex flex-col gap-y-10 justify-center px-10 pt-5">
 					<div className="flex gap-x-10 justify-center">
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400"></aticle>
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400"></aticle>
+						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+							<span>wall</span>
+						</aticle>
+						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+							<span>roof</span>
+						</aticle>
 					</div>
 					<div className="flex gap-x-10 justify-center">
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400"></aticle>
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400"></aticle>
+						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+							<span>floor</span>
+						</aticle>
+						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+							<span>window</span>
+						</aticle>
 					</div>
 				</div>
 			</div>
