9dbb8492ce75af7b9a83b04c2be9c71c71916a6f
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-06 21:51:36 +0330

    make material container flex

--- src/pages/Estimation/Estimate/VisualSteps/Material/VisualMaterial.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/Material/VisualMaterial.jsx
@@ -9,24 +9,24 @@ const VisualMaterial = () => {
 			<div className="">
 				<div className="w-full flex flex-col gap-y-10 justify-center px-10 pt-5">
 					<div className="flex gap-x-10 justify-center">
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+						<aticle className="flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
 							<span className="text-xl font-semibold text-white uppercase">
 								wall
 							</span>
 						</aticle>
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+						<aticle className="flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
 							<span className="text-xl font-semibold text-white uppercase">
 								roof
 							</span>
 						</aticle>
 					</div>
 					<div className="flex gap-x-10 justify-center">
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+						<aticle className="flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
 							<span className="text-xl font-semibold text-white uppercase">
 								floor
 							</span>
 						</aticle>
-						<aticle className="inline-block w-52 h-52 rounded-2xl bg-gray-400">
+						<aticle className="flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
 							<span className="text-xl font-semibold text-white uppercase">
 								window
 							</span>
