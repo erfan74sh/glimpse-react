261c0952a35e71732c40eb259c90764d419731ba
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-07 15:58:18 +0330

    create Building component

--- src/pages/Estimation/Estimate/VisualSteps/SitePlan/SitePlan3D/SitePlan3D.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/SitePlan/SitePlan3D/SitePlan3D.jsx
@@ -5,8 +5,17 @@ import { OrbitControls } from "@react-three/drei";
 // state
 import { selectInput } from "../../../../../../features/data/inputDataSlice";
 
-const Material3D = () => {
+const Building = () => {
 	const mesh = useRef(null);
+	return (
+		<mesh ref={mesh}>
+			<boxBufferGeometry attach="geometry" args={[4 / 10, 3.5 / 10, 6 / 10]} />
+			<meshStandardMaterial attach="material" color="#4d6ab9" />
+		</mesh>
+	);
+};
+
+const Material3D = () => {
 	return <div>3d</div>;
 };
 
