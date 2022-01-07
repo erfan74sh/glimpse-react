711bd99ff59eb40dca363507495d558ab1ecac17
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-07 18:19:53 +0330

    change title of SitePlanVisual component

--- src/pages/Estimation/Estimate/VisualSteps/SitePlan/VisualSitePlan.jsx
+++ src/pages/Estimation/Estimate/VisualSteps/SitePlan/VisualSitePlan.jsx
@@ -6,7 +6,7 @@ const VisualSitePlan = () => {
 	return (
 		<>
 			<h2 className=" mb-9 mx-8 pb-4 border-b text-xl font-normal uppercase border-gray-500">
-				details
+				section
 			</h2>
 			<div className="">
 				<div className="w-full px-10 h-96">
