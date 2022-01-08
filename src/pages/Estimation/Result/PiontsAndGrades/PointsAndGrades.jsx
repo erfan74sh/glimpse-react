8bfd045527f05429f013a4f4b898c950c6cf2f30
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 19:50:57 +0330

    add heating load grade and style ul element

--- src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
@@ -3,9 +3,17 @@ import React from "react";
 const PointsAndGrades = () => {
 	return (
 		<div className="px-8 py-7 rounded-md shadow-full bg-white">
-			<ul>
+			<ul className="flex flex-col gap-y-3">
 				<li className="flex gap-x-1">
-					<span className="font-medium text-blue-550 capitalize">{`_cooling load >`}</span>
+					<span className="font-medium text-blue-550 capitalize">{`cooling load >`}</span>
+					<span>
+						your Alternative can earn{" "}
+						<span className="font-medium text-blue-550">EC++</span> from 19th
+						topic of National Building Regulations of Iran
+					</span>
+				</li>
+				<li className="flex gap-x-1">
+					<span className="font-medium text-blue-550 capitalize">{`heating load >`}</span>
 					<span>
 						your Alternative can earn{" "}
 						<span className="font-medium text-blue-550">EC++</span> from 19th
