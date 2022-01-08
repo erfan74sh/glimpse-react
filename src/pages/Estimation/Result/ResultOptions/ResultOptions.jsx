168cecab937f5b2f23da627129d144ff88474379
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 20:55:46 +0330

    show new zone input based on state

--- src/pages/Estimation/Result/ResultOptions/ResultOptions.jsx
+++ src/pages/Estimation/Result/ResultOptions/ResultOptions.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Link } from "react-router-dom";
 
 const ResultOptions = () => {
+	const [showNewZone, setShowNewZone] = useState(false);
 	return (
 		<section className="flex gap-x-8">
-			<div className="flex gap-x-4">
-				<button className="p-1 py-1.5 w-36 text-sm text-white bg-blue-550 uppercase rounded-md">
+			<div className="flex gap-x-4 overflow-hidden">
+				<button
+					className="p-1 py-1.5 w-36 text-sm text-white bg-blue-550 uppercase rounded-md"
+					onClick={() => setShowNewZone(!showNewZone)}
+				>
 					new zone
 				</button>
-				<div className="flex gap-x-4 items-baseline">
+				<div
+					className={` ${
+						showNewZone ? "max-w-3xl" : "w-0"
+					} flex gap-x-4 items-baseline`}
+				>
 					<input
 						type="text"
 						placeholder="enter zone name"
