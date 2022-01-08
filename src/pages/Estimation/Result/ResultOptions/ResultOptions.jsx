df7b7ac1bc46a52a45073005cae5d7667e0e81f6
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 20:37:00 +0330

    create btns

--- src/pages/Estimation/Result/ResultOptions/ResultOptions.jsx
+++ src/pages/Estimation/Result/ResultOptions/ResultOptions.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+// import { Link } from "react-router-dom";
 
 const ResultOptions = () => {
-	return <div>options</div>;
+	return (
+		<section>
+			<div>
+				<button>new zone</button>
+			</div>
+			<div>
+				<button>new alternative</button>
+			</div>
+			<div>
+				<button>compare zones</button>
+			</div>
+		</section>
+	);
 };
 
 export default ResultOptions;
