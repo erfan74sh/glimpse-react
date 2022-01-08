c31b05a8e74f9c6a726c139b4f8c50bed2b5c2c9
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 12:42:42 +0330

    use pigeon map for estimation modal

--- src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
+++ src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { Map, Marker } from "pigeon-maps";
 
 const mapContainerStyle = {
 	width: "65vw",
 	height: "55vh",
 };
-const center = {
-	lat: 32.42791,
-	lng: 53.688046,
-};
+const center = [32.42791, 53.688046];
 
 const markers = [
 	{ lat: 32.654629, lng: 51.667984 },
@@ -16,32 +13,15 @@ const markers = [
 	{ lat: 29.591768, lng: 52.583698 },
 ];
 
-const libraries = ["places"];
-
 const ModalMap = () => {
-	const { isLoaded, loadError } = useLoadScript({
-		googleMapsApiKey: "AIzaSyAJ141ObenmuFdj9mIj7YogQBoS7vstB0U",
-		libraries,
-	});
-	if (loadError) return "error loading map";
-	if (!isLoaded) return "loading map";
-
 	return (
 		<div>
-			<GoogleMap mapContainerStyle={mapContainerStyle} zoom={5} center={center}>
-				{markers.map((marker, idx) => (
-					<Marker
-						key={idx}
-						position={{ lat: marker.lat, lng: marker.lng }}
-						icon={{
-							url: "/MapMarker.svg",
-							scaledSize: new window.google.maps.Size(20, 20),
-							origin: new window.google.maps.Point(0, 0),
-							anchor: new window.google.maps.Point(10, 10),
-						}}
-					/>
-				))}
-			</GoogleMap>
+			<Map
+				height={mapContainerStyle.height}
+				width={mapContainerStyle.width}
+				defaultCenter={center}
+				defaultZoom={5}
+			></Map>
 		</div>
 	);
 };
