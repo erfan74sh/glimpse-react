cff1bedccf3a09430506a7e4f715d61690a892f2
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 13:45:02 +0330

    use formik context to get location value

--- src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
+++ src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map, Marker } from "pigeon-maps";
+import { useFormikContext } from "formik";
 
 const mapContainerStyle = {
 	width: "65vw",
 	height: "55vh",
 };
-const center = [32.42791, 53.688046];
+const initialCenter = [32.42791, 53.688046];
 
 const markers = [
 	{ name: "tehran", latLng: [35.689198, 51.388973] },
@@ -15,18 +16,34 @@ const markers = [
 ];
 
 const ModalMap = () => {
+	const [center, setCenter] = useState(initialCenter);
+	const [zoom, setZoom] = useState(5);
+	const { values } = useFormikContext();
+	console.log(values);
 	return (
 		<div>
 			<Map
 				height={mapContainerStyle.height}
 				width={mapContainerStyle.width}
-				defaultCenter={center}
-				defaultZoom={5}
+				center={center}
+				zoom={zoom}
+				onBoundsChanged={({ center, zoom }) => {
+					setCenter(center);
+					setZoom(zoom);
+				}}
 			>
 				{markers.map((marker, idx) => (
 					<Marker anchor={marker.latLng} key={idx} />
 				))}
 			</Map>
+			<button
+				onClick={() => {
+					setCenter([36.514051, 53.0484]);
+					setZoom(7);
+				}}
+			>
+				change
+			</button>
 		</div>
 	);
 };
