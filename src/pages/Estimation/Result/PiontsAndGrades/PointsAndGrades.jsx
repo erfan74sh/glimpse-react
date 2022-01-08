57a4a29bdffc48fe33587b7269ebe22f9d01cc8d
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 19:37:34 +0330

    style header of ponts and grades

--- src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const PointsAndGrades = () => {
-	return <div>points and grades</div>;
+	return (
+		<div className="px-8 py-7 rounded-md shadow-full bg-white">
+			points and grades
+		</div>
+	);
 };
 
 export default PointsAndGrades;
