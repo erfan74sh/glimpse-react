fc739df4b9a1e8ae368c4ff1d7fbcc14c7349220
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 19:46:36 +0330

    add grades item

--- src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const PointsAndGrades = () => {
 	return (
 		<div className="px-8 py-7 rounded-md shadow-full bg-white">
-			points and grades
+			<ul>
+				<li>
+					<span>{`_cooling load >`}</span>
+					<span>
+						your Alternative can earn <span>EC++</span> from 19th topic of
+						National Building Regulations of Iran
+					</span>
+				</li>
+			</ul>
 		</div>
 	);
 };
