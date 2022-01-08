e8a459aa258b330987cc562c82cef1f2051caa82
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-08 12:49:11 +0330

    render markers via mapping through all markers available

--- src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
+++ src/pages/Home/EstimationModal/ModalMap/ModalMap.jsx
@@ -8,9 +8,10 @@ const mapContainerStyle = {
 const center = [32.42791, 53.688046];
 
 const markers = [
-	{ lat: 32.654629, lng: 51.667984 },
-	{ lat: 38.096237, lng: 46.2738 },
-	{ lat: 29.591768, lng: 52.583698 },
+	{ name: "tehran", latLng: [32.654629, 51.667984] },
+	{ name: "yazd", latLng: [38.096237, 46.2738] },
+	{ name: "bushehr", latLng: [29.591768, 52.583698] },
+	{ name: "mazandaran", latLng: [29.591768, 52.583698] },
 ];
 
 const ModalMap = () => {
@@ -21,7 +22,11 @@ const ModalMap = () => {
 				width={mapContainerStyle.width}
 				defaultCenter={center}
 				defaultZoom={5}
-			></Map>
+			>
+				{markers.map((marker, idx) => (
+					<Marker anchor={marker.latLng} key={idx} />
+				))}
+			</Map>
 		</div>
 	);
 };
