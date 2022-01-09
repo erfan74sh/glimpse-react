cd74169f7f698effe6e446f823c4028898cc6e21
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-09 18:27:56 +0330

    style dropdown list and dropdown main

--- src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
+++ src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// icons
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const DropdownMenu = ({ items, name }) => {
 	return (
-		<div>
-			<ul>
+		<div className="relative flex justify-between items-center px-2 py-1.5 w-36 text-sm text-white bg-gray-350 uppercase rounded-md">
+			<span>selected item</span>
+			<FontAwesomeIcon icon={faCaretDown} />
+			<ul className="absolute flex flex-col gap-y-2 min-w-full left-0 -bottom-1 transform translate-y-full p-2 rounded-md bg-gray-350">
 				{items.map((item, idx) => {
 					return (
 						<li key={idx}>
