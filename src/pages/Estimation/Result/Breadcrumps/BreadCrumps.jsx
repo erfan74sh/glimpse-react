7980c834fc5f61ec4f7beb108fb806af7c81577f
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-09 18:00:50 +0330

    create DropdownMenu component

--- src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
+++ src/pages/Estimation/Result/Breadcrumps/BreadCrumps.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+const DropdownMenu = ({ options }) => {
+	return (
+		<div>
+			<ul>
+				<li>project 1</li>
+				<li>project 2</li>
+				<li>project 3</li>
+			</ul>
+		</div>
+	);
+};
+
 const BreadCrumps = () => {
 	return <div>sds</div>;
 };
