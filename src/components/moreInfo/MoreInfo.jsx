b4e9f521649f4645d5123801eeff5f77d9ceee26
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-09 13:07:10 +0330

    change font-weight and opacity in moreInfo component

--- src/components/moreInfo/MoreInfo.jsx
+++ src/components/moreInfo/MoreInfo.jsx
@@ -6,7 +6,7 @@ const MoreInfo = ({ moreInfo }) => {
 			<span className="flex items-center justify-center w-4 h-4 text-xs font-bold border border-blue-550 rounded-full text-blue-550 cursor-help">
 				!
 			</span>
-			<span className="hidden w-max px-2 py-1 absolute left-full transform translate-x-1 rounded-md text-white text-xs bg-gray-650 bg-opacity-80 group-hover:block z-10">
+			<span className="hidden w-max px-2 py-1.5 absolute left-full transform translate-x-1 rounded-md text-white text-xs font-medium bg-gray-650 bg-opacity-90 group-hover:block z-10">
 				{moreInfo}
 			</span>
 		</div>
