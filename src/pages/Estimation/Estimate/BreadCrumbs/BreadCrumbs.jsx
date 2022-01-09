0283d434e0690a0035e8f2081375011a21c151f6
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-09 17:36:34 +0330

    change font-color

--- src/pages/Estimation/Estimate/BreadCrumbs/BreadCrumbs.jsx
+++ src/pages/Estimation/Estimate/BreadCrumbs/BreadCrumbs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BreadCrumbs = () => {
 	return (
-		<ul className="flex gap-x-1 text-xl">
+		<ul className="flex gap-x-1 text-xl text-gray-650">
 			<li>{`project name >`}</li>
 			<li>{`zone name :`}</li>
 			<li>alternative name</li>
