1b2bafdc946fe1453efb77456b274756a3110571
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-09 17:34:35 +0330

    style breadcrumbs ul element

--- src/pages/Estimation/Estimate/BreadCrumbs/BreadCrumbs.jsx
+++ src/pages/Estimation/Estimate/BreadCrumbs/BreadCrumbs.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const BreadCrumbs = () => {
 	return (
-		<ul>
+		<ul className="flex gap-x-1 text-lg">
 			<li>{`project name >`}</li>
-			<li>{`project name :`}</li>
+			<li>{`zone name :`}</li>
 			<li>alternative name</li>
 		</ul>
 	);
