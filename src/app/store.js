99820dd89d532553327771e2f6c0fd2257692d2d
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-11 17:41:57 +0330

    add primaryData to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import inputReducer from "../features/data/inputDataSlice";
 import userInfoSlice from "../features/userInfo/UserInfoSlice";
+import EstimationPrimDataSlice from "../features/estimationPrimData/EstimationPrimDataSlice";
 
 export const store = configureStore({
 	reducer: {
 		input: inputReducer,
 		userInfo: userInfoSlice,
+		primaryData: EstimationPrimDataSlice,
 	},
 });
