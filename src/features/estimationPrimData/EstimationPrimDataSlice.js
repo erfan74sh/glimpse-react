bd3e4cd5b681c5f88528fb6d5aaa794aeae366cd
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-11 15:54:33 +0330

    create primData slice for estimation

---
+++ src/features/estimationPrimData/EstimationPrimDataSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	highPerformanceBuildings: "",
+	subset: "",
+	buildingProgram: "",
+	projectName: "",
+	zoneName: "",
+	alternativeName: "",
+	location: "",
+};
