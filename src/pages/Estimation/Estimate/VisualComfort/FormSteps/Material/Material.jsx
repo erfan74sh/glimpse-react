73be580babd4f3d8afdc2eec108ae2209e2b591f
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-14 18:00:29 +0330

    rename component and folder

--- src/pages/Estimation/Estimate/VisualComfort/FormSteps/Material/Material.jsx
+++ src/pages/Estimation/Estimate/VisualComfort/FormSteps/Material/Material.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Formik, Form } from "formik";
+import { Form, Formik } from "formik";
 import * as yup from "yup";
 // state
 import {
@@ -13,148 +13,145 @@ import SelectField from "../../../../../../components/inputs/SelectField/SelectF
 const Material = ({ nextStep, prevStep }) => {
 	const data = useSelector(selectInput);
 
-	// const [wallMaterial, setWallMaterial] = useState("");
-	// const [ceilingMaterial, setCeilingMaterial] = useState("");
-	// const [floorMaterial, setFloorMaterial] = useState("");
-	// const [glassMaterial, setGlassMaterial] = useState("");
-
 	const dispatch = useDispatch();
 
-	const handleWallMaterialValue = (v) => {
-		// setWallMaterial(e.target.value);
+	const handleEastWallConditionValue = (v) => {
 		dispatch(
 			updateData({
 				...data,
-				wallMaterial: v,
+				eastWallCondition: v,
+			})
+		);
+	};
+	const handleWestWallConditionValue = (v) => {
+		dispatch(
+			updateData({
+				...data,
+				westWallCondition: v,
+			})
+		);
+	};
+	const handleNorthWallConditionValue = (v) => {
+		dispatch(
+			updateData({
+				...data,
+				northWallCondition: v,
+			})
+		);
+	};
+	const handleSouthWallConditionValue = (v) => {
+		dispatch(
+			updateData({
+				...data,
+				southWallCondition: v,
+			})
+		);
+	};
+	const handleRoofConditionValue = (v) => {
+		dispatch(
+			updateData({
+				...data,
+				roofCondition: v,
+			})
+		);
+	};
+	const handleFloorConditionValue = (v) => {
+		dispatch(
+			updateData({
+				...data,
+				floorCondition: v,
 			})
 		);
 	};
 
-	const handleCeilingMaterialValue = (v) => {
-		// setCeilingMaterial(e.target.value);
-		dispatch(
-			updateData({
-				...data,
-				ceilingMaterial: v,
-			})
-		);
-	};
-
-	const handleFloorMaterialValue = (v) => {
-		// setFloorMaterial(e.target.value);
-		dispatch(
-			updateData({
-				...data,
-				floorMaterial: v,
-			})
-		);
-	};
-
-	const handleGlassMaterialValue = (v) => {
-		// setGlassMaterial(e.target.dataset.value);
-		dispatch(
-			updateData({
-				...data,
-				glassMaterial: v,
-			})
-		);
-	};
-
-	const wallMaterialOptions = [
-		{ label: "1.719" },
-		{ label: "1.649" },
-		{ label: "0.979" },
-		{ label: "0.225" },
-	];
-	const floorMaterialOptions = [
-		{ label: "0.37" },
-		{ label: "0.47" },
-		{ label: "0.65" },
-	];
-	const roofMaterialOptions = [
-		{ label: "0.878" },
-		{ label: "0.211" },
-		{ label: "0.393" },
-		{ label: "0.386" },
-		{ label: "1.092" },
-	];
-	const glassMaterialOptions = [
-		{ label: "0.58" },
-		{ label: "0.72" },
-		{ label: "0.8" },
-		{ label: "0.9" },
+	const WallOptions = [{ label: "adiabatic" }, { label: "outdoor" }];
+	const floorOptions = [
+		{ label: "adiabatic" },
+		{ label: "outdoor" },
+		{ label: "ground" },
 	];
 
 	return (
 		<Formik
 			initialValues={data}
 			onSubmit={(values) => {
-				// todo: edit submit function
+				// todo: edit form submit
 				console.log(values);
 				nextStep();
 			}}
 			validationSchema={yup.object({
-				wallMaterial: yup
+				eastWallCondition: yup
 					.string()
-					.oneOf(["1.719", "1.649", "0.979", "0.225"], "choose from list")
+					.oneOf(["adiabatic", "outdoor"], "choose from list")
 					.required("required"),
-				ceilingMaterial: yup
+				westWallCondition: yup
 					.string()
-					.oneOf(
-						["0.878", "0.211", "0.393", "0.386", "1.092"],
-						"choose from list"
-					)
+					.oneOf(["adiabatic", "outdoor"], "choose from list")
 					.required("required"),
-				floorMaterial: yup
+				northWallCondition: yup
 					.string()
-					.oneOf(["0.37", "0.47", "0.65"], "choose from list")
+					.oneOf(["adiabatic", "outdoor"], "choose from list")
 					.required("required"),
-				glassMaterial: yup
+				southWallCondition: yup
 					.string()
-					.oneOf(["0.58", "0.72", "0.8", "0.9"], "choose one")
+					.oneOf(["adiabatic", "outdoor"], "choose from list")
+					.required("required"),
+				roofCondition: yup
+					.string()
+					.oneOf(["adiabatic", "outdoor"], "choose from list")
+					.required("required"),
+				floorCondition: yup
+					.string()
+					.oneOf(["adiabatic", "outdoor", "ground"], "choose from list")
 					.required("required"),
 			})}
 		>
 			<Form className="h-full flex flex-col">
 				<fieldset className="flex flex-col gap-y-5 pr-10 mb-8">
 					<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
-						material
+						Boundry conditions
 					</legend>
 					<SelectField
-						name="wallMaterial"
-						label="Wall material"
-						selectOptions={wallMaterialOptions}
-						placeholder="select type"
-						moreInfo="ضریب هدایتی دیوار"
-						unit="w/m.k"
-						handleValue={handleWallMaterialValue}
+						name="eastWallCondition"
+						selectOptions={WallOptions}
+						label="Boundry condition east wall"
+						placeholder="choose condition"
+						handleValue={handleEastWallConditionValue}
 					/>
 					<SelectField
-						name="ceilingMaterial"
-						label="roof material"
-						selectOptions={roofMaterialOptions}
-						placeholder="select type"
-						moreInfo="ضریب هدایتی بام"
-						unit="w/m.k"
-						handleValue={handleCeilingMaterialValue}
+						name="westWallCondition"
+						selectOptions={WallOptions}
+						label="Boundry condition west wall"
+						placeholder="choose condition"
+						handleValue={handleWestWallConditionValue}
 					/>
 					<SelectField
-						name="floorMaterial"
-						label="Floor material"
-						selectOptions={floorMaterialOptions}
-						placeholder="select type"
-						moreInfo="ضریب هدایتی کف"
-						unit="w/m.k"
-						handleValue={handleFloorMaterialValue}
+						name="northWallCondition"
+						selectOptions={WallOptions}
+						label="Boundry condition north wall"
+						placeholder="choose condition"
+						handleValue={handleNorthWallConditionValue}
 					/>
 					<SelectField
-						name="glassMaterial"
-						selectOptions={glassMaterialOptions}
-						label="window material"
-						placeholder="select type"
-						moreInfo="ضریب هدایتی پنجره"
-						unit="w/m.k"
-						handleValue={handleGlassMaterialValue}
+						name="southWallCondition"
+						selectOptions={WallOptions}
+						label="Boundry condition south wall"
+						placeholder="choose condition"
+						handleValue={handleSouthWallConditionValue}
+					/>
+					<SelectField
+						name="roofCondition"
+						selectOptions={WallOptions}
+						label="Boundry condition roof"
+						placeholder="choose condition"
+						handleValue={handleRoofConditionValue}
+					/>
+					<SelectField
+						name="floorCondition"
+						selectOptions={floorOptions}
+						label="Boundry condition floor"
+						placeholder="choose condition"
+						handleValue={handleFloorConditionValue}
 					/>
 				</fieldset>
 				<section className="flex justify-center gap-x-4 mt-auto pr-10">
@@ -170,7 +167,7 @@ const Material = ({ nextStep, prevStep }) => {
 						type="submit"
 						className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
 					>
-						next <span className="text-xs lowercase">(site plan)</span>
+						next <span className="text-xs lowercase">(material)</span>
 					</button>
 				</section>
 			</Form>
