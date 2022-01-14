15b42bd1fa7dfa70c3e8b286ae1035bdd959be88
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-14 17:44:02 +0330

    add VisualComfortDataSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import inputReducer from "../features/data/inputDataSlice";
 import userInfoSlice from "../features/userInfo/UserInfoSlice";
 import EstimationPrimDataSlice from "../features/estimationPrimData/EstimationPrimDataSlice";
+import VisualComfortDataSlice from "../features/visualComfortData/VisualComfortDataSlice";
 
 export const store = configureStore({
 	reducer: {
 		input: inputReducer,
 		userInfo: userInfoSlice,
 		primaryData: EstimationPrimDataSlice,
+		visualComfortData: VisualComfortDataSlice,
 	},
 });
