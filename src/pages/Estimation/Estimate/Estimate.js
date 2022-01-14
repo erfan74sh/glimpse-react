e0e8aceb21ed12dfa23af67a05930080c76ae97f
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-14 17:28:51 +0330

    render apropriate component based on subset

--- src/pages/Estimation/Estimate/Estimate.js
+++ src/pages/Estimation/Estimate/Estimate.js
@@ -1,67 +1,16 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { useSelector } from "react-redux";
 // components
-import ProgressBar from "../../../components/progress-bar/ProgressBar";
-import Geometry from "./FormSteps/Geometry";
-import BoundryCondition from "./FormSteps/BoundryCondition/BoundryCondition";
-import SitePlan from "./FormSteps/SitePlan";
-import Material from "./FormSteps/Material";
-import Hvac from "./FormSteps/Hvac/Hvac";
-import Review from "./FormSteps/Review";
-import VisualBoundryCondition from "./VisualSteps/BoundryCondition";
-import VisualGeometry from "./VisualSteps/Geometry";
-import VisualMaterial from "./VisualSteps/Material";
-import VisualSitePlan from "./VisualSteps/SitePlan";
-import VisualHvac from "./VisualSteps/Hvac";
-import VisualReview from "./VisualSteps/Review/VisualReview";
 import BreadCrumbs from "./BreadCrumbs/BreadCrumbs";
-
+import ThermalComfort from "./ThermalComfort";
+import VisualComfort from "./VisualComfort";
 // state
-import { selectInput } from "../../../features/data/inputDataSlice";
 import { selectPrimaryData } from "../../../features/estimationPrimData/EstimationPrimDataSlice";
 // style
 import "./Estimate.scss";
 
 const Estimate = () => {
 	const primaryData = useSelector(selectPrimaryData);
-
-	const inputData = useSelector(selectInput);
-	const navigate = useNavigate();
-	const [step, setStep] = useState(0);
-
-	const handleNextStep = () => {
-		setStep((prev) => prev + 1);
-	};
-
-	const handlePrevStep = () => {
-		setStep((prev) => prev - 1);
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		// todo: send data to server
-		console.log(inputData);
-		navigate("/estimation/result/0");
-	};
-
-	const steps = [
-		<Geometry nextStep={handleNextStep} />,
-		<BoundryCondition nextStep={handleNextStep} prevStep={handlePrevStep} />,
-		<SitePlan nextStep={handleNextStep} prevStep={handlePrevStep} />,
-		<Material nextStep={handleNextStep} prevStep={handlePrevStep} />,
-		<Hvac nextStep={handleNextStep} prevStep={handlePrevStep} />,
-		<Review prevStep={handlePrevStep} />,
-	];
-
-	const visualSteps = [
-		<VisualGeometry />,
-		<VisualBoundryCondition />,
-		<VisualSitePlan />,
-		<VisualMaterial />,
-		<VisualHvac />,
-		<VisualReview />,
-	];
 
 	return (
 		<main className=" px-24 py-10" id="estimate__main">
@@ -78,25 +27,8 @@ const Estimate = () => {
 				</section>
 			</header>
 			<main className="flex">
-				<section className="w-2/5 pr-10 relative">
-					{steps[step]}
-					<div className="absolute right-0 top-1/2 transform -translate-y-1/2 h-3/4">
-						<ProgressBar step={step} />
-					</div>
-				</section>
-				<section className="flex flex-col w-3/5 pl-24">
-					{visualSteps[step]}
-					<button
-						type="submit"
-						form="energy-and-comfort"
-						onClick={(e) => handleSubmit(e)}
-						className={`self-center mt-16 px-10 py-2 text-white font-medium uppercase rounded-md bg-blue-550 ${
-							step !== 5 && "opacity-25 pointer-events-none"
-						}`}
-					>
-						start estimate
-					</button>
-				</section>
+				{primaryData.subset === "thermal comfort" && <ThermalComfort />}
+				{primaryData.subset === "visual comfort" && <VisualComfort />}
 			</main>
 		</main>
 	);
