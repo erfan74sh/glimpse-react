cbb5fe68726d33973d6ee5956158cdec716df9f5
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-16 20:08:47 +0330

    add signUp functionality

---
+++ src/services/auth.service.js
@@ -0,0 +1,17 @@
+import { axios } from "./axios";
+
+const signUp = async (userInfo) => {
+	try {
+		const response = await axios.post("/users/", userInfo);
+		return response;
+	} catch (err) {
+		if (err.response) {
+			// not in the 200 range
+			console.log(err.response.data);
+			console.log(err.response.status);
+			console.log(err.response.headers);
+		} else {
+			console.log(`Error: ${err.message}`);
+		}
+	}
+};
