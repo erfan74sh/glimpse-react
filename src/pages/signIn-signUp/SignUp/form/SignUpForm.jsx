8d6ea760f7a0c929f898f8c034f74600c8113589
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-17 13:38:30 +0330

    add signIn service on form submit

--- src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
+++ src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import * as yup from "yup";
+// import Axios from "axios";
+import authService from "../../../../services/auth.service";
 // components
 import TextField from "../../../../components/inputs/TextField/TextField";
 
 const SignUpForm = () => {
+	const register = async () => {
+		const test = {
+			email: "erfan12323sh@gmail.com",
+			scope_of_activity: "Architect",
+			phone_number: "string",
+			full_name: "erfan shaf",
+			password: "123456",
+		};
+		try {
+			await authService.signUp(test);
+		} catch (err) {
+			// console.log(err);
+			console.log("eeeeeeeeeeeer");
+		}
+		// const response = await Axios.post(
+		// 	"https://bspsim.app/api/v1/users/",
+		// 	test
+		// ).catch((err) => console.log("Error:", err));
+		// console.log(response);
+	};
+
 	const validate = yup.object({
 		firstName: yup
 			.string()
@@ -30,7 +53,10 @@ const SignUpForm = () => {
 				password: "",
 			}}
 			validationSchema={validate}
-			onSubmit={(values) => console.log(values)}
+			onSubmit={(values) => {
+				console.log(values);
+				register();
+			}}
 		>
 			<Form className="flex flex-col items-center gap-y-7">
 				<TextField type="text" name="firstName" placeholder="first name" />
