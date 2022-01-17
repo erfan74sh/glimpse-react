09cd44d5ed9c222a1375eaafce17759f061ec2fd
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-17 13:57:27 +0330

    change fields and initial formik value

--- src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
+++ src/pages/signIn-signUp/SignUp/form/SignUpForm.jsx
@@ -5,6 +5,7 @@ import * as yup from "yup";
 import authService from "../../../../services/auth.service";
 // components
 import TextField from "../../../../components/inputs/TextField/TextField";
+import SelectField from "../../../../components/inputs/SelectField";
 
 const SignUpForm = () => {
 	const register = async () => {
@@ -28,29 +29,32 @@ const SignUpForm = () => {
 		// console.log(response);
 	};
 
+	const scopeOfActivityOptions = [{ label: "Architect" }];
+
 	const validate = yup.object({
-		firstName: yup
-			.string()
-			.max(15, "must be 15 characters or less")
-			.required("required"),
-		lastName: yup
-			.string()
-			.max(15, "must be 15 characters or less")
-			.required("required"),
-		email: yup.string().email("email is invalid").required("required"),
-		password: yup
-			.string()
-			.min(6, "password must be at least 6 characters")
-			.max(20, "password must be 20 characters or less")
-			.required("required"),
+		// firstName: yup
+		// 	.string()
+		// 	.max(15, "must be 15 characters or less")
+		// 	.required("required"),
+		// lastName: yup
+		// 	.string()
+		// 	.max(15, "must be 15 characters or less")
+		// 	.required("required"),
+		// email: yup.string().email("email is invalid").required("required"),
+		// password: yup
+		// 	.string()
+		// 	.min(6, "password must be at least 6 characters")
+		// 	.max(20, "password must be 20 characters or less")
+		// 	.required("required"),
 	});
 	return (
 		<Formik
 			initialValues={{
-				firstName: "",
-				lastName: "",
+				"full-name": "",
 				email: "",
 				password: "",
+				"phone-number": "",
+				"scope-of-activity": "",
 			}}
 			validationSchema={validate}
 			onSubmit={(values) => {
@@ -59,10 +63,37 @@ const SignUpForm = () => {
 			}}
 		>
 			<Form className="flex flex-col items-center gap-y-7">
-				<TextField type="text" name="firstName" placeholder="first name" />
-				<TextField type="text" name="lastName" placeholder="last name" />
-				<TextField type="email" name="email" placeholder="email" />
-				<TextField type="password" name="password" placeholder="password" />
+				<TextField
+					type="text"
+					name="full-name"
+					placeholder="fullname"
+					label="fullname"
+				/>
+				<TextField
+					type="email"
+					name="email"
+					placeholder="email"
+					label="email"
+				/>
+				<TextField
+					type="password"
+					name="password"
+					placeholder="password"
+					label="password"
+				/>
+				<TextField
+					type="text"
+					name="phone-number"
+					placeholder="phone number"
+					label="phone number"
+				/>
+				<SelectField
+					selectOptions={scopeOfActivityOptions}
+					name="scope-of-activity"
+					label="scope of activity"
+					placeholder="select option"
+					handleValue={() => null}
+				/>
 				<button
 					type="submit"
 					className="py-1 px-4 rounded-md capitalize bg-blue-550 text-white transition-all hover:shadow-b-sm"
