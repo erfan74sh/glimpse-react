c72ebc6851a8b935cfaea0f949ff061d4c837232
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-20 13:19:32 +0330

    fix bug in declare function

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,8 +1,8 @@
-export default authHeader = () => {
+export default function authHeader() {
 	const user = JSON.parse(localStorage.getItem("user"));
 	if (user && user["access_token"]) {
 		return { Authorization: `${user["token_type"]} ${user["access_token"]}` };
 	} else {
 		return {};
 	}
-};
+}
