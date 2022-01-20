6e6f1d5db3034717a186d1b16abce6a2375bd224
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-20 16:43:02 +0330

    get user fullname from api

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import authService from "../../services/auth.service";
 // asset
 import AvatarImg from "../../assets/avatars/avatar-01.png";
 
 const Profile = () => {
+	const [userFullame, setUserFullname] = useState("");
+	useEffect(() => {
+		authService.getCurrentUser().then(
+			(response) => {
+				setUserFullname(response.data["full_name"]);
+				console.log(response.data);
+			},
+			(error) => {
+				console.log("errore", error.response);
+				if (ErrorEvent.response && ErrorEvent.response.status === 403) {
+					console.log("unvalid");
+				}
+			}
+		);
+	}, []);
 	return (
 		<button type="button" className="flex gap-x-6 items-center">
-			<span className="text-lg">User name</span>
+			<span className="text-lg">
+				{userFullame !== "" ? userFullame : "not login"}
+			</span>
 			<img src={AvatarImg} alt="avatr" className="w-10 h-10 object-cover" />
 		</button>
 	);
