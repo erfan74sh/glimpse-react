5d52db29faef349e80819f93fca639cf0d720a55
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-20 12:34:01 +0330

    add else statement if there is no user in localStorage

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -4,5 +4,7 @@ export default authHeader = () => {
 	const user = authService.getCurrentUser();
 	if (user && user["access_token"]) {
 		return { Authorization: `${user["token_type"]} ${user["access_token"]}` };
+	} else {
+		return {};
 	}
 };
