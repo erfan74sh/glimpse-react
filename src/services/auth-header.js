a2ff4165696788d49d4b97da523ee44634201203
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-23 01:02:13 +0330

    get access token from "userAuth" in localStorage

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -1,5 +1,5 @@
 export default function authHeader() {
-	const user = JSON.parse(localStorage.getItem("user"));
+	const user = JSON.parse(localStorage.getItem("userAuth"));
 	if (user && user["access_token"]) {
 		return { Authorization: `${user["token_type"]} ${user["access_token"]}` };
 	} else {
