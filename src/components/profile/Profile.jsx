76e7f5c2532c1fdbe4793f10251b124413d157e9
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-23 01:01:46 +0330

    show profile name from redux store

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import authService from "../../services/auth.service";
 // asset
 import AvatarImg from "../../assets/avatars/avatar-01.png";
+// state
+import { selectUser } from "../../features/auth/authSlice";
 
 const Profile = () => {
 	const [userFullame, setUserFullname] = useState("");
+	const { isLoggedIn, user } = useSelector(selectUser);
 	useEffect(() => {
-		authService.getCurrentUser().then(
-			(response) => {
-				setUserFullname(response.data["full_name"]);
-				console.log(response.data);
-			},
-			(error) => {
-				console.log("errore", error.response);
-				if (ErrorEvent.response && ErrorEvent.response.status === 403) {
-					console.log("unvalid");
-				}
-			}
-		);
+		// authService.getCurrentUser().then(
+		// 	(response) => {
+		// 		setUserFullname(response.data["full_name"]);
+		// 		console.log(response.data);
+		// 	},
+		// 	(error) => {
+		// 		console.log("errore", error.response);
+		// 		if (ErrorEvent.response && ErrorEvent.response.status === 403) {
+		// 			console.log("unvalid");
+		// 		}
+		// 	}
+		// );
 	}, []);
 	return (
 		<button type="button" className="flex gap-x-6 items-center">
 			<span className="text-lg">
-				{userFullame !== "" ? userFullame : "not login"}
+				{isLoggedIn ? user["full_name"] : "not login"}
 			</span>
 			<img src={AvatarImg} alt="avatr" className="w-10 h-10 object-cover" />
 		</button>
