dbd3a68ff8a4653269f149d5f20d146a7fed8f0d
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-22 12:14:40 +0330

    change logIn function

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -17,23 +17,29 @@ const signUp = async (userInfo) => {
 	}
 };
 
-const logIn = async (userInfo) => {
-	try {
-		const response = await axios.post("/login/access-token", userInfo);
-		if (response && response.data["access_token"]) {
+const logIn = (userInfo) => {
+	// try {
+	// 	const response = await axios.post("/login/access-token", userInfo);
+	// 	if (response && response.data["access_token"]) {
+	// 		localStorage.setItem("user", JSON.stringify(response.data));
+	// 	}
+	// 	return response;
+	// } catch (err) {
+	// 	if (err.response) {
+	// 		// not in the 200 range
+	// 		console.log(err.response.data);
+	// 		console.log(err.response.status);
+	// 		console.log(err.response.headers);
+	// 	} else {
+	// 		console.log(`Error: ${err.message}`);
+	// 	}
+	// }
+	return axios.post("/login/access-token", userInfo).then((response) => {
+		if (response.data["access_token"]) {
 			localStorage.setItem("user", JSON.stringify(response.data));
 		}
-		return response;
-	} catch (err) {
-		if (err.response) {
-			// not in the 200 range
-			console.log(err.response.data);
-			console.log(err.response.status);
-			console.log(err.response.headers);
-		} else {
-			console.log(`Error: ${err.message}`);
-		}
-	}
+		return response.data;
+	});
 };
 
 const logOut = () => {
