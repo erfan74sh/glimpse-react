a97c0d116925cbf64991dedb949b2d6bd477a9a2
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-22 12:38:47 +0330

    add message to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import inputReducer from "../features/data/inputDataSlice";
 import userInfoSlice from "../features/userInfo/UserInfoSlice";
 import EstimationPrimDataSlice from "../features/estimationPrimData/EstimationPrimDataSlice";
 import VisualComfortDataSlice from "../features/visualComfortData/VisualComfortDataSlice";
+import messageSlice from "../features/message/messageSlice";
 
 export const store = configureStore({
 	reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
 		userInfo: userInfoSlice,
 		primaryData: EstimationPrimDataSlice,
 		visualComfortData: VisualComfortDataSlice,
+		message: messageSlice,
 	},
 });
