9d070610676225f64fb1366c78637800e2a6e36e
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-22 12:16:40 +0330

    change signUp function

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,20 +1,21 @@
 import authHeader from "./auth-header";
 import { axios } from "./axios";
 
-const signUp = async (userInfo) => {
-	try {
-		const response = await axios.post("/users/", userInfo);
-		return response;
-	} catch (err) {
-		if (err.response) {
-			// not in the 200 range
-			console.log(err.response.data);
-			console.log(err.response.status);
-			console.log(err.response.headers);
-		} else {
-			console.log(`Error: ${err.message}`);
-		}
-	}
+const signUp = (userInfo) => {
+	// try {
+	// 	const response = await axios.post("/users/", userInfo);
+	// 	return response;
+	// } catch (err) {
+	// 	if (err.response) {
+	// 		// not in the 200 range
+	// 		console.log(err.response.data);
+	// 		console.log(err.response.status);
+	// 		console.log(err.response.headers);
+	// 	} else {
+	// 		console.log(`Error: ${err.message}`);
+	// 	}
+	// }
+	return axios.post("/users/", userInfo);
 };
 
 const logIn = (userInfo) => {
