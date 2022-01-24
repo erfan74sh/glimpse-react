72948ea496b96371d47d45f5075a6428f7eb9aa6
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-24 20:38:40 +0330

    show error message in register proccess

--- src/pages/signIn-signUp/SignUp/SignUp.jsx
+++ src/pages/signIn-signUp/SignUp/SignUp.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 // assets
 import Logo from "../../../assets/images/logo-02.png";
 import SignUpForm from "./form/SignUpForm";
+// slice
+import {
+	selectMessage,
+	clearMessage,
+} from "../../../features/message/messageSlice";
 
 const SignUp = () => {
+	const { message } = useSelector(selectMessage);
 	return (
 		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
 			<img src={Logo} alt="logo" className="w-60 h-auto" />
 			<SignUpForm />
+			{message && <div className="text-red-500">{message}</div>}
 			<span className="text-lg font-normal text-blue-550 capitalize">
 				or{" "}
 				<Link to="/auth/sign-in" className="underline">
