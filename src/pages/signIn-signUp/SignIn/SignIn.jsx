d4b16f4257206270648adf62ebe879c90e4b6354
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-24 20:24:01 +0330

    show errore message from server

--- src/pages/signIn-signUp/SignIn/SignIn.jsx
+++ src/pages/signIn-signUp/SignIn/SignIn.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 // components
 import SignInForm from "./form";
 // assets
 import Logo from "../../../assets/images/logo-02.png";
+// slice
+import { selectMessage } from "../../../features/message/messageSlice";
 
 const SignIn = () => {
+	const { message } = useSelector(selectMessage);
+	console.log(message);
 	return (
 		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
 			<img src={Logo} alt="logo" className="w-60 h-auto" />
 			<SignInForm />
+			{message && <div>{message}</div>}
 			<Link to="#" className="text-sm text-gray-650 font-normal capitalize">
 				forget password?
 			</Link>
