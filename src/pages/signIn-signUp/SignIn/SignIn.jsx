12ff7f91a789ca59b5c29d7ea24c9473cf665741
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-24 20:29:31 +0330

    style errore message

--- src/pages/signIn-signUp/SignIn/SignIn.jsx
+++ src/pages/signIn-signUp/SignIn/SignIn.jsx
@@ -15,10 +15,12 @@ const SignIn = () => {
 		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
 			<img src={Logo} alt="logo" className="w-60 h-auto" />
 			<SignInForm />
-			{message && <div>{message}</div>}
-			<Link to="#" className="text-sm text-gray-650 font-normal capitalize">
-				forget password?
-			</Link>
+			<div className="flex flex-col gap-y-2 items-center">
+				{message && <div className="text-red-500">{message}</div>}
+				<Link to="#" className="text-sm text-gray-650 font-normal capitalize">
+					forget password?
+				</Link>
+			</div>
 			<span className="text-lg font-normal text-blue-550 capitalize">
 				or{" "}
 				<Link to="/auth/sign-up" className="underline">
