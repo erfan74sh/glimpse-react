37444f4bf8259fac0029bd84df28e9a576a61649
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-24 18:37:32 +0330

    create handleLogout function

--- src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
+++ src/components/dropdowns/dropdownProfile/DropdownProfile.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 //
 // assets
 import ProfileIcon from "../../../assets/icons/Profile.svg";
 import ChargeIcon from "../../../assets/icons/charge.svg";
 import SignOutIcon from "../../../assets/icons/sign-out.svg";
 import ProjectHistory from "../../../assets/icons/project-history.png";
+// services
+import { logout } from "../../../features/auth/authSlice";
 
 const DropdownProfile = ({ showDropdown, handleShowProjectHistoryModal }) => {
+	const dispatch = useDispatch();
+	const handleLogout = () => {
+		dispatch(logout());
+	};
+
 	return (
 		<ul
 			className={`${
@@ -38,7 +46,11 @@ const DropdownProfile = ({ showDropdown, handleShowProjectHistoryModal }) => {
 				</Link>
 			</li>
 			<li>
-				<Link to="/auth" className="flex gap-x-4 px-2 py-4">
+				<Link
+					to="/auth"
+					className="flex gap-x-4 px-2 py-4"
+					onClick={handleLogout}
+				>
 					<img src={SignOutIcon} alt="sign out icon" className="w-6 h-auto" />
 					<span className="text-red-350">logout</span>
 				</Link>
