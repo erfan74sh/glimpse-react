7da43f0495cf891b3063e275c7c4ea650f585010
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-26 12:25:48 +0330

    dynamic title for estimation title

--- src/pages/Estimation/Estimate/Estimate.js
+++ src/pages/Estimation/Estimate/Estimate.js
@@ -16,7 +16,7 @@ const Estimate = () => {
 		<main className=" px-24 py-10" id="estimate__main">
 			<header className="mb-10">
 				<h1 className="flex justify-between pl-3 leading-8 border-l-8 border-blue-550 text-blue-550 text-2xl font-bold uppercase">
-					energy and comfort
+					{primaryData.subset}
 				</h1>
 				<section className="px-5 pt-10 pb-4">
 					<BreadCrumbs
