ba317cd6ebaa65e27d1c787843f4a65f08052583
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 13:24:17 +0330

    use energyConsumptionData for inputs in VisualMaterial comp

--- src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Material/VisualMaterial.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Material/VisualMaterial.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // state
-import { selectInput } from "../../../../../../features/data/inputDataSlice";
+import { selectEnergyConsumptionData } from "../../../../../../features/energyConsumptionData/energyConsumptionsDataSlice";
 // assets
 import wall1 from "../../../../../../assets/images/materials/wall1.png";
 import wall2 from "../../../../../../assets/images/materials/wall2.png";
@@ -24,7 +24,7 @@ import win4 from "../../../../../../assets/images/materials/win4.png";
 import { faExpand } from "@fortawesome/free-solid-svg-icons";
 
 const VisualMaterial = () => {
-	const data = useSelector(selectInput);
+	const data = useSelector(selectEnergyConsumptionData);
 	const wallImages = [
 		{ val: "1.719", url: wall1 },
 		{ val: "1.649", url: wall2 },
@@ -52,20 +52,20 @@ const VisualMaterial = () => {
 
 	return (
 		<>
-			<h2 className=" mb-9 mx-8 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+			<h2 className=" mx-8 mb-9 border-b border-gray-500 pb-4 text-xl font-normal uppercase">
 				details
 			</h2>
 			<div className="">
-				<div className="w-full flex flex-col gap-y-10 justify-center px-10 pt-5">
-					<div className="flex gap-x-10 justify-center">
-						<article className="relative flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
+				<div className="flex w-full flex-col justify-center gap-y-10 px-10 pt-5">
+					<div className="flex justify-center gap-x-10">
+						<article className="relative flex h-52 w-52 items-center justify-center rounded-2xl bg-gray-400">
 							{data.wallMaterial === 0 ? (
-								<span className="text-xl font-semibold text-white uppercase">
+								<span className="text-xl font-semibold uppercase text-white">
 									wall
 								</span>
 							) : (
 								<div
-									className="w-full h-full relative group"
+									className="group relative h-full w-full"
 									onClick={(e) =>
 										e.currentTarget
 											.getElementsByTagName("img")[0]
@@ -79,23 +79,23 @@ const VisualMaterial = () => {
 											)[0].url
 										}
 										alt="wall 1"
-										className="w-full h-full object-contain"
+										className="h-full w-full object-contain"
 									/>
 									<FontAwesomeIcon
 										icon={faExpand}
-										className="absolute bottom-0 right-0 transform -translate-y-4 -translate-x-4 text-2xl cursor-pointer text-gray-600 transition group-hover:text-gray-700 group-hover:scale-125"
+										className="absolute bottom-0 right-0 -translate-y-4 -translate-x-4 transform cursor-pointer text-2xl text-gray-600 transition group-hover:scale-125 group-hover:text-gray-700"
 									/>
 								</div>
 							)}
 						</article>
-						<article className="flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
+						<article className="flex h-52 w-52 items-center justify-center rounded-2xl bg-gray-400">
 							{data.ceilingMaterial === 0 ? (
-								<span className="text-xl font-semibold text-white uppercase">
+								<span className="text-xl font-semibold uppercase text-white">
 									roof
 								</span>
 							) : (
 								<div
-									className="w-full h-full relative group"
+									className="group relative h-full w-full"
 									onClick={(e) =>
 										e.currentTarget
 											.getElementsByTagName("img")[0]
@@ -109,25 +109,25 @@ const VisualMaterial = () => {
 											)[0].url
 										}
 										alt="wall 1"
-										className="w-full h-full object-contain"
+										className="h-full w-full object-contain"
 									/>
 									<FontAwesomeIcon
 										icon={faExpand}
-										className="absolute bottom-0 right-0 transform -translate-y-4 -translate-x-4 text-2xl cursor-pointer text-gray-600 transition group-hover:text-gray-700 group-hover:scale-125"
+										className="absolute bottom-0 right-0 -translate-y-4 -translate-x-4 transform cursor-pointer text-2xl text-gray-600 transition group-hover:scale-125 group-hover:text-gray-700"
 									/>
 								</div>
 							)}
 						</article>
 					</div>
-					<div className="flex gap-x-10 justify-center">
-						<article className="flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
+					<div className="flex justify-center gap-x-10">
+						<article className="flex h-52 w-52 items-center justify-center rounded-2xl bg-gray-400">
 							{data.floorMaterial === 0 ? (
-								<span className="text-xl font-semibold text-white uppercase">
+								<span className="text-xl font-semibold uppercase text-white">
 									floor
 								</span>
 							) : (
 								<div
-									className="w-full h-full relative group"
+									className="group relative h-full w-full"
 									onClick={(e) =>
 										e.currentTarget
 											.getElementsByTagName("img")[0]
@@ -141,23 +141,23 @@ const VisualMaterial = () => {
 											)[0].url
 										}
 										alt="wall 1"
-										className="w-full h-full object-contain"
+										className="h-full w-full object-contain"
 									/>
 									<FontAwesomeIcon
 										icon={faExpand}
-										className="absolute bottom-0 right-0 transform -translate-y-4 -translate-x-4 text-2xl cursor-pointer text-gray-600 transition group-hover:text-gray-700 group-hover:scale-125"
+										className="absolute bottom-0 right-0 -translate-y-4 -translate-x-4 transform cursor-pointer text-2xl text-gray-600 transition group-hover:scale-125 group-hover:text-gray-700"
 									/>
 								</div>
 							)}
 						</article>
-						<article className="flex justify-center items-center w-52 h-52 rounded-2xl bg-gray-400">
+						<article className="flex h-52 w-52 items-center justify-center rounded-2xl bg-gray-400">
 							{data.glassMaterial === 0 ? (
-								<span className="text-xl font-semibold text-white uppercase">
+								<span className="text-xl font-semibold uppercase text-white">
 									window
 								</span>
 							) : (
 								<div
-									className="w-full h-full relative group"
+									className="group relative h-full w-full"
 									onClick={(e) =>
 										e.currentTarget
 											.getElementsByTagName("img")[0]
@@ -171,11 +171,11 @@ const VisualMaterial = () => {
 											)[0].url
 										}
 										alt="wall 1"
-										className="w-full h-full object-contain"
+										className="h-full w-full object-contain"
 									/>
 									<FontAwesomeIcon
 										icon={faExpand}
-										className="absolute bottom-0 right-0 transform -translate-y-4 -translate-x-4 text-2xl cursor-pointer text-gray-600 transition group-hover:text-gray-700 group-hover:scale-125"
+										className="absolute bottom-0 right-0 -translate-y-4 -translate-x-4 transform cursor-pointer text-2xl text-gray-600 transition group-hover:scale-125 group-hover:text-gray-700"
 									/>
 								</div>
 							)}
