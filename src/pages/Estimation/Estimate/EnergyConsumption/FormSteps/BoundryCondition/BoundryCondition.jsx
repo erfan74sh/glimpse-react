de0e5885d730f9421518d776fa25d1ee7e9606cd
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 13:12:02 +0330

    use energyConsumption data

--- src/pages/Estimation/Estimate/EnergyConsumption/FormSteps/BoundryCondition/BoundryCondition.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/FormSteps/BoundryCondition/BoundryCondition.jsx
@@ -4,14 +4,14 @@ import { Form, Formik } from "formik";
 import * as yup from "yup";
 // state
 import {
-	selectInput,
+	selectEnergyConsumptionData,
 	updateData,
-} from "../../../../../../features/data/inputDataSlice";
+} from "../../../../../../features/energyConsumptionData/energyConsumptionsDataSlice";
 // components
 import SelectField from "../../../../../../components/inputs/SelectField/SelectField";
 
 const BoundryCondition = ({ nextStep, prevStep }) => {
-	const data = useSelector(selectInput);
+	const data = useSelector(selectEnergyConsumptionData);
 
 	const dispatch = useDispatch();
 
@@ -106,9 +106,9 @@ const BoundryCondition = ({ nextStep, prevStep }) => {
 					.required("required"),
 			})}
 		>
-			<Form className="h-full flex flex-col">
-				<fieldset className="flex flex-col gap-y-5 pr-10 mb-8">
-					<legend className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+			<Form className="flex h-full flex-col">
+				<fieldset className="mb-8 flex flex-col gap-y-5 pr-10">
+					<legend className="mb-9 w-full border-b border-gray-500 pb-4 text-xl font-normal uppercase">
 						Boundry conditions
 					</legend>
 					<SelectField
@@ -154,10 +154,10 @@ const BoundryCondition = ({ nextStep, prevStep }) => {
 						handleValue={handleFloorConditionValue}
 					/>
 				</fieldset>
-				<section className="flex justify-center gap-x-4 mt-auto pr-10">
+				<section className="mt-auto flex justify-center gap-x-4 pr-10">
 					<button
 						type="button"
-						className="flex items-center gap-x-1 px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
+						className="text-blue-550 border-blue-550 flex items-center gap-x-1 rounded-md border-2 bg-white px-5 py-1 font-medium uppercase"
 						value="geometry"
 						onClick={prevStep}
 					>
@@ -165,7 +165,7 @@ const BoundryCondition = ({ nextStep, prevStep }) => {
 					</button>
 					<button
 						type="submit"
-						className="flex items-center gap-x-1 px-5 py-1 text-white font-medium uppercase rounded-md bg-blue-550 border-2 border-blue-550"
+						className="bg-blue-550 border-blue-550 flex items-center gap-x-1 rounded-md border-2 px-5 py-1 font-medium uppercase text-white"
 					>
 						next <span className="text-xs lowercase">(material)</span>
 					</button>
