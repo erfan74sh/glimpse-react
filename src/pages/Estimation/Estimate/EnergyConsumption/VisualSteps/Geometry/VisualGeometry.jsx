4c30ea3c6cb61eef2c186321f7bf679e44e8344a
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 13:23:48 +0330

    use energyConsumptionData for inputs in VisualGeometry comp

--- src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Geometry/VisualGeometry.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Geometry/VisualGeometry.jsx
@@ -3,17 +3,17 @@ import { useSelector } from "react-redux";
 // component
 import Model3D from "../../../3D";
 // state
-import { selectInput } from "../../../../../../features/data/inputDataSlice";
+import { selectEnergyConsumptionData } from "../../../../../../features/energyConsumptionData/energyConsumptionsDataSlice";
 
 const VisualGeometry = () => {
-	const inputData = useSelector(selectInput);
+	const inputData = useSelector(selectEnergyConsumptionData);
 	return (
 		<>
-			<h2 className=" mb-9 mx-8 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+			<h2 className=" mx-8 mb-9 border-b border-gray-500 pb-4 text-xl font-normal uppercase">
 				3D view
 			</h2>
 			<div className="">
-				<div className="w-full px-10 h-96">
+				<div className="h-96 w-full px-10">
 					<Model3D
 						xDim={inputData.xDim}
 						yDim={inputData.yDim}
