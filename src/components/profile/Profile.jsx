45d3a9a429941d7c9c049de88c809e9dbff4b9b2
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 18:47:57 +0330

    refactor: show user name if user is loggedIn

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -8,11 +8,9 @@ import { selectUser } from "../../features/auth/authSlice";
 const Profile = () => {
 	const { isLoggedIn, user } = useSelector(selectUser);
 	return (
-		<button type="button" className="flex gap-x-6 items-center">
-			<span className="text-lg">
-				{isLoggedIn ? user["full_name"] : "not login"}
-			</span>
-			<img src={AvatarImg} alt="avatr" className="w-10 h-10 object-cover" />
+		<button type="button" className="flex items-center gap-x-6">
+			<span className="text-lg">{isLoggedIn && user["full_name"]}</span>
+			<img src={AvatarImg} alt="avatr" className="h-10 w-10 object-cover" />
 		</button>
 	);
 };
