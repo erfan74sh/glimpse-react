ce2d99f3ef50d99f72eb209bc88c3e6fbcf9798d
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 13:23:09 +0330

    use energyConsumptionData for inputs in Review comp

--- src/pages/Estimation/Estimate/EnergyConsumption/FormSteps/Review/Review.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/FormSteps/Review/Review.jsx
@@ -1,90 +1,90 @@
 import React from "react";
 import { useSelector } from "react-redux";
 // state
-import { selectInput } from "../../../../../../features/data/inputDataSlice";
+import { selectEnergyConsumptionData } from "../../../../../../features/energyConsumptionData/energyConsumptionsDataSlice";
 
 const Review = ({ prevStep }) => {
-	const data = useSelector(selectInput);
+	const data = useSelector(selectEnergyConsumptionData);
 
 	return (
-		<div className="h-full flex flex-col">
+		<div className="flex h-full flex-col">
 			<section className="flex flex-col pr-10">
-				<h2 className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+				<h2 className="mb-9 w-full border-b border-gray-500 pb-4 text-xl font-normal uppercase">
 					review
 				</h2>
 				<ul className="flex flex-col gap-y-2">
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							floor:
 						</span>
 						<span>{data.floorLevel}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							rotation:
 						</span>
 						<span>{data.rotation}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							width:
 						</span>
 						<span>{data.xDim}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							length:
 						</span>
 						<span>{data.yDim}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							WWR-north:
 						</span>
 						<span>{data.wwrNorth}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							WWR-south:
 						</span>
 						<span>{data.wwrSouth}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							shading type:
 						</span>
 						<span>{data.shadingType}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							wall u-value:
 						</span>
 						<span>{data.wallMaterial}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							roof u-value:
 						</span>
 						<span>{data.ceilingMaterial}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							floor u-value:
 						</span>
 						<span>{data.floorMaterial}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							window u-value:
 						</span>
 						<span>{data.glassMaterial}</span>
 					</li>
 				</ul>
 			</section>
-			<section className="flex justify-center gap-x-4 mt-auto pr-10">
+			<section className="mt-auto flex justify-center gap-x-4 pr-10">
 				<button
 					type="button"
-					className="flex items-center justify-between px-5 py-1 text-blue-550 font-medium uppercase rounded-md bg-white border-2 border-blue-550"
+					className="text-blue-550 border-blue-550 flex items-center justify-between rounded-md border-2 bg-white px-5 py-1 font-medium uppercase"
 					onClick={prevStep}
 					value="site plan"
 				>
