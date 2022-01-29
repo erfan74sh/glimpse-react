a2e12132602a01da5224d6e44db073005a81b26c
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 13:24:28 +0330

    use energyConsumptionData for inputs in VisualReview comp

--- src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Review/VisualReview.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Review/VisualReview.jsx
@@ -1,86 +1,86 @@
 import React from "react";
 import { useSelector } from "react-redux";
 // state
-import { selectInput } from "../../../../../../features/data/inputDataSlice";
+import { selectEnergyConsumptionData } from "../../../../../../features/energyConsumptionData/energyConsumptionsDataSlice";
 
 const VisualReview = () => {
-	const data = useSelector(selectInput);
+	const data = useSelector(selectEnergyConsumptionData);
 
 	return (
-		<div className="h-full flex flex-col">
+		<div className="flex h-full flex-col">
 			<section className="flex flex-col pr-10">
-				<h2 className="w-full mb-9 pb-4 border-b text-xl font-normal uppercase border-gray-500">
+				<h2 className="mb-9 w-full border-b border-gray-500 pb-4 text-xl font-normal uppercase">
 					review
 				</h2>
 				<ul className="flex flex-col gap-y-2">
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							east wall boundry condition:
 						</span>
 						<span>{data.eastWallCondition}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							west wall boundry condition:
 						</span>
 						<span>{data.westWallCondition}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							south wall boundry condition:
 						</span>
 						<span>{data.southWallCondition}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							north wall boundry condition:
 						</span>
 						<span>{data.northWallCondition}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							roof boundry condition:
 						</span>
 						<span>{data.roofCondition}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							floor boundry condition:
 						</span>
 						<span>{data.floorCondition}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							south neighbor distance:
 						</span>
 						<span>{data.southNeighborDist}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							south neighbor height:
 						</span>
 						<span>{data.southNeighborHeight}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							north neighbor distance:
 						</span>
 						<span>{data.northNeighborDist}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							north neighbor height:
 						</span>
 						<span>{data.northNeighborHeight}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							HVAC system:
 						</span>
 						<span>{data.HVAC}</span>
 					</li>
-					<li className="flex justify-between border-b border-gray-400 text-gray-650">
-						<span className="text-gray-900 font-semibold capitalize">
+					<li className="text-gray-650 flex justify-between border-b border-gray-400">
+						<span className="font-semibold capitalize text-gray-900">
 							natural ventilation:
 						</span>
 						<span>{data.naturalVent}</span>
