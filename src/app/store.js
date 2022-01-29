e9520fdb86c46f5dd95e75b1c29bd0815dc3b8a3
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 12:40:33 +0330

    add energyConsumptionData & structureDesignData slices to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,15 +3,19 @@ import inputReducer from "../features/data/inputDataSlice";
 import userInfoSlice from "../features/userInfo/UserInfoSlice";
 import EstimationPrimDataSlice from "../features/estimationPrimData/EstimationPrimDataSlice";
 import VisualComfortDataSlice from "../features/visualComfortData/VisualComfortDataSlice";
+import energyConsumptionDataSlice from "../features/energyConsumptionData/energyConsumptionsDataSlice";
+import structureDesignDataSlice from "../features/structureDesignData/structureDesignDataSlice";
 import messageSlice from "../features/message/messageSlice";
 import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
 	reducer: {
-		input: inputReducer,
 		userInfo: userInfoSlice,
 		primaryData: EstimationPrimDataSlice,
+		input: inputReducer,
 		visualComfortData: VisualComfortDataSlice,
+		energyConsumptionData: energyConsumptionDataSlice,
+		structureDesignData: structureDesignDataSlice,
 		message: messageSlice,
 		auth: authReducer,
 	},
