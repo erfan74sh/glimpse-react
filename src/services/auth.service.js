e2deb7af9bfcb6b5ed4ac1ec20a3036a4ce7a14e
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-29 18:48:09 +0330

    remove commented codes

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,39 +2,10 @@ import authHeader from "./auth-header";
 import { axios } from "./axios";
 
 const signUp = (userInfo) => {
-	// try {
-	// 	const response = await axios.post("/users/", userInfo);
-	// 	return response;
-	// } catch (err) {
-	// 	if (err.response) {
-	// 		// not in the 200 range
-	// 		console.log(err.response.data);
-	// 		console.log(err.response.status);
-	// 		console.log(err.response.headers);
-	// 	} else {
-	// 		console.log(`Error: ${err.message}`);
-	// 	}
-	// }
 	return axios.post("/users/", userInfo);
 };
 
 const logIn = (userInfo) => {
-	// try {
-	// 	const response = await axios.post("/login/access-token", userInfo);
-	// 	if (response && response.data["access_token"]) {
-	// 		localStorage.setItem("user", JSON.stringify(response.data));
-	// 	}
-	// 	return response;
-	// } catch (err) {
-	// 	if (err.response) {
-	// 		// not in the 200 range
-	// 		console.log(err.response.data);
-	// 		console.log(err.response.status);
-	// 		console.log(err.response.headers);
-	// 	} else {
-	// 		console.log(`Error: ${err.message}`);
-	// 	}
-	// }
 	return axios.post("/login/access-token", userInfo).then((response) => {
 		if (response.data["access_token"]) {
 			localStorage.setItem("userAuth", JSON.stringify(response.data));
