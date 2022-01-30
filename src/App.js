fb6d18f3aede2839202dec4c6f37a511a698bbdc
Erfan Shafiee <erfan74sh@gmail.com>
2022-01-30 22:15:18 +0330

    add lazy load for pages and do codeSpliting

--- src/App.js
+++ src/App.js
@@ -1,51 +1,64 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-// pages
-import Home from "./pages/Home";
-// path: estimation
-import Estimation from "./pages/Estimation";
-import Result from "./pages/Estimation/Result";
-import Zone from "./pages/Estimation/Result/Zone/Zone";
-import Estimate from "./pages/Estimation/Estimate";
-import CompareZones from "./pages/Estimation/Result/CompareZones/CompareZones";
 //
-import Profile from "./pages/Profile";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
-import PricingPlans from "./pages/PricingPlans";
-// path: auth
-import SignInSignUp from "./pages/signIn-signUp";
+//
 import SignIn from "./pages/signIn-signUp/SignIn";
 import SignUp from "./pages/signIn-signUp/SignUp";
 // protectedRoute
 import ProtectedRoutes from "./router/ProtectedRoutes";
-// style
+import Result from "./pages/Estimation/Result";
+import Zone from "./pages/Estimation/Result/Zone/Zone";
+import Estimate from "./pages/Estimation/Estimate";
+import CompareZones from "./pages/Estimation/Result/CompareZones/CompareZones";
 import "./App.scss";
+//
+//
+// pages
+const Home = lazy(() => import("./pages/Home"));
+// import Home from "./pages/Home";
+// path: estimation
+const Estimation = lazy(() => import("./pages/Estimation"));
+// import Estimation from "./pages/Estimation";
+//
+const Profile = lazy(() => import("./pages/Profile"));
+// import Profile from "./pages/Profile";
+const AboutUs = lazy(() => import("./pages/AboutUs"));
+// import AboutUs from "./pages/AboutUs";
+const ContactUs = lazy(() => import("./pages/ContactUs"));
+// import ContactUs from "./pages/ContactUs";
+const PricingPlans = lazy(() => import("./pages/PricingPlans"));
+// import PricingPlans from "./pages/PricingPlans";
+// path: auth
+const SignInSignUp = lazy(() => import("./pages/signIn-signUp"));
+// import SignInSignUp from "./pages/signIn-signUp";
+// style
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route element={<ProtectedRoutes />}>
-				<Route path="/estimation" element={<Estimation />}>
-					<Route index element={<Estimate />} />
-					<Route path="estimate" element={<Estimate />} />
-					<Route path="result" element={<Result />}>
-						<Route path=":zoneId" element={<Zone />} />
+		<Suspense fallback={<div>loading</div>}>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route element={<ProtectedRoutes />}>
+					<Route path="/estimation" element={<Estimation />}>
+						<Route index element={<Estimate />} />
+						<Route path="estimate" element={<Estimate />} />
+						<Route path="result" element={<Result />}>
+							<Route path=":zoneId" element={<Zone />} />
+						</Route>
+						<Route path="compare-zones" element={<CompareZones />} />
 					</Route>
-					<Route path="compare-zones" element={<CompareZones />} />
+					<Route path="profile" element={<Profile />} />
 				</Route>
-				<Route path="profile" element={<Profile />} />
-			</Route>
-			<Route path="about-us" element={<AboutUs />} />
-			<Route path="contact-us" element={<ContactUs />} />
-			<Route path="pricing" element={<PricingPlans />} />
-			<Route path="auth" element={<SignInSignUp />}>
-				<Route index element={<SignIn />} />
-				<Route path="sign-in" element={<SignIn />} />
-				<Route path="sign-up" element={<SignUp />} />
-			</Route>
-		</Routes>
+				<Route path="about-us" element={<AboutUs />} />
+				<Route path="contact-us" element={<ContactUs />} />
+				<Route path="pricing" element={<PricingPlans />} />
+				<Route path="auth" element={<SignInSignUp />}>
+					<Route index element={<SignIn />} />
+					<Route path="sign-in" element={<SignIn />} />
+					<Route path="sign-up" element={<SignUp />} />
+				</Route>
+			</Routes>
+		</Suspense>
 	);
 }
 
