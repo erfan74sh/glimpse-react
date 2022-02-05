46c4c622bb5d9cb9d8f37238b386dcd28cc096d1
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-05 18:37:09 +0330

    sort tailwind classes

--- src/pages/AboutUs/memberCard/MemberCard.jsx
+++ src/pages/AboutUs/memberCard/MemberCard.jsx
@@ -3,17 +3,17 @@ import React from "react";
 const MemberCard = ({ fullName, position, job, image }) => {
 	return (
 		<>
-			<div className="relative group cursor-pointer">
+			<div className="group relative cursor-pointer">
 				<img
 					src={image}
 					alt="profile pic"
-					className="w-45 h-45 mb-5 rounded-full group-hover:rounded-none transition-all hover:rounded-none object-cover overflow-hidden"
+					className="w-45 h-45 mb-5 overflow-hidden rounded-full object-cover transition-all hover:rounded-none group-hover:rounded-none"
 				/>
-				<div className="absolute top-0 w-full h-full p-4 bg-white opacity-0 overflow-hidden transition-all group-hover:opacity-70 ">
+				<div className="absolute top-0 h-full w-full overflow-hidden bg-white p-4 opacity-0 transition-all group-hover:opacity-70 ">
 					text about member
 				</div>
 			</div>
-			<h4 className="text-lg font-bold text-blue-550">{fullName}</h4>
+			<h4 className="text-blue-550 text-lg font-bold">{fullName}</h4>
 			<p>{position}</p>
 			<span className="leading-none">{job}</span>
 		</>
