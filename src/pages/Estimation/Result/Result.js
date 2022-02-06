65ee946fa121d8c6342b60f8f6245eb67c1a481d
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-06 17:27:46 +0330

    add navbar to result page

--- src/pages/Estimation/Result/Result.js
+++ src/pages/Estimation/Result/Result.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 // compoents
+import Nav from "../../../components/Nav";
 import BreadCrumps from "./Breadcrumps/BreadCrumps";
 import ResultOptions from "./ResultOptions/ResultOptions";
 
@@ -10,20 +11,23 @@ import "./Result.scss";
 
 const Result = () => {
 	return (
-		<main className=" px-24 py-16" id="result__main">
-			<header className="mb-10">
-				<h1 className="border-blue-550 text-blue-550 border-l-8 pl-3 text-3xl font-bold uppercase leading-8">
-					result
-				</h1>
-				<nav className="mt-14">
-					<BreadCrumps />
-				</nav>
-				<ResultOptions />
-			</header>
-			<main className="">
-				<Outlet />
+		<>
+			<Nav />
+			<main className=" px-24 py-16" id="result__main">
+				<header className="mb-10">
+					<h1 className="border-blue-550 text-blue-550 border-l-8 pl-3 text-3xl font-bold uppercase leading-8">
+						result
+					</h1>
+					<nav className="mt-14">
+						<BreadCrumps />
+					</nav>
+					<ResultOptions />
+				</header>
+				<main className="">
+					<Outlet />
+				</main>
 			</main>
-		</main>
+		</>
 	);
 };
 
