e9401f92bceeb833692e41a455d84bc63fba12b6
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-07 14:00:32 +0330

    create hardcoded alternatives and pass as props to PointsAndGrades component

--- src/pages/Estimation/Result/Zone/Zone.jsx
+++ src/pages/Estimation/Result/Zone/Zone.jsx
@@ -93,6 +93,30 @@ const Zone = () => {
 		setSeries(data);
 	}, []);
 
+	// todo: get alternatives data from server and remove hardcodings
+	const alternatives = [
+		{
+			name: "output 1",
+			id: 1,
+			outputs: {
+				svd: 10,
+				ase: 25,
+				sda: 75,
+				udi: 64,
+			},
+		},
+		{
+			name: "output 2",
+			id: 1,
+			outputs: {
+				svd: 25,
+				ase: 35,
+				sda: 45,
+				udi: 95,
+			},
+		},
+	];
+
 	const handleVisibility = (e) => {
 		const name = e.currentTarget.parentNode.dataset.name;
 		let temState = [...series];
@@ -105,7 +129,7 @@ const Zone = () => {
 			<section className="max-h-152 flex items-center justify-center overflow-hidden">
 				<Chart series={series} />
 			</section>
-			<PointsAndGrades />
+			<PointsAndGrades subset="visual comfort" alternatives={alternatives} />
 			<section className="mt-20 flex h-96 flex-col px-40">
 				<header className="mx-8 mb-9 flex items-center justify-between border-b border-gray-500 pb-2 text-xl font-normal uppercase">
 					<h2>alternatives</h2>
