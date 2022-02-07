cb410813f4d53aedcb56f421373c533bddc585ee
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-07 13:06:30 +0330

    add points and grades title to component

--- src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
@@ -5,13 +5,18 @@ import EnergyConsumptionGrades from "./EnergyConsumptionGrades/EnergyConsumption
 const PointsAndGrades = ({ subset }) => {
 	return (
 		<>
-			{subset !== "thermal comfort" && (
-				<div className="shadow-full rounded-md bg-white px-8 py-7">
-					<ul className="flex flex-col gap-y-3">
-						{subset === "energy consumption" && <EnergyConsumptionGrades />}
-						{subset === "visual comfort" && <VisualComfortGrades />}
-					</ul>
-				</div>
+			{subset && subset !== "thermal comfort" && (
+				<section className="mt-18">
+					<h2 className="mb-4 px-8 text-xl font-normal uppercase">
+						points and grades
+					</h2>
+					<div className="shadow-full rounded-md bg-white px-8 py-7">
+						<ul className="flex flex-col gap-y-3">
+							{subset === "energy consumption" && <EnergyConsumptionGrades />}
+							{subset === "visual comfort" && <VisualComfortGrades />}
+						</ul>
+					</div>
+				</section>
 			)}
 		</>
 	);
