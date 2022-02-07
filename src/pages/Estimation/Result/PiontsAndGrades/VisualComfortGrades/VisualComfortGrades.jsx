0ec99dd0603ccab50194b977a4cdbd95dfa88864
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-07 15:11:21 +0330

    add alt name as title in each article

--- src/pages/Estimation/Result/PiontsAndGrades/VisualComfortGrades/VisualComfortGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/VisualComfortGrades/VisualComfortGrades.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 
-const VisualComfortGrades = () => {
+const VisualComfortGrades = ({ alternatives }) => {
 	return (
 		<>
-			<li className="flex gap-x-1">
-				<span className="text-blue-550 font-medium capitalize">{`cooling load >`}</span>
-				<span>
-					your Alternative can earn{" "}
-					<span className="text-blue-550 font-medium">EC++</span> from 19th
-					topic of National Building Regulations of Iran
-				</span>
-			</li>
-			<li className="flex gap-x-1">
-				<span className="text-blue-550 font-medium capitalize">{`heating load >`}</span>
-				<span>
-					your Alternative can earn{" "}
-					<span className="text-blue-550 font-medium">EC++</span> from 19th
-					topic of National Building Regulations of Iran
-				</span>
-			</li>
+			{alternatives.map((alt) => {
+				return (
+					<article>
+						<h4>{alt.name}</h4>
+						<ul>
+							<li className="flex gap-x-1">
+								<span className="text-blue-550 font-medium capitalize">{`cooling load >`}</span>
+								<span>
+									your Alternative can earn{" "}
+									<span className="text-blue-550 font-medium">EC++</span> from
+									19th topic of National Building Regulations of Iran
+								</span>
+							</li>
+							<li className="flex gap-x-1">
+								<span className="text-blue-550 font-medium capitalize">{`heating load >`}</span>
+								<span>
+									your Alternative can earn{" "}
+									<span className="text-blue-550 font-medium">EC++</span> from
+									19th topic of National Building Regulations of Iran
+								</span>
+							</li>
+						</ul>
+					</article>
+				);
+			})}
 		</>
 	);
 };
