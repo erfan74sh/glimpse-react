c8447539b37f5e53be2bedc5ebc0282c59f43c54
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-08 17:19:23 +0330

    style activityScope text

--- src/pages/AboutUs/memberCard/MemberCard.jsx
+++ src/pages/AboutUs/memberCard/MemberCard.jsx
@@ -10,7 +10,9 @@ const MemberCard = ({ fullName, education, activityScope, image }) => {
 					className="w-45 h-45 mb-5 overflow-hidden rounded-full object-cover transition-all hover:rounded-none group-hover:rounded-none"
 				/>
 				<div className="absolute top-0 flex h-full w-full content-center items-center overflow-hidden bg-white p-4 opacity-0 transition-all group-hover:opacity-70 ">
-					<span className="w-full">{activityScope}</span>
+					<span className="w-full text-lg font-medium text-black">
+						{activityScope}
+					</span>
 				</div>
 			</div>
 			<h4 className="text-blue-550 text-lg font-bold">{fullName}</h4>
