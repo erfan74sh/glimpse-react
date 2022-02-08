5c79c1980b65f479b967616fbb2d87d220ff88b2
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-08 17:17:05 +0330

    edit props name

--- src/pages/AboutUs/memberCard/MemberCard.jsx
+++ src/pages/AboutUs/memberCard/MemberCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MemberCard = ({ fullName, position, job, image }) => {
+const MemberCard = ({ fullName, education, activityScope, image }) => {
 	return (
 		<>
 			<div className="group relative cursor-pointer">
@@ -9,13 +9,12 @@ const MemberCard = ({ fullName, position, job, image }) => {
 					alt="profile pic"
 					className="w-45 h-45 mb-5 overflow-hidden rounded-full object-cover transition-all hover:rounded-none group-hover:rounded-none"
 				/>
-				<div className="absolute top-0 h-full w-full overflow-hidden bg-white p-4 opacity-0 transition-all group-hover:opacity-70 ">
-					text about member
+				<div className="absolute top-0 flex h-full w-full content-center items-center overflow-hidden bg-white p-4 opacity-0 transition-all group-hover:opacity-70 ">
+					<span className="w-full">{activityScope}</span>
 				</div>
 			</div>
 			<h4 className="text-blue-550 text-lg font-bold">{fullName}</h4>
-			<p>{position}</p>
-			<span className="leading-none">{job}</span>
+			<p>{education}</p>
 		</>
 	);
 };
