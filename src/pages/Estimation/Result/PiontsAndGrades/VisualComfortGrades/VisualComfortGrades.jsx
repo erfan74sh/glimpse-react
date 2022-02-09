002201fd8a20eabfda8a7756363d0b41df51a06b
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-09 18:32:23 +0330

    create array of object for alternatives outputs

--- src/pages/Estimation/Result/PiontsAndGrades/VisualComfortGrades/VisualComfortGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/VisualComfortGrades/VisualComfortGrades.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const VisualComfortGrades = ({ alternatives }) => {
+	const [outputData, setOutputData] = useState([]);
+	useEffect(() => {
+		const tempOutput = [];
+		alternatives.forEach((alter) => {
+			let tempData = {};
+			const data = alter.data.filter(
+				(v) =>
+					v.name === "udi" ||
+					v.name === "sda" ||
+					v.name === "ase" ||
+					v.name === "svd"
+			);
+			data.forEach((element) => {
+				tempData[element.name] = element.amt;
+			});
+			tempOutput.push({
+				name: alter.name,
+				id: alter.id,
+				outputs: tempData,
+			});
+		});
+		setOutputData(tempOutput);
+	}, [alternatives]);
+
 	return (
 		<>
-			{alternatives.map((alt, idx) => {
+			{outputData.map((alt, idx) => {
 				const { svd, sda, udi, ase } = alt.outputs;
 				return (
 					<article key={idx}>
