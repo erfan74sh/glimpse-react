9d39dd562bb97331ceed279ccd7b6d0c9c4c59ec
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-10 12:25:41 +0330

    edit conditional rendering conditions

--- src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
@@ -5,17 +5,17 @@ import EnergyConsumptionGrades from "./EnergyConsumptionGrades/EnergyConsumption
 const PointsAndGrades = ({ subset, alternatives }) => {
 	return (
 		<>
-			{subset && subset !== "thermal comfort" && (
+			{subset && subset !== "thermal_comfort" && (
 				<section className="mt-18">
 					<h2 className="mb-4 px-8 text-xl font-normal uppercase">
 						points and grades
 					</h2>
 					<div className="shadow-full flex flex-col gap-y-5 rounded-md bg-white px-8 py-7">
 						<div className="flex flex-col gap-y-3">
-							{subset === "energy consumption" && (
+							{subset === "energy_consumption" && (
 								<EnergyConsumptionGrades alternatives={alternatives} />
 							)}
-							{subset === "visual comfort" && (
+							{subset === "visual_comfort" && (
 								<VisualComfortGrades alternatives={alternatives} />
 							)}
 						</div>
