5c9e90107eb44172f321d05fb08a808a04f66cb7
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-10 21:39:13 +0330

    add title of subset for each subset

--- src/components/ProjectHistoryModal/ProjectsInSubset.jsx
+++ src/components/ProjectHistoryModal/ProjectsInSubset.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 // services
 import visualComfortServices from "../../services/estimations/visualComfort.service";
 import energyConsumptionServices from "../../services/estimations/energyConsumption.service";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 const ProjectsInSubset = ({ subset }) => {
 	const [projectsInSubset, setProjectsInSubset] = useState([]);
@@ -37,27 +39,35 @@ const ProjectsInSubset = ({ subset }) => {
 		fetchData();
 	}, [subset]);
 	return (
-		<ul className="flex flex-col gap-y-5 py-5">
-			{projectsInSubset.map((project, idx) => {
-				return (
-					<li className="w-133 shadow-full-sm rounded-md bg-white" key={idx}>
-						<Link
-							to={{
-								pathname: "/result",
-								search: `?subset=visual_comfort&project_name=${project.project_name}&zone_name=${project.zone_name}`,
-							}}
-							target="_blank"
-							className="flex items-center justify-between p-3 text-gray-600"
-						>
-							<span className="font-semibold ">{`${idx + 1} _ ${
-								project.project_name
-							}`}</span>
-							<span className="text-xs">Last edit :1/2/2022</span>
-						</Link>
-					</li>
-				);
-			})}
-		</ul>
+		<article>
+			<h4 className="flex items-center justify-between border-b-2">
+				<span>{subset}</span>
+				<span>
+					<FontAwesomeIcon icon={faCaretDown} />
+				</span>
+			</h4>
+			<ul className="flex flex-col gap-y-5 py-5 px-2">
+				{projectsInSubset.map((project, idx) => {
+					return (
+						<li className="w-133 shadow-full-sm rounded-md bg-white" key={idx}>
+							<Link
+								to={{
+									pathname: "/result",
+									search: `?subset=visual_comfort&project_name=${project.project_name}&zone_name=${project.zone_name}`,
+								}}
+								target="_blank"
+								className="flex items-center justify-between p-3 text-gray-600"
+							>
+								<span className="font-semibold ">{`${idx + 1} _ ${
+									project.project_name
+								}`}</span>
+								<span className="text-xs">Last edit :1/2/2022</span>
+							</Link>
+						</li>
+					);
+				})}
+			</ul>
+		</article>
 	);
 };
 
