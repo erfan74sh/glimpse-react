9e53b76c7007573e81899092ddb050d34d43a359
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-14 18:55:19 +0330

    define fullData and map throuh it to render items

--- src/pages/Estimation/Estimate/VisualComfort/VisualSteps/Review/VisualReview.jsx
+++ src/pages/Estimation/Estimate/VisualComfort/VisualSteps/Review/VisualReview.jsx
@@ -6,6 +6,33 @@ import { selectVisualComfortData } from "../../../../../../features/visualComfor
 const VisualReview = () => {
 	const data = useSelector(selectVisualComfortData);
 
+	const fullData = [
+		{
+			name: "north_neighbor_distance",
+			label: "north neighbor distance",
+			unit: "m",
+			value: data.north_neighbor_distance,
+		},
+		{
+			name: "north_neighbor_height",
+			label: "north neighbor height",
+			unit: "m",
+			value: data.north_neighbor_height,
+		},
+		{
+			name: "south_neighbor_distance",
+			label: "south neighbor distance",
+			unit: "m",
+			value: data.south_neighbor_distance,
+		},
+		{
+			name: "south_neighbor_height",
+			label: "south neighbor height",
+			unit: "m",
+			value: data.south_neighbor_height,
+		},
+	];
+
 	return (
 		<div className="flex h-full flex-col">
 			<section className="flex flex-col pr-10">
@@ -13,30 +40,28 @@ const VisualReview = () => {
 					review
 				</h2>
 				<ul className="flex flex-col gap-y-2">
-					<li className="text-gray-650 flex justify-between border-b border-gray-400">
-						<span className="font-semibold capitalize text-gray-900">
-							south neighbor distance:
-						</span>
-						<span>{data.south_neighbor_distance}</span>
-					</li>
-					<li className="text-gray-650 flex justify-between border-b border-gray-400">
-						<span className="font-semibold capitalize text-gray-900">
-							south neighbor height:
-						</span>
-						<span>{data.south_neighbor_height}</span>
-					</li>
-					<li className="text-gray-650 flex justify-between border-b border-gray-400">
-						<span className="font-semibold capitalize text-gray-900">
-							north neighbor distance:
-						</span>
-						<span>{data.north_neighbor_distance}</span>
-					</li>
-					<li className="text-gray-650 flex justify-between border-b border-gray-400">
-						<span className="font-semibold capitalize text-gray-900">
-							north neighbor height:
-						</span>
-						<span>{data.north_neighbor_height}</span>
-					</li>
+					{fullData.map((item, idx) => {
+						return (
+							<li
+								className="text-gray-650 flex justify-between border-b border-gray-400"
+								key={idx}
+							>
+								<span className="font-semibold capitalize text-gray-900">
+									{item.label}:
+								</span>
+								<div>
+									<span>
+										{typeof item.value !== "object"
+											? item.value
+											: item.value[data[item.name]]}
+									</span>
+									<span className="text-sm normal-case text-gray-800">
+										{item.unit && ` ${item.unit}`}
+									</span>
+								</div>
+							</li>
+						);
+					})}
 				</ul>
 			</section>
 		</div>
