be2e750dcce99e48fa24e74f7fccc23280fcf8a9
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-14 11:57:32 +0330

    remove underLine from subset in title

--- src/pages/Estimation/Estimate/Estimate.js
+++ src/pages/Estimation/Estimate/Estimate.js
@@ -18,7 +18,7 @@ const Estimate = () => {
 		<main className=" px-24 py-10" id="estimate__main">
 			<header className="mb-10">
 				<h1 className="border-blue-550 text-blue-550 flex justify-between border-l-8 pl-3 text-2xl font-bold uppercase leading-8">
-					{primaryData.subset}
+					{primaryData.subset.replace("_", " ")}
 				</h1>
 				<section className="px-5 pt-10 pb-4">
 					<BreadCrumbs
