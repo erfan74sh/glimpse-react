627bfc1d6e6cccdd9560e39707fb179599add8d6
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-14 17:40:52 +0330

    define fullData in review step

--- src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Review/VisualReview.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Review/VisualReview.jsx
@@ -5,6 +5,80 @@ import { selectEnergyConsumptionData } from "../../../../../../features/energyCo
 
 const VisualReview = () => {
 	const data = useSelector(selectEnergyConsumptionData);
+	const fullData = [
+		{
+			name: "east_wall_bc",
+			label: "east wall boundry condition",
+			unit: "",
+			value: data.east_wall_bc,
+		},
+		{
+			name: "west_wall_bc",
+			label: "west wall boundry condition",
+			unit: "",
+			value: data.west_wall_bc,
+		},
+		{
+			name: "south_wall_bc",
+			label: "south wall boundry condition",
+			unit: "",
+			value: data.south_wall_bc,
+		},
+		{
+			name: "north_wall_bc",
+			label: "north wall boundry condition",
+			unit: "",
+			value: data.north_wall_bc,
+		},
+		{
+			name: "roof_bc",
+			label: "roof boundry condition",
+			unit: "",
+			value: data.roof_bc,
+		},
+		{
+			name: "floor_bc",
+			label: "floor boundry condition",
+			unit: "",
+			value: data.floor_bc,
+		},
+		{
+			name: "north_neighbor_distance",
+			label: "north neighbor distance",
+			unit: "m",
+			value: data.north_neighbor_distance,
+		},
+		{
+			name: "north_neighbor_height",
+			label: "north neighbor height",
+			unit: "m",
+			value: data.north_neighbor_height,
+		},
+		{
+			name: "south_neighbor_distance",
+			label: "south neighbor distance",
+			unit: "m",
+			value: data.south_neighbor_distance,
+		},
+		{
+			name: "south_neighbor_height",
+			label: "south neighbor height",
+			unit: "m",
+			value: data.south_neighbor_height,
+		},
+		{
+			name: "hvac",
+			label: "HVAC system",
+			unit: "",
+			value: data.hvac,
+		},
+		{
+			name: "natural_ventilation",
+			label: "natural ventilation",
+			unit: "",
+			value: data.natural_ventilation,
+		},
+	];
 
 	return (
 		<div className="flex h-full flex-col">
