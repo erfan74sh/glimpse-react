12d70ad2be7dbc9f1181f599f27e75fe04d9f15d
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-17 11:05:43 +0330

    show label if send as prop and sort tailwind classes

--- src/components/inputs/TextField/TextField.jsx
+++ src/components/inputs/TextField/TextField.jsx
@@ -5,16 +5,16 @@ const TextField = ({ label, ...props }) => {
 	const [field, meta] = useField(props);
 	return (
 		<div className="relative flex flex-col gap-y-1 capitalize">
-			<label>{label}</label>
+			{label && <label>{label}</label>}
 			<input
 				{...field}
 				{...props}
-				className={`w-full p-2 border ${
+				className={`w-full border p-2 ${
 					meta.touched && meta.error ? " border-red-600" : "border-gray-300"
 				} rounded-md outline-none`}
 			/>
 			{meta.touched && meta.error ? (
-				<span className="absolute bottom-0 left-1 transform translate-y-full text-xs text-red-600">
+				<span className="absolute bottom-0 left-1 translate-y-full transform text-xs text-red-600">
 					{meta.error}
 				</span>
 			) : null}
