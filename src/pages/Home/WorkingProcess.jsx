9b9c94884b23d51fa4ba7d31a140b9069dc9906a
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-18 12:51:41 +0330

    add arrow between items and edit style

--- src/pages/Home/WorkingProcess.jsx
+++ src/pages/Home/WorkingProcess.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // assets
 import SimulationIcon from "../../assets/icons/simulation.png";
 import AnalyzingIcon from "../../assets/icons/analysing.png";
 import DataSetIcon from "../../assets/icons/building-data-set.png";
 import MachineLearningIcon from "../../assets/icons/machine-learning.png";
 import ResultIcon from "../../assets/icons/result.png";
+// icons
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 const WorkingProcess = () => {
 	return (
@@ -12,56 +15,90 @@ const WorkingProcess = () => {
 			<header className="bg-white text-center">
 				<h3 className="text-blue-550 text-2xl font-bold">ساختار کلی و مراحل</h3>
 			</header>
-			<ul className="flex justify-between px-56 py-16">
-				<li className="flex flex-col items-center">
-					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-gray-100">
+			<ul className="flex items-center justify-evenly  px-56 py-16">
+				<li className="relative flex flex-col items-center">
+					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-white">
 						<img
 							src={SimulationIcon}
 							alt="icon"
 							className="h-1/2 w-1/2 object-contain"
 						/>
 					</div>
-					<span className="font-medium">شبیه‌سازی</span>
+					<span className="absolute top-full min-w-full pt-1 text-center font-medium">
+						شبیه‌سازی
+					</span>
 				</li>
-				<li className="flex flex-col items-center">
-					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-gray-100">
+				<li>
+					<FontAwesomeIcon
+						icon={faAngleLeft}
+						className="text-4xl text-gray-400"
+					/>
+				</li>
+				<li className="relative flex flex-col items-center">
+					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-white">
 						<img
 							src={AnalyzingIcon}
 							alt="icon"
 							className="h-1/2 w-1/2 object-contain"
 						/>
 					</div>
-					<span className="font-medium">تحلیل و بررسی</span>
+					<span className="absolute top-full min-w-full pt-1 text-center font-medium">
+						تحلیل و بررسی
+					</span>
 				</li>
-				<li className="flex flex-col items-center">
-					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-gray-100">
+				<li>
+					<FontAwesomeIcon
+						icon={faAngleLeft}
+						className="text-4xl text-gray-400"
+					/>
+				</li>
+				<li className="relative flex flex-col items-center">
+					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-white">
 						<img
 							src={DataSetIcon}
 							alt="icon"
 							className="h-1/2 w-1/2 object-contain"
 						/>
 					</div>
-					<span className="font-medium">ساخت مجموعه داده</span>
+					<span className="absolute top-full min-w-full pt-1 text-center font-medium">
+						ساخت مجموعه داده
+					</span>
 				</li>
-				<li className="flex flex-col items-center">
-					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-gray-100">
+				<li>
+					<FontAwesomeIcon
+						icon={faAngleLeft}
+						className="text-4xl text-gray-400"
+					/>
+				</li>
+				<li className="relative flex flex-col items-center">
+					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-white">
 						<img
 							src={MachineLearningIcon}
 							alt="icon"
 							className="h-1/2 w-1/2 object-contain"
 						/>
 					</div>
-					<span className="text-center font-medium">هوش مصنوعی</span>
+					<span className=" absolute top-full min-w-full pt-1 text-center font-medium">
+						هوش مصنوعی
+					</span>
 				</li>
-				<li className="flex flex-col items-center">
-					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-gray-100">
+				<li>
+					<FontAwesomeIcon
+						icon={faAngleLeft}
+						className="text-4xl text-gray-400"
+					/>
+				</li>
+				<li className="relative flex flex-col items-center">
+					<div className="shadow-neo flex h-32 w-32 items-center justify-center rounded-full bg-white">
 						<img
 							src={ResultIcon}
 							alt="icon"
 							className="h-3/5 w-3/5 object-contain"
 						/>
 					</div>
-					<span className="font-medium">نتایج</span>
+					<span className="absolute top-full min-w-full pt-1 text-center font-medium">
+						نتایج
+					</span>
 				</li>
 			</ul>
 		</section>
