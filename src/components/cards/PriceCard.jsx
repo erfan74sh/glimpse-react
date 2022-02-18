640da151f68fb14cf14edb06a3c584e70bb19a2a
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-18 09:59:33 +0330

    wrap whole priceCard into an article element

--- src/components/cards/PriceCard.jsx
+++ src/components/cards/PriceCard.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 
 const PriceCard = ({ period, options, price }) => {
 	return (
-		<Link to="#">
-			<h4>{period && period}</h4>
-			<ul>
-				{options && options.map((option, idx) => <li key={idx}>{option}</li>)}
-			</ul>
-			<span>{price ? `${price} هزار تومان` : "رایگان"}</span>
-		</Link>
+		<article>
+			<Link to="#">
+				<h4>{period && period}</h4>
+				<ul>
+					{options && options.map((option, idx) => <li key={idx}>{option}</li>)}
+				</ul>
+				<span>{price ? `${price} هزار تومان` : "رایگان"}</span>
+			</Link>
+		</article>
 	);
 };
 
