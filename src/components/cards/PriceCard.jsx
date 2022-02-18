c773a5acf5738f665daba84072b50910c7f54b56
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-18 11:03:48 +0330

    style priceCard

--- src/components/cards/PriceCard.jsx
+++ src/components/cards/PriceCard.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 const PriceCard = ({ period, options, price }) => {
 	return (
-		<article>
-			<Link to="#">
+		<Link to="#">
+			<article className="hover:border-blue-550 hover:shadow-b-sm flex h-36 w-52 flex-col items-center gap-y-1.5 rounded-lg border-2 border-blue-400 bg-white py-3 px-2 text-lg transition-all">
 				<h4>{period && period}</h4>
 				<ul>
 					{options && options.map((option, idx) => <li key={idx}>{option}</li>)}
 				</ul>
-				<span>{price ? `${price} هزار تومان` : "رایگان"}</span>
-			</Link>
-		</article>
+				<span className="mt-auto font-bold">
+					{price ? `${price} هزار تومان` : "رایگان"}
+				</span>
+			</article>
+		</Link>
 	);
 };
 
