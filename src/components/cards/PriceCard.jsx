536444f0d06ed0be1c0cd3c453c7772644218b89
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-18 09:56:25 +0330

    change simulationNumber prop to option prop and map through it

--- src/components/cards/PriceCard.jsx
+++ src/components/cards/PriceCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const PriceCard = ({ period, simulationNumber, price }) => {
+const PriceCard = ({ period, options, price }) => {
 	return (
 		<Link to="#">
 			<h4>{period && period}</h4>
 			<ul>
-				<li>{simulationNumber && simulationNumber} عدد شبیه‌سازی</li>
+				{options && options.map((option, idx) => <li key={idx}>{option}</li>)}
 			</ul>
 			<span>{price ? `${price} هزار تومان` : "رایگان"}</span>
 		</Link>
