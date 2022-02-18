db867357958af3a44962b1add97892d34fe324e0
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-18 11:18:47 +0330

    add new RecomendedPackage and remove priv one

--- src/pages/PricingPlans/PricingPlans.js
+++ src/pages/PricingPlans/PricingPlans.js
@@ -8,6 +8,7 @@ import BronzeBadge from "../../assets/images/badge-bronze.png";
 import { Link } from "react-router-dom";
 // style
 import "./PricingPlan.scss";
+import RecomendedPackages from "../../components/recomendedPackages/RecomendedPackages";
 
 const PricingPlans = () => {
 	return (
@@ -16,81 +17,11 @@ const PricingPlans = () => {
 				<Nav />
 			</header>
 			<main id="pricing-container" className="px-24 py-16">
-				<header className="text-center text-3xl text-gray-650 font-bold">
+				<header className="text-gray-650 text-center text-3xl font-bold">
 					<h1>Choose a plan that’s right for you</h1>
 				</header>
-				<section className="flex gap-x-10 justify-center py-16">
-					<article className="w-84 h-133 flex flex-col items-center px-10 py-7 pt-3 bg-white rounded-3xl shadow-full transition-all transform hover:scale-110">
-						<img
-							src={GoldBadge}
-							alt="gold badge"
-							className="w-36 h-48 object-contain"
-						/>
-						<h3 className="mb-4 text-3xl font-bold text-blue-550 uppercase">
-							gold
-						</h3>
-						<ul className="flex flex-col items-center">
-							<li>30 Calculation </li>
-							<li>45 Revision</li>
-							<li>Numerical Results</li>
-							<li>Simulation</li>
-							<li>Rhino And Revit</li>
-							<li> Grasshopper Exports</li>
-							<li>Design Suggestions</li>
-						</ul>
-						<button
-							type="button"
-							className="mt-auto px-8 py-1 text-white text-lg bg-blue-550 rounded-lg transition-all hover:shadow-b-sm"
-						>
-							<Link to="#">buy now</Link>
-						</button>
-					</article>
-					<article className="w-84 h-133 flex flex-col items-center px-10 py-7 pt-3 bg-white rounded-3xl shadow-full transition-all transform hover:scale-110">
-						<img
-							src={SilverBadge}
-							alt="gold badge"
-							className="w-36 h-48 object-contain"
-						/>
-						<h3 className="mb-4 text-3xl font-bold text-blue-550 uppercase">
-							silver
-						</h3>
-						<ul className="flex flex-col items-center">
-							<li>15 Calculation </li>
-							<li>20 Revision</li>
-							<li>Numerical Results</li>
-							<li>Simulation</li>
-							<li>Rhino And Revit</li>
-							<li>Exports</li>
-						</ul>
-						<button
-							type="button"
-							className="mt-auto px-8 py-1 text-white text-lg bg-blue-550 rounded-lg transition-all hover:shadow-b-sm"
-						>
-							<Link to="#">buy now</Link>
-						</button>
-					</article>
-
-					<article className="w-84 h-133 flex flex-col items-center px-10 py-7 pt-3 bg-white rounded-3xl shadow-full transition-all transform hover:scale-110">
-						<img
-							src={BronzeBadge}
-							alt="gold badge"
-							className="w-36 h-48 object-contain"
-						/>
-						<h3 className="mb-4 text-3xl font-bold text-blue-550 uppercase">
-							bronze
-						</h3>
-						<ul className="flex flex-col items-center">
-							<li>2 Calculation </li>
-							<li>4 Revision</li>
-							<li>Numerical Results</li>
-						</ul>
-						<button
-							type="button"
-							className="mt-auto px-8 py-1 text-white text-lg bg-blue-550 rounded-lg transition-all hover:shadow-b-sm"
-						>
-							<Link to="#">buy now</Link>
-						</button>
-					</article>
+				<section className=" py-16" dir="rtl">
+					<RecomendedPackages />
 				</section>
 			</main>
 		</>
