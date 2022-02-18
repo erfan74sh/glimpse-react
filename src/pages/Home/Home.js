665e9bdedd015c9c36b3d19db2a0b1426ef1fc59
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-18 14:41:02 +0330

    replace persian text and edit styling of header

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -38,22 +38,28 @@ const Home = () => {
 							className="text-blue-550 hover:bg-blue-550 rounded-b-full rounded-t-full border-none bg-white px-9 py-4 text-lg font-medium outline-none transition-all hover:text-white"
 							onClick={() => setShowEstimateModal(true)}
 						>
-							calculate now
+							شروع به شبیه‌سازی
 						</button>
 					</section>
 				</div>
-				<section className="w-1/2 text-lg font-medium">
-					<h2 className="text-blue-550 mb-2 text-center font-bold">Glimpse</h2>
-					<p className="text-justify leading-6">
-						Civil engineering is a professional engineering discipline that
-						deals with the design, construction, and maintenance of the physical
-						and naturally built environment, including public works such as
-						roads, bridges, canals, dams, airports, sewerage systems, pipelines,
-						structural components of buildings, and railways.
-						<br />g from military engineering. Civil engineering can take place
-						in the public sector from municipal public works departments through
-						to federal government agencies, and in the private sector from
-						locally based firms to global Fortune 500 companies.
+				<section className="w-1/2 text-lg font-medium" dir="rtl">
+					<h2 className="text-blue-550 mb-9 text-center text-2xl font-bold">
+						بی‌اس‌پی سیم چیست؟
+					</h2>
+					<p className="text-gray-650 text-justify leading-6">
+						BSPsim ابزار جامع و یکپارچه شبیه سازی است که به کمک الگوریتم های هوش
+						مصنوعی به ارزیابی عملکرد ساختمان می پردازد. این ابزار برای تمام
+						ذینفعان صنعت ساختمان قابل استفاده است و فارغ از تخصص آنها، ارزیابی
+						هایی در حوزه های متعدد انرژی، آسایش حرارتی، نور، آکوستیک، حریق و ..،
+						در مقیاس های مختلف، در کمترین زمان و بالاترین دقت ارائه می دهد. در
+						هر بخش، ورودی ها و اطلاعات مورد نیاز بسادگی قابل تعریف بوده و تنها
+						با چند کلیک به سرعت نتایج پیچیده ترین محاسبات و شبیه سازی ها به
+						منظور کشف بهترین پاسخ ها، مقایسه گزینه ها، و سنجش انطباق شرایط با
+						استانداردهای ملی و بین المللی در اختیار کاربر قرار می گیرد.مخاطب
+						اصلی این ابزار دفاتر طراحی و مهندسین مشاور ساختمان، محققین حوزه های
+						انرژی و محیط، سازه، معماری و ...، دانشجویان، مالکین و بهره برداران
+						ساختمان ها، پیمانکاران، و تمام افرادی است که به دنبال اطلاعاتی از
+						عملکرد های ساختمان و اجزاء آن هستند.
 					</p>
 				</section>
 			</header>
