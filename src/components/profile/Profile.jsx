8100aec3f0da406a79adb3bc36cc6516f1eb18dd
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-19 12:11:57 +0330

    show icon for profile in navbar

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // asset
-import AvatarImg from "../../assets/avatars/avatar-01.png";
+// import AvatarImg from "../../assets/avatars/avatar-01.png";
 // state
 import { selectUser } from "../../features/auth/authSlice";
+// icons
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Profile = () => {
 	const { isLoggedIn, user } = useSelector(selectUser);
 	return (
-		<button type="button" className="flex items-center gap-x-6">
+		<button type="button" className="flex items-center gap-x-2">
 			<span className="text-lg">{isLoggedIn && user["full_name"]}</span>
-			<img src={AvatarImg} alt="avatr" className="h-10 w-10 object-cover" />
+			{/* <img src={AvatarImg} alt="avatr" className="h-10 w-10 object-cover" /> */}
+			<span className="flex h-10 w-10 items-center justify-center">
+				<FontAwesomeIcon icon={faUserCircle} className="text-3xl" />
+			</span>
 		</button>
 	);
 };
