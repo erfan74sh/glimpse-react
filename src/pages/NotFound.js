b4e7f3e177b505233ba3870dc9a52d275357041e
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-22 13:04:43 +0330

    style nav section and nav items

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -9,7 +9,25 @@ const NotFound = () => {
 				<p>page not found!</p>
 			</header>
 			<nav>
-				<Link to="/">go home</Link>
+				<ul className="mt-10 flex gap-x-5">
+					<li>
+						<Link
+							to="/"
+							className="border-b-2 border-blue-400 px-4 py-2 capitalize transition hover:text-blue-400"
+						>
+							go home
+						</Link>
+					</li>
+					<li>or</li>
+					<li>
+						<Link
+							to="/contact-us"
+							className="border-b-2 border-blue-400 px-4 py-2 capitalize transition hover:text-blue-400"
+						>
+							contact us
+						</Link>
+					</li>
+				</ul>
 			</nav>
 		</main>
 	);
