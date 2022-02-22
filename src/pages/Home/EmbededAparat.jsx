0825f80cf73694d31ae16b71030883f898fe3364
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-22 10:55:54 +0330

    render aparat video with useScript hook

--- src/pages/Home/EmbededAparat.jsx
+++ src/pages/Home/EmbededAparat.jsx
@@ -1,17 +1,12 @@
 import React, { useEffect } from "react";
+import useScript from "../../hooks/useScript";
 
 const EmbededAparat = () => {
-	useEffect(() => {
-		const script = document.createElement("script");
-		script.type = "text/JavaScript";
-		script.src =
-			"https://www.aparat.com/embed/ZHdED?data[rnddiv]=46590385690&data[responsive]=yes";
-		script.async = true;
-
-		const wrapperEl = document.getElementById(46590385690);
-
-		wrapperEl.appendChild(script);
-	}, []);
+	useScript(46590385690, {
+		type: "text/JavaScript",
+		src: "https://www.aparat.com/embed/ZHdED?data[rnddiv]=46590385690&data[responsive]=yes",
+		async: true,
+	});
 
 	return (
 		<section className="my-18 mx-auto max-w-4xl">
