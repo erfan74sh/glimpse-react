cd53c1f28b61ca3a0eee7e0fdc09c3d73175addb
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-22 14:22:51 +0330

    fix errore in console and pass height property to map

--- src/pages/ContactUs/ContactUsMap.jsx
+++ src/pages/ContactUs/ContactUsMap.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import { Map, Marker } from "pigeon-maps";
 
-const mapContainerStyle = {
-	width: "100%",
-	height: "250px",
-};
 const companyLocation = [35.796784220168966, 51.39476019337767];
 
 const ContactUsMap = () => {
 	return (
 		<div>
-			<Map
-				height={mapContainerStyle.height}
-				width={mapContainerStyle.width}
-				defaultZoom={15}
-				defaultCenter={companyLocation}
-			>
+			<Map height={250} defaultZoom={15} defaultCenter={companyLocation}>
 				<Marker anchor={companyLocation} width={40} color="#147CDD" />
 			</Map>
 		</div>
