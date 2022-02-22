b32d273aa0c76de6c1752399e1975a36f1658c0c
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-22 12:57:41 +0330

    style header and layout

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-	return <div>NotFound</div>;
+	return (
+		<main className="flex h-screen w-screen flex-col items-center justify-center">
+			<header className="flex flex-col items-center">
+				<h1 className="text-9xl font-medium">404</h1>
+				<p>page not found!</p>
+			</header>
+			<nav>
+				<Link to="/">go home</Link>
+			</nav>
+		</main>
+	);
 };
 
 export default NotFound;
