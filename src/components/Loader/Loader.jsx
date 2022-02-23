d17ae7874f7ee2fd14eed81c51d9cde701cc9445
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-23 13:32:48 +0330

    style layout of Loader component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Loader = ({ message }) => {
-	return <div>{message}</div>;
+	return (
+		<div className="fixed z-50 h-screen w-screen bg-white bg-opacity-50 backdrop-blur-sm backdrop-filter">
+			<div className="flex h-full w-full flex-col items-center justify-center">
+				<span>animation here</span>
+				<span>{message}</span>
+			</div>
+		</div>
+	);
 };
 
 export default Loader;
