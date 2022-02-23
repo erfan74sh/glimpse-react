be7f9d80e85366af7aa20f60e0d43036cb80d5dd
Erfan Shafiee <erfan74sh@gmail.com>
2022-02-23 14:30:25 +0330

    create LoaderAnimation component and stylesheet

---
+++ src/components/Loader/LoaderAnimation.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./LoaderAnimation.css";
+
+const LoaderAnimation = () => {
+	return (
+		<div class="loadingio-spinner-spin-jrg84hivzx">
+			<div class="ldio-4l4ea0gctgu">
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default LoaderAnimation;
