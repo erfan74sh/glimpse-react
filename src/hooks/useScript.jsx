0d538ababa03f45432aef33afe04b7a9781e16f3
erfan shafiee <erfan74sh@gmail.com>
2022-03-15 13:15:34 +0330

    remove consol.logs from hook

--- src/hooks/useScript.jsx
+++ src/hooks/useScript.jsx
@@ -3,13 +3,11 @@ import { useEffect } from "react";
 const useScript = (wrapperElId, attributes) => {
 	useEffect(() => {
 		const script = document.createElement("script");
-		console.log(attributes);
 		for (const attr in attributes) {
 			script[attr] = attributes[attr];
 		}
 
 		const wrapperEl = document.getElementById(wrapperElId);
-		console.log(script);
 
 		wrapperEl.appendChild(script);
 
