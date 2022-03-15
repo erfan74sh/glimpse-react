e0561ea7005bffae0be13e9b9e783edd11c54193
erfan shafiee <erfan74sh@gmail.com>
2022-03-15 13:03:23 +0330

    fix error: edit class to className

--- src/components/Loader/LoaderAnimation.jsx
+++ src/components/Loader/LoaderAnimation.jsx
@@ -3,8 +3,8 @@ import "./LoaderAnimation.css";
 
 const LoaderAnimation = () => {
 	return (
-		<div class="loadingio-spinner-spin-jrg84hivzx">
-			<div class="ldio-4l4ea0gctgu">
+		<div className="loadingio-spinner-spin-jrg84hivzx">
+			<div className="ldio-4l4ea0gctgu">
 				<div>
 					<div></div>
 				</div>
