8293f306211f3c2de39b8b5e01ddd7f53ef8cb32
erfan shafiee <erfan74sh@gmail.com>
2022-03-27 12:49:06 +0430

    fix image of sections

--- src/pages/Home/Simulations.jsx
+++ src/pages/Home/Simulations.jsx
@@ -8,19 +8,19 @@ const Simulations = () => {
 					id="structure-simulations"
 					className="flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
 				>
-					<span className="text-gray-650 font-bold">آسایش حرارتی</span>
+					<span className="font-bold text-gray-650">سازه</span>
 				</li>
 				<li
 					id="energy-simulations"
 					className="flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
 				>
-					<span className="text-gray-650 font-bold">مصرف انرژی</span>
+					<span className="font-bold text-gray-650">مصرف انرژی</span>
 				</li>
 				<li
 					id="ieq-simulations"
 					className="flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
 				>
-					<span className="text-gray-650 font-bold">سازه</span>
+					<span className="font-bold text-gray-650">آسایش حرارتی</span>
 				</li>
 			</ul>
 		</section>
