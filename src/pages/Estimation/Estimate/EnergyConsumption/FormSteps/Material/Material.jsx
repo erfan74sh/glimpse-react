e96589c72cc62779d99519b93453f63314319a6a
erfan shafiee <erfan74sh@gmail.com>
2022-03-27 15:14:58 +0430

    add more info and more description for input options

--- src/pages/Estimation/Estimate/EnergyConsumption/FormSteps/Material/Material.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/FormSteps/Material/Material.jsx
@@ -52,31 +52,83 @@ const Material = ({ nextStep, prevStep }) => {
 	};
 
 	const wallMaterialOptions = [
-		{ label: "Brick wall + Brick facade", value: "1.719" },
-		{ label: "Cement Block + Stone facade", value: "1.649" },
-		{ label: "Cement Block + Cement facade", value: "0.979" },
 		{
-			label:
-				"Two layer Brick wall with Insulation in the middle + Brick facade",
+			label: "1.719",
+			value: "1.719",
+			moreDescription: "(Brick wall + Brick facade)",
+			moreInfo: "دیوار آجری با نمای آجری",
+		},
+		{
+			label: "1.649",
+			value: "1.649",
+			moreDescription: "(Cement Block + Stone facade)",
+			moreInfo: "دیوار بلوک سیمانی با نمای سنگ",
+		},
+		{
+			label: "0.979",
+			value: "0.979",
+			moreDescription: "(Cement Block + Cement facade)",
+			moreInfo: "دیوار بلوک سیمانی با نمای سیمانی",
+		},
+		{
+			label: "0.225",
 			value: "0.225",
+			moreDescription:
+				"(Two layer Brick wall with Insulation in the middle + Brick facade)",
+			moreInfo: "دیوار دولایه آجری با عایق میانی و نمای آجری",
 		},
 	];
 	const floorMaterialOptions = [
-		{ label: "Reinforced Concrete with air gap + Terrazzo", value: "0.37" },
-		{ label: "Beam Block + Parquet flooring", value: "0.47" },
-		{ label: "Reinforced concrete + Parquet flooring", value: "0.65" },
+		{
+			label: "0.37",
+			value: "0.37",
+			moreDescription: "(Reinforced Concrete with air gap + Terrazzo)",
+			moreInfo: "کف بتنی با پوشش موزاییک و فاصله هوایی",
+		},
+		{
+			label: "0.47",
+			value: "0.47",
+			moreDescription: "(Beam Block + Parquet flooring)",
+			moreInfo: "کف تیرچه بلوک با پوشش پارکت",
+		},
+		{
+			label: "0.65",
+			value: "0.65",
+			moreDescription: "(Reinforced concrete + Parquet flooring)",
+			moreInfo: "کف بتنی با پوشش پارکت",
+		},
 	];
 	const roofMaterialOptions = [
-		{ label: "Gypsum Board + Reinforced Concrete + Terrazzo", value: "0.878" },
 		{
-			label: "Reinforced Concrete with thermal insullation + Terrazzo",
-			value: "0.211",
+			label: "0.878",
+			value: "0.878",
+			moreDescription: "(Gypsum Board + Reinforced Concrete + Terrazzo)",
+			moreInfo: "بام بتنی با کفپوش موزاییکی",
 		},
-		{ label: "Beam block + terrazzo", value: "0.393" },
-		{ label: "Beam Block with out terrazzo", value: "0.386" },
 		{
-			label: "Gypsum Plaster + Reinforced Concrete + Terrazzo",
+			label: "0.211",
+			value: "0.211",
+			moreDescription:
+				"(Reinforced Concrete with thermal insullation + Terrazzo)",
+			moreInfo: "بام بتنی با کف پوش موزاییکی و عایق حرارتی",
+		},
+		{
+			label: "0.393",
+			value: "0.393",
+			moreDescription: "(Beam block + terrazzo)",
+			moreInfo: "بام تیرچه بلوک با کفپوش موزاییکی",
+		},
+		{
+			label: "0.386",
+			value: "0.386",
+			moreDescription: "(Beam Block with out terrazzo)",
+			moreInfo: "بام تیرچه بلوک بدون کفپوش موزاییکی",
+		},
+		{
+			label: "1.092",
 			value: "1.092",
+			moreDescription: "(Gypsum Plaster + Reinforced Concrete + Terrazzo)",
+			moreInfo: "بام بتنی با کفپوش موزاییکی و نازک کاری گچ اندود داخلی",
 		},
 	];
 	const glassMaterialOptions = [
