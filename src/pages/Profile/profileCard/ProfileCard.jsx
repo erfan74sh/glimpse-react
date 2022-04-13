cbf345cdfe6f6262d746eda28a5e3bd0e59f6ff4
erfan shafiee <erfan74sh@gmail.com>
2022-04-13 21:40:59 +0430

    show user info based on props

--- src/pages/Profile/profileCard/ProfileCard.jsx
+++ src/pages/Profile/profileCard/ProfileCard.jsx
@@ -1,32 +1,25 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 // assets
 import ProfilePic from "../../../assets/profile/01.png";
-// states
-import { selectUserInfo } from "../../../features/userInfo/UserInfoSlice";
-import { Link } from "react-router-dom";
 
-const ProfileCard = () => {
-	const userInfo = useSelector(selectUserInfo);
-
+const ProfileCard = ({ user }) => {
 	return (
-		<div className="flex flex-col justify-between items-center w-113 p-14 rounded-md bg-white shadow-full">
+		<div className="flex w-113 flex-col items-center justify-between rounded-md bg-white p-14 shadow-full">
 			<img
 				src={ProfilePic}
 				alt="profile"
-				className="w-48 h-48 object-cover overflow-hidden rounded-full mb-16"
+				className="mb-16 h-48 w-48 overflow-hidden rounded-full object-cover"
 			/>
-			<div className="flex flex-col items-center gap-y-5 mb-12 text-xl text-gray-650 font-normal">
-				<h3 className="mb-5 text-3xl font-bold capitalize">{`${userInfo.firstName} ${userInfo.lastName}`}</h3>
-				<span className="px-10 text-center capitalize">
-					{userInfo.education}
-				</span>
-				<span>{userInfo.email}</span>
-				<span>{userInfo.phone}</span>
+			<div className="mb-12 flex flex-col items-center gap-y-5 text-xl font-normal text-gray-650">
+				<h3 className="mb-5 text-3xl font-bold capitalize">{user.full_name}</h3>
+				<span className="px-10 text-center capitalize">{user.education}</span>
+				<span>{user.email}</span>
+				<span>{user.phone_number}</span>
 			</div>
 			<button
 				type="button"
-				className="py-3 px-5 text-lg text-white bg-blue-550 rounded-md"
+				className="rounded-md bg-blue-550 py-3 px-5 text-lg text-white"
 			>
 				<Link to="/">simulation history</Link>
 			</button>
