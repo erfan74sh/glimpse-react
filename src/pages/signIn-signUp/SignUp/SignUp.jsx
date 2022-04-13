dbb45064aa186e1659813f28d35d29670b813aa9
erfan shafiee <erfan74sh@gmail.com>
2022-04-13 15:16:41 +0430

    add counter for redirecting user after sucsessfull register

--- src/pages/signIn-signUp/SignUp/SignUp.jsx
+++ src/pages/signIn-signUp/SignUp/SignUp.jsx
@@ -16,19 +16,29 @@ const SignUp = () => {
 		setSuccessful(isSuccessful);
 	};
 
+	const [counter, setCounter] = useState(3);
+
+	useEffect(() => {
+		const timer =
+			counter > 0 &&
+			successful &&
+			setTimeout(() => setCounter(counter - 1), 1000);
+		return () => clearTimeout(timer);
+	}, [counter, successful]);
+
 	const dispatch = useDispatch();
 	const { message } = useSelector(selectMessage);
 	useEffect(() => {
 		dispatch(clearMessage());
 	}, [dispatch]);
 	return (
-		<div className="flex flex-col items-center px-12 py-12 gap-y-9 bg-white rounded-md shadow-full">
-			<img src={Logo} alt="logo" className="w-60 h-auto" />
+		<div className="flex flex-col items-center gap-y-9 rounded-md bg-white px-12 py-12 shadow-full">
+			<img src={Logo} alt="logo" className="h-auto w-60" />
 			{!successful ? (
 				<>
 					<SignUpForm handleSuccessful={handleSuccessful} />
 					{message && <div className="text-red-500">{message}</div>}
-					<span className="text-lg font-normal text-blue-550 capitalize">
+					<span className="text-lg font-normal capitalize text-blue-550">
 						or{" "}
 						<Link to="/auth/sign-in" className="underline">
 							log in
@@ -36,7 +46,15 @@ const SignUp = () => {
 					</span>
 				</>
 			) : (
-				message && <div className="text-green-600">{message}</div>
+				message && (
+					<div className="flex flex-col items-center justify-center">
+						<div className="text-green-600">{message}</div>
+						<span className="text-gray-500">
+							and will be redirected in {counter}{" "}
+							{counter <= 1 ? "second" : "seconds"}
+						</span>
+					</div>
+				)
 			)}
 		</div>
 	);
