539474bee51f9eb594bc6edc64472268c0520caf
erfan shafiee <erfan74sh@gmail.com>
2022-04-13 21:38:14 +0430

    send user data to child components

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Nav from "../../components/Nav";
+import { selectUser } from "../../features/auth/authSlice";
 import ProfileCard from "./profileCard/ProfileCard";
 import ProfileInfo from "./profileInfo/ProfileInfo";
 
 const Profile = () => {
+	const { user } = useSelector(selectUser);
 	return (
 		<div className="min-h-screen">
 			<header className="">
@@ -11,13 +14,13 @@ const Profile = () => {
 			</header>
 			<main className="px-24 py-16">
 				<header className="mb-14">
-					<h1 className="pl-3 leading-8 border-l-8 border-blue-550 text-blue-550 text-3xl font-bold uppercase">
+					<h1 className="border-l-8 border-blue-550 pl-3 text-3xl font-bold uppercase leading-8 text-blue-550">
 						profile
 					</h1>
 				</header>
-				<section className="flex gap-x-12 items-start">
-					<ProfileCard />
-					<ProfileInfo />
+				<section className="flex items-start gap-x-12">
+					<ProfileCard user={user} />
+					<ProfileInfo user={user} />
 				</section>
 			</main>
 		</div>
