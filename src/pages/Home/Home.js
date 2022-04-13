1666413bf4fea659deb7e936701a3ace0bc2f020
erfan shafiee <erfan74sh@gmail.com>
2022-04-13 12:53:33 +0430

    center lifecycle image

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -33,13 +33,13 @@ const Home = () => {
 			)}
 			<Nav lang="persian" />
 			<Header handleShowModal={handleShowEstimationModal} />
-			<main className="font-irancell mt-24" dir="rtl">
+			<main className="mt-24 font-irancell" dir="rtl">
 				<EmbededAparat
 					videoId={16504825167}
 					videoUrl="https://www.aparat.com/embed/mVSx2?data[rnddiv]=16504825167&data[responsive]=yes"
 				/>
 				<section id="work-frame">
-					<div className="mb-16 flex items-center justify-center pr-28">
+					<div className="mb-16 flex items-center justify-center">
 						<img src={LifeCycleImg} alt="life-cycle" className="h-auto w-1/2" />
 					</div>
 					<WorkingProcess />
@@ -51,8 +51,8 @@ const Home = () => {
 				/>
 				<section className="pb-40">
 					<header className="relative mb-12 text-center">
-						<span className="bg-blue-550 absolute left-1/2 top-1/2 z-0 block h-0.5 w-2/3 translate-y-1/2 -translate-x-1/2 transform"></span>
-						<h3 className="text-blue-550 relative z-10 inline-block bg-white px-5 text-2xl font-bold">
+						<span className="absolute left-1/2 top-1/2 z-0 block h-0.5 w-2/3 translate-y-1/2 -translate-x-1/2 transform bg-blue-550"></span>
+						<h3 className="relative z-10 inline-block bg-white px-5 text-2xl font-bold text-blue-550">
 							بسته‌های پیشنهادی
 						</h3>
 					</header>
