88961634b1bcaab72d547baa4858f9cc36ea0ceb
erfan shafiee <erfan74sh@gmail.com>
2022-04-14 21:47:55 +0430

    add user icon in profile page

--- src/pages/Profile/profileCard/ProfileCard.jsx
+++ src/pages/Profile/profileCard/ProfileCard.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// assets
-import ProfilePic from "../../../assets/profile/01.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// icons
+import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
 
 const ProfileCard = ({ user }) => {
 	return (
-		<div className="flex w-113 flex-col items-center justify-between rounded-md bg-white p-14 shadow-full">
-			<img
-				src={ProfilePic}
-				alt="profile"
-				className="mb-16 h-48 w-48 overflow-hidden rounded-full object-cover"
-			/>
-			<div className="mb-12 flex flex-col items-center gap-y-5 text-xl font-normal text-gray-650">
+		<div className="w-113 shadow-full flex flex-col items-center justify-between rounded-md bg-white p-14">
+			<span className="mb-16 flex h-48 w-48 items-center justify-center rounded-full bg-gray-400">
+				<FontAwesomeIcon icon={faUserAlt} size="7x" className="text-gray-650" />
+			</span>
+			<div className="text-gray-650 mb-12 flex flex-col items-center gap-y-5 text-xl font-normal">
 				<h3 className="mb-5 text-3xl font-bold capitalize">{user.full_name}</h3>
 				<span className="px-10 text-center capitalize">{user.education}</span>
 				<span>{user.email}</span>
@@ -19,7 +18,7 @@ const ProfileCard = ({ user }) => {
 			</div>
 			<button
 				type="button"
-				className="rounded-md bg-blue-550 py-3 px-5 text-lg text-white"
+				className="bg-blue-550 rounded-md py-3 px-5 text-lg text-white"
 			>
 				<Link to="/">simulation history</Link>
 			</button>
