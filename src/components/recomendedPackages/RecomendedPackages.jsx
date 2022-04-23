06e8ee721266d29d744cb63d75ffa90056277449
erfan shafiee <erfan74sh@gmail.com>
2022-04-23 16:02:12 +0430

    add paymentPriceIRR to packages object

--- src/components/recomendedPackages/RecomendedPackages.jsx
+++ src/components/recomendedPackages/RecomendedPackages.jsx
@@ -5,22 +5,54 @@ import PriceCard from "../cards/PriceCard";
 const packagesCategories = [
 	{
 		categoryName: "آزمایشی",
-		packagesList: [{ period: "۱۵ روزه", options: ["۴۰ عدد شبیه‌سازی"] }],
+		packagesList: [
+			{ period: "۱۵ روزه", options: ["۴۰ عدد شبیه‌سازی"], paymentpriceIRR: 0 },
+		],
 	},
 	{
 		categoryName: "دانشجویی",
 		packagesList: [
-			{ period: "یک ماهه", options: ["۳۰۰ عدد شبیه‌سازی"], price: "۴۹" },
-			{ period: "سه ماهه", options: ["۱۵۰۰ عدد شبیه‌سازی"], price: "۱۳۹" },
-			{ period: "شش ماهه", options: ["۵۰۰۰ عدد شبیه‌سازی"], price: "۲۶۹" },
+			{
+				period: "یک ماهه",
+				options: ["۳۰۰ عدد شبیه‌سازی"],
+				price: "۴۹",
+				paymentpriceIRR: 490000,
+			},
+			{
+				period: "سه ماهه",
+				options: ["۱۵۰۰ عدد شبیه‌سازی"],
+				price: "۱۳۹",
+				paymentpriceIRR: 1390000,
+			},
+			{
+				period: "شش ماهه",
+				options: ["۵۰۰۰ عدد شبیه‌سازی"],
+				price: "۲۶۹",
+				paymentpriceIRR: 2690000,
+			},
 		],
 	},
 	{
 		categoryName: "حرفه‌ای",
 		packagesList: [
-			{ period: "یک ماهه", options: ["۴۵۰ عدد شبیه‌سازی"], price: "۷۴" },
-			{ period: "سه ماهه", options: ["۲۵۰۰ عدد شبیه‌سازی"], price: "۲۳۹" },
-			{ period: "شش ماهه", options: ["۸۰۰۰ عدد شبیه‌سازی"], price: "۴۶۹" },
+			{
+				period: "یک ماهه",
+				options: ["۴۵۰ عدد شبیه‌سازی"],
+				price: "۷۴",
+				paymentpriceIRR: 740000,
+			},
+			{
+				period: "سه ماهه",
+				options: ["۲۵۰۰ عدد شبیه‌سازی"],
+				price: "۲۳۹",
+				paymentpriceIRR: 2390000,
+			},
+			{
+				period: "شش ماهه",
+				options: ["۸۰۰۰ عدد شبیه‌سازی"],
+				price: "۴۶۹",
+				paymentpriceIRR: 4690000,
+			},
 		],
 	},
 ];
