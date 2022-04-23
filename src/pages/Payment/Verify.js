0aa65acc85f8e26fa010f68f983dda0e73dc1e9c
erfan shafiee <erfan74sh@gmail.com>
2022-04-23 19:06:06 +0430

    remove ilustration from page

--- src/pages/Payment/Verify.js
+++ src/pages/Payment/Verify.js
@@ -4,17 +4,12 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-// assets
-import PaymentIlus from "../../assets/images/undraw_personal.svg";
 
 const Verifyy = () => {
 	return (
-		<div className="flex min-h-screen items-center justify-center gap-x-5 bg-gray-50 px-48">
-			<div className="">
-				<img src={PaymentIlus} alt="ilustration" className="w-113 h-auto" />
-			</div>
-			<div className="w-100 flex flex-col items-center rounded-xl bg-white px-8 shadow-xl ">
-				<div className="flex flex-col items-center gap-y-2 border-b-2 border-dashed py-5">
+		<div className="flex min-h-screen items-center justify-center gap-x-10 bg-gray-50 px-40">
+			<div className="w-113 flex flex-col content-center items-center rounded-xl bg-white px-10 py-3 shadow-xl ">
+				<div className="flex w-full flex-col items-center gap-y-2 border-b-2 border-dashed py-5">
 					<div
 						className={`flex flex-col items-center gap-y-2 text-3xl font-bold ${
 							true ? "text-green-500" : "text-red-500"
@@ -31,9 +26,9 @@ const Verifyy = () => {
 						<span>۱۲۳۴۵۶۷۸۹</span>
 					</div>
 				</div>
-				<div>
-					<div>
-						<span>مبلغ:</span>
+				<div className="flex w-full flex-col gap-y-10 py-5">
+					<div className="flex justify-between font-medium text-gray-500">
+						<span>مبلغ پرداخت شده:</span>
 						<span>100 هزار تومان</span>
 					</div>
 					<button>go home</button>
