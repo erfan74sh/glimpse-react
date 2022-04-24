f32c741ef1435e8f0134e89d85498c1e315f9c7d
erfan shafiee <erfan74sh@gmail.com>
2022-04-24 19:00:06 +0430

    responsive PricingPlan page

--- src/pages/PricingPlans/PricingPlans.js
+++ src/pages/PricingPlans/PricingPlans.js
@@ -11,13 +11,16 @@ const PricingPlans = () => {
 			<header>
 				<Nav lang="persian" />
 			</header>
-			<main id="pricing-container" className="px-24 py-16">
+			<main
+				id="pricing-container"
+				className="px-5 py-16 sm:px-10 md:px-16 lg:px-24"
+			>
 				<header className="mb-14" dir="rtl">
-					<h1 className="font-irancell border-blue-550 text-blue-550 border-r-4 pr-3 text-3xl font-bold uppercase leading-8">
+					<h1 className="font-irancell border-blue-550 text-blue-550 pr-3 text-center text-3xl font-bold uppercase leading-8 md:border-r-4 md:text-right">
 						برنامه فروش
 					</h1>
 				</header>
-				<section className=" py-16" dir="rtl">
+				<section className="py-16" dir="rtl">
 					<RecomendedPackages />
 				</section>
 			</main>
