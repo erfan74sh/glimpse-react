8c125a2808bb06cdeb42ec78ff1ee98ed0571c35
erfan shafiee <erfan74sh@gmail.com>
2022-04-25 21:56:12 +0430

    responsive PointsAndGrades component

--- src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
+++ src/pages/Estimation/Result/PiontsAndGrades/PointsAndGrades.jsx
@@ -7,10 +7,10 @@ const PointsAndGrades = ({ subset, alternatives }) => {
 		<>
 			{subset && subset !== "thermal_comfort" && (
 				<section className="mt-18">
-					<h2 className="mb-4 px-8 text-xl font-normal uppercase">
+					<h2 className="mb-4 px-1 text-xl font-normal uppercase">
 						points and grades
 					</h2>
-					<div className="shadow-full flex flex-col gap-y-5 rounded-md bg-white px-8 py-7">
+					<div className="shadow-full flex flex-col gap-y-5 rounded-md bg-white p-3 md:px-8  md:py-7">
 						<div className="flex flex-col gap-y-3">
 							{subset === "energy_consumption" && (
 								<EnergyConsumptionGrades alternatives={alternatives} />
