323c0e4b8349210429849fe1b1eef28e9e7ee6ed
erfan shafiee <erfan74sh@gmail.com>
2022-04-25 18:24:57 +0430

    responsive footer in home page

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -19,27 +19,26 @@ const Footer = () => {
 	return (
 		<footer className="font-irancell" dir="rtl">
 			<header className="relative mb-12 text-center">
-				<span className="bg-blue-550 absolute left-1/2 top-1/2 z-0 block h-0.5 w-2/3 translate-y-1/2 -translate-x-1/2 transform"></span>
+				<span className="bg-blue-550 absolute left-1/2 top-1/2 z-0 block h-0.5 w-11/12 translate-y-1/2 -translate-x-1/2 transform md:w-2/3"></span>
 				<h3 className="text-blue-550 relative z-10 inline-block bg-white px-5 text-2xl font-bold">
 					ارتباط با ما
 				</h3>
 			</header>
-			<section className="px-84 relative z-50 flex justify-between">
-				<ul className="flex flex-col gap-y-8">
+			<section className="xl:px-84 relative z-50 flex flex-col justify-between gap-y-5 px-10 md:flex-row md:px-14 lg:px-24">
+				<ul className="flex flex-col items-center gap-y-8 md:items-start">
 					<li className="flex items-center gap-x-2">
-						<span className="flex h-6 w-6 items-center justify-center">
+						<span className="hidden h-6 w-6 items-center justify-center md:flex">
 							<FontAwesomeIcon
 								icon={faMapMarkerAlt}
 								className="text-2xl text-gray-600"
 							/>
 						</span>
-
-						<span>
+						<span className="text-center md:text-right">
 							تهران.دانشگاه شهید‌بهشتی، ساختمان شماره ۲ دانشکده معماری و شهرسازی
 						</span>
 					</li>
 					<li className="flex items-center gap-x-2">
-						<span className="flex h-6 w-6 items-center justify-center">
+						<span className="hidden h-6 w-6 items-center justify-center md:flex">
 							<FontAwesomeIcon
 								icon={faPhone}
 								className="text-2xl text-gray-600"
@@ -50,7 +49,7 @@ const Footer = () => {
 					</li>
 				</ul>
 				<ul
-					className="flex flex-col items-start gap-y-8 font-sans text-2xl text-gray-600"
+					className="flex items-center  justify-center gap-x-5 gap-y-8 font-sans text-2xl text-gray-600 md:flex-col md:items-start"
 					dir="ltr"
 				>
 					<li className="group h-6">
@@ -64,7 +63,7 @@ const Footer = () => {
 									className="group-hover:text-blue-550 "
 								/>{" "}
 							</span>
-							<span className="text-base">BSP.sim</span>
+							<span className="hidden text-base md:inline-block">BSP.sim</span>
 						</Link>
 					</li>
 					<li className="group h-6">
@@ -78,7 +77,7 @@ const Footer = () => {
 									className="group-hover:text-blue-550 "
 								/>{" "}
 							</span>
-							<span className="text-base">BSP.sim</span>
+							<span className="hidden text-base md:inline-block">BSP.sim</span>
 						</Link>
 					</li>
 					<li className="group h-6">
@@ -92,15 +91,17 @@ const Footer = () => {
 									className="group-hover:text-blue-550 "
 								/>{" "}
 							</span>
-							<span className="text-base">Info@bsp.green</span>
+							<span className="hidden text-base md:inline-block">
+								Info@bsp.green
+							</span>
 						</Link>
 					</li>
 				</ul>
 			</section>
 			<img
 				src={FooterImage}
-				alt=""
-				className="-mt-84 z-0 mx-auto h-auto w-2/3"
+				alt="footer"
+				className="-mt-84 z-0 hidden h-auto md:block lg:mx-auto xl:w-2/3"
 			/>
 			<section className="flex flex-col items-center py-10">
 				<img src={Logo} alt="glimpse logo" className="h-auto w-48" />
