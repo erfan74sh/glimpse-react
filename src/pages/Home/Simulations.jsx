3fe735ce239190928269bbafb665e95b232b3763
erfan shafiee <erfan74sh@gmail.com>
2022-04-25 18:25:57 +0430

    responsive Simulation section in home page

--- src/pages/Home/Simulations.jsx
+++ src/pages/Home/Simulations.jsx
@@ -3,24 +3,24 @@ import React from "react";
 const Simulations = () => {
 	return (
 		<section className="pt-20 pb-28">
-			<ul className="flex justify-center gap-x-24">
+			<ul className="flex flex-col items-center justify-center gap-y-5 gap-x-3 sm:flex-row md:gap-x-10 lg:gap-x-24">
 				<li
 					id="structure-simulations"
-					className="flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
+					className="shadow-neo flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
 				>
-					<span className="font-bold text-gray-650">سازه</span>
+					<span className="text-gray-650 font-bold">سازه</span>
 				</li>
 				<li
 					id="energy-simulations"
-					className="flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
+					className="shadow-neo flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
 				>
-					<span className="font-bold text-gray-650">مصرف انرژی</span>
+					<span className="text-gray-650 font-bold">مصرف انرژی</span>
 				</li>
 				<li
 					id="ieq-simulations"
-					className="flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
+					className="shadow-neo flex h-56 w-40 flex-col items-center gap-y-4 rounded-lg pt-4 text-lg"
 				>
-					<span className="font-bold text-gray-650">آسایش حرارتی</span>
+					<span className="text-gray-650 font-bold">آسایش حرارتی</span>
 				</li>
 			</ul>
 		</section>
