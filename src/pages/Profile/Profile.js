c9311debc8bca1c78528f3f0903c060244a89ed9
erfan shafiee <erfan74sh@gmail.com>
2022-04-26 13:56:26 +0430

    responsive Profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -12,13 +12,13 @@ const Profile = () => {
 			<header className="">
 				<Nav />
 			</header>
-			<main className="px-24 py-16">
+			<main className="px-5 py-16 md:px-10 lg:px-24">
 				<header className="mb-14">
-					<h1 className="border-l-8 border-blue-550 pl-3 text-3xl font-bold uppercase leading-8 text-blue-550">
+					<h1 className="border-blue-550 text-blue-550 border-l-8 pl-3 text-3xl font-bold uppercase leading-8">
 						profile
 					</h1>
 				</header>
-				<section className="flex items-start gap-x-12">
+				<section className="flex flex-col gap-x-12 gap-y-10 lg:flex-row lg:items-start">
 					<ProfileCard user={user} />
 					<ProfileInfo user={user} />
 				</section>
