8318cbed0ddbf3228e1afb273a6d4229d641d78c
erfan shafiee <erfan74sh@gmail.com>
2022-04-26 20:59:34 +0430

    responsive VisualHVAC component

--- src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Hvac/VisualHvac.jsx
+++ src/pages/Estimation/Estimate/EnergyConsumption/VisualSteps/Hvac/VisualHvac.jsx
@@ -24,11 +24,11 @@ const VisualHvac = () => {
 	];
 	return (
 		<>
-			<h2 className=" mx-8 mb-9 border-b border-gray-500 pb-4 text-xl font-normal uppercase">
+			<h2 className="mb-9 border-b border-gray-500 pb-4 text-xl font-normal uppercase">
 				details
 			</h2>
 			<div className="">
-				<div className="flex h-96 w-full justify-center px-10">
+				<div className="flex h-96 w-full justify-center lg:px-10">
 					<article
 						className={`relative flex h-96 w-96 items-center justify-center rounded-2xl ${
 							hvac !== "" ? "bg-white" : "bg-gray-400"
