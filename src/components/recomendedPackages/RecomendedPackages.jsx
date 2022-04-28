2eeb2ac9be02278cb69da49c8671dd943b1fdc28
erfan shafiee <erfan74sh@gmail.com>
2022-04-28 15:54:53 +0430

    use packagesCategories constant from constants folder

--- src/components/recomendedPackages/RecomendedPackages.jsx
+++ src/components/recomendedPackages/RecomendedPackages.jsx
@@ -2,61 +2,8 @@ import React from "react";
 import Axios from "axios";
 // components
 import PriceCard from "../cards/PriceCard";
-
-const packagesCategories = [
-	{
-		categoryName: "آزمایشی",
-		packagesList: [
-			{ period: "۱۵ روزه", options: ["۴۰ عدد شبیه‌سازی"], paymentpriceIRR: 0 },
-		],
-	},
-	{
-		categoryName: "دانشجویی",
-		packagesList: [
-			{
-				period: "یک ماهه",
-				options: ["۳۰۰ عدد شبیه‌سازی"],
-				price: "۴۹",
-				paymentpriceIRR: 490000,
-			},
-			{
-				period: "سه ماهه",
-				options: ["۱۵۰۰ عدد شبیه‌سازی"],
-				price: "۱۳۹",
-				paymentpriceIRR: 1390000,
-			},
-			{
-				period: "شش ماهه",
-				options: ["۵۰۰۰ عدد شبیه‌سازی"],
-				price: "۲۶۹",
-				paymentpriceIRR: 2690000,
-			},
-		],
-	},
-	{
-		categoryName: "حرفه‌ای",
-		packagesList: [
-			{
-				period: "یک ماهه",
-				options: ["۴۵۰ عدد شبیه‌سازی"],
-				price: "۷۴",
-				paymentpriceIRR: 740000,
-			},
-			{
-				period: "سه ماهه",
-				options: ["۲۵۰۰ عدد شبیه‌سازی"],
-				price: "۲۳۹",
-				paymentpriceIRR: 2390000,
-			},
-			{
-				period: "شش ماهه",
-				options: ["۸۰۰۰ عدد شبیه‌سازی"],
-				price: "۴۶۹",
-				paymentpriceIRR: 4690000,
-			},
-		],
-	},
-];
+// constants
+import { packagesCategories } from "../../constants";
 
 const RecomendedPackages = () => {
 	const handlePayment = async (price, description) => {
