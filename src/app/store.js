5b4e0e451d934b282089d29eba5fb67b50b85273
erfan shafiee <erfan74sh@gmail.com>
2022-04-28 13:33:09 +0430

    remove unused reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import inputReducer from "../features/data/inputDataSlice";
 import userInfoSlice from "../features/userInfo/UserInfoSlice";
 import EstimationPrimDataSlice from "../features/estimationPrimData/EstimationPrimDataSlice";
 import VisualComfortDataSlice from "../features/visualComfortData/VisualComfortDataSlice";
 import energyConsumptionDataSlice from "../features/energyConsumptionData/energyConsumptionsDataSlice";
-import structureDesignDataSlice from "../features/structureDesignData/structureDesignDataSlice";
 import messageSlice from "../features/message/messageSlice";
 import authReducer from "../features/auth/authSlice";
 
@@ -12,10 +10,8 @@ export const store = configureStore({
 	reducer: {
 		userInfo: userInfoSlice,
 		primaryData: EstimationPrimDataSlice,
-		input: inputReducer,
 		visualComfortData: VisualComfortDataSlice,
 		energyConsumptionData: energyConsumptionDataSlice,
-		structureDesignData: structureDesignDataSlice,
 		message: messageSlice,
 		auth: authReducer,
 	},
