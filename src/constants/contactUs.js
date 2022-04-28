2ac793c5bb999e55b927961c5419c0e555677777
erfan shafiee <erfan74sh@gmail.com>
2022-04-28 15:44:05 +0430

    create contactUsInfo constants

---
+++ src/constants/contactUs.js
@@ -0,0 +1,19 @@
+export const contactUsInfo = {
+	instagram: {
+		username: "BSP.sim",
+		url: "#",
+	},
+	linkedin: {
+		username: "BSP.sim",
+		url: "#",
+	},
+	email: {
+		username: "Info@bsp.green",
+		url: "#",
+	},
+	phoneNumber: "۰۲۱۸۸۹۲۹۰۱۷ - ۰۹۹۳۹۴۴۴۷۷۵",
+	location: {
+		address: "دانشگاه شهید‌بهشتی، ساختمان شماره ۲ دانشکده معماری و شهرسازی",
+		latLong: [35.796784220168966, 51.39476019337767],
+	},
+};
