a53b8742e4fd1a8148aba9408723ed64fd460444
erfan shafiee <erfan74sh@gmail.com>
2022-04-28 15:16:00 +0430

    add aboutUs contants to aboutUs.js

---
+++ src/constants/aboutUs.js
@@ -0,0 +1,94 @@
+import pic01 from "./../assets/profile/01.png";
+import pic02 from "./../assets/profile/02.jpeg";
+import pic03 from "./../assets/profile/03.png";
+import pic04 from "./../assets/profile/04.png";
+import pic05 from "./../assets/profile/05.png";
+import pic06 from "./../assets/profile/06.png";
+import pic07 from "./../assets/profile/07.png";
+import pic08 from "./../assets/profile/08.png";
+import pic09 from "./../assets/profile/09.jpg";
+import pic10 from "./../assets/profile/10.jpg";
+import pic11 from "./../assets/profile/11.jpg";
+import pic12 from "./../assets/profile/12.jpg";
+import pic13 from "./../assets/profile/13.jpg";
+
+export const members = [
+	{
+		fullName: "محمد تحصیلدوست",
+		education: "دکتری معماری علوم ساختمان",
+		activityScope: "عضو هیئت علمی",
+		image: pic05,
+	},
+	{
+		fullName: "زهرا سادات زمردیان",
+		education: "دکتری معماری علوم ساختمان",
+		activityScope: "عضو هیئت علمی",
+		image: pic08,
+	},
+	{
+		fullName: "رهام افغانی",
+		education: "دکتری مهندسی سیستم‌های ساختمانی",
+		activityScope: "عضو هیئت علمی",
+		image: pic09,
+	},
+	{
+		fullName: "پارسا باقرزاده",
+		education: "دکتری علوم کامپیوتر",
+		activityScope: "هوش مصنوعی",
+		image: pic13,
+	},
+	{
+		fullName: "پگاه اشراقی",
+		education: "پژوهشگر دکتری انرژی و معماری",
+		activityScope: "انرژی - آسایش حرارتی",
+		image: pic06,
+	},
+	{
+		fullName: "هانیه نورکجوری",
+		education: "ارشد انرژی و معماری",
+		activityScope: "نور - حریق",
+		image: pic04,
+	},
+	{
+		fullName: "محمد رجائیان هونجانی",
+		education: "ارشد تکنولوژی معماری",
+		activityScope: "سازه",
+		image: pic01,
+	},
+	{
+		fullName: "امیرمحمد ربانی جلالی",
+		education: "ارشد تکنولوژی معماری",
+		activityScope: "سازه",
+		image: pic11,
+	},
+	{
+		fullName: "آرمان نیکخواه",
+		education: "ارشد انرژی و معماری",
+		activityScope: "هوش مصنوعی - تحقیق و توسعه",
+		image: pic02,
+	},
+	{
+		fullName: "سوگند حقیقت",
+		education: "ارشد انرژی و معماری",
+		activityScope: "گرافیک - مکانیک",
+		image: pic07,
+	},
+	{
+		fullName: "محمد طباطبایی",
+		education: "ارشد انرژی و معماری",
+		activityScope: "طراح رایانشی - تولید داده",
+		image: pic03,
+	},
+	{
+		fullName: "عرفان شفیعی",
+		education: "ارشد معماری",
+		activityScope: "توسعه‌دهنده فرانت‌اند",
+		image: pic10,
+	},
+	{
+		fullName: "محمدرضا وطنخواه",
+		education: "ارشد مهندسی نرم‌افزار",
+		activityScope: "توسعه‌دهنده بک‌اند",
+		image: pic12,
+	},
+];
