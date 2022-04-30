62ec90d84bf1a498aaf24748ca04cbbb2f087103
erfan shafiee <erfan74sh@gmail.com>
2022-04-30 16:55:39 +0430

    create eNums constants

---
+++ src/constants/eNums.js
@@ -0,0 +1,53 @@
+// common eNums
+export const shadingTypeENums = {
+	1: "Horizontal",
+	2: "Horizontal Louvre",
+	3: "Vertical",
+	4: "All Modes",
+};
+// energyConsumption eNums
+export const boundryConditionENums = {
+	0: "Adiabatic",
+	1: "External",
+	2: "Ground",
+};
+export const hvacENums = {
+	0: "Ideal air loads",
+	1: "PTAC | residential",
+	2: "PTHP | residential",
+	7: "VAV w/reheat",
+	8: "VAV w/PFP boxes",
+	11: "Fan coil units + DOAS",
+};
+export const floorLevelENums = {
+	0: "Ground Floor",
+	1: "1st Floor",
+	2: "2nd Floor",
+	3: "3rd Floor",
+	4: "4th Floor",
+	5: "5th Floor",
+};
+export const wallUValueENums = {
+	1.719: "Brick wall + Brick facade",
+	1.649: "Cement Block + Stone facade",
+	0.979: "Cement Block + Cement facade",
+	0.225: "Two layer Brick wall with Insulation in the middle + Brick facade",
+};
+export const floorUValueENums = {
+	0.37: "Reinforced Concrete with air gap + Terrazzo",
+	0.47: "Beam Block + Parquet flooring",
+	0.65: "Reinforced concrete + Parquet flooring",
+};
+export const roofUValueENums = {
+	0.878: "Gypsum Board + Reinforced Concrete + Terrazzo",
+	0.211: "Reinforced Concrete with thermal insullation + Terrazzo",
+	0.393: "Beam block + terrazzo",
+	0.386: "Beam Block with out terrazzo",
+	1.092: "Gypsum Plaster + Reinforced Concrete + Terrazzo",
+};
+export const windowUValueENums = {
+	1.7: "Triple glazed windows",
+	2.6: "Double low-E glazed windows",
+	3.1: "Double glazed windows",
+	5.7: "Single glazed windows",
+};
